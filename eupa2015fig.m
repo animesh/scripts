c2ab138da5f266fb74e9ed16d83871d0a12e5252
Ani <krishn.bhakt@gmail.com>
2015-08-11 20:46:20 +0200

    test old neural network perl script

--- eupa2015fig.m
+++ eupa2015fig.m
@@ -3,12 +3,12 @@
 loglog([1 10 100 1000 10000 100000],[64/60 104/60 129/60 141/60 8 111],'b-o')
 hold on
 loglog([1 10 100 1000 10000 100000],[90.8/60 102.4/60 103.8/60 138.7/60 48 144],'r-o') % waiting for original results
-title('COMET: Running Time over ~128G RAM  and 32 Xeon ~2GHz CPU [LogLog Plot]')
+title('COMET: Running Time')
 xlabel('# Spectra');
 ylabel('# Minutes');
 legend('Classic','Spark','Location','NorthWest')
 hold off
-print('RunTime','-dbmp')
+print('RunTime','-djpeg')
 
 
 %% Score bar
@@ -26,28 +26,15 @@ plotBarStackGroups(Y, groupLabels); % plot groups of stacked bars
 title('Top Ranking Peptide Per Spectra; Classic and Spark based COMET algorithm')
 xlabel('Number of spectra searched');
 ylabel('# Peptide Matches [Log10]');
-legend('#common','#unique','Location','NorthEast');
-print('ScorePerf','-dbmp')
+legend('#common','#unique','Location','NorthWest');
+print('ScorePerf','-djpeg')
 
 
 %% CPU variation
-plot([12 24 36 48 60],[341.26 181.851 138.7 112.62 99.529],'b-o')
-title('SparkHydra Performance Run Over 1K Spectra')
-xlabel('# CPU');
-ylabel('# Seconds');
-print('CPUperf','-dbmp')
+plot(1:1000,-log(1:1000),'k.') % simulated
+title('Spark performance over CPU')
+xlabel('#cpu');
+ylabel('#time');
+print('CPUperf','-djpeg')
 
 
-%% run classical comet with oxidation M and phosphorylation STY against whole Uniprot with reverse as decoy
-
-F:\promec\comet>comet.2015011.win64.exe -Pcomet.params.steve.dbchange scan1000000.mzXML
- Comet version "2015.01 rev. 1"
-
- Search start:  06/16/2015, 04:11:10 PM
- - Input file: scan1000000.mzXML
-   - Load spectra:
-F:\promec\comet>comet.2015011.win64.exe -Pcomet.params.steve.dbchange scan10.mzXML
- Comet version "2015.01 rev. 1"
-
- Search start:  06/16/2015, 04:22:32 PM
- 
