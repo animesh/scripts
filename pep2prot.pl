b5fb9a769eeaea6d0106c6d7b994a2aef9f10796
animeshs@PROMEC <animeshs@promec01.medisin.ntnu.no>
2015-09-17 22:37:15 +0200

    maxquant peptide list mapped to the mutated protein sequences

--- pep2prot.pl
+++ pep2prot.pl
@@ -7,27 +7,34 @@ my %sfull;
 my $seqc;
 my $f1=shift @ARGV;
 my $f2=shift @ARGV;
-my $pid=1;
-my $col=5;
+my $col=0;
 
 open(F1,$f1);
 while(my $l1=<F1>){
 	chomp $l1;
         $l1=~s/\r//g;
-        if($l1=~/^>/){my @st=split(/\|/,$l1);$seqc=$st[$pid];$sfull{$seqc}=$l1;}
-        else{$l1=~s/[0-9]|\s+//g;$seqh{$seqc}.=uc($l1);}
+        if($l1=~/^>/){my @comsplit=split(/;/,$l1);$seqc=$comsplit[0];}
+        else{my @comsplitseq=split(/;/,$l1);$comsplitseq[0]=~s/[0-9]|\s+//g;$seqh{$seqc}.=uc($comsplitseq[0]);}
 }
 close F1;
 
 open(F2,$f2);
 while(my $l2=<F2>){
-	chomp $l2;
+	if($l2!~m/^Sequence/){
+		chomp $l2;
         $l2=~s/\r//g;
-        my @temp=parse_line(',',0,$l2);
+        my @temp=parse_line('\t',0,$l2);
+        print "$temp[$col]\t";
+		my $cntmat=0;
         foreach my $seq (keys %seqh){
-        	if($seqh{$seq}=~/$temp[$col]/){print "$temp[$col],$sfull{$seq},$l2\n";}
+        	if($seqh{$seq}=~/$temp[$col]/){
+				my $loccntmat=$seqh{$seq}=~s/$temp[$col]/$temp[$col]/g;
+				$seq=~s/^>//g;print "$seq;";
+				$cntmat+=$loccntmat;
+				}
         }
-        print 
+        print "\t$cntmat\n";
+	}
 }
 close F2;
 
