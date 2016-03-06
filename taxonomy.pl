6ae754fac2d75ba831b02a28020b461a0ef2d421
Animesh Sharma <animeshs@promec1-vm.medisin.ntnu.no>
2016-03-06 22:58:24 +0100

    get taxonomy information for organism with ftp://ftp.ncbi.nih.gov/pub/taxonomy/ dmp files

---
+++ taxonomy.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+use Bio::DB::Taxonomy;  
+my $db = Bio::DB::Taxonomy->new(  -source => 'flatfile',
+				  -directory => 'taxdmp/',
+				  -nodesfile => 'taxdmp/nodes.dmp',
+                                  -namesfile => 'taxdmp/names.dmp');
+my $name=shift @ARGV;
+open(F,$name);
+while(<F>) {
+my @tmp=split(/\t/);
+print "$tmp[0]\t";
+my $taxonid = $db->get_taxonid($tmp[0]);
+#my $taxon = $db->get_taxon(-taxonid => $taxonid);
+#print $taxon->id, "\t";
+#print $taxon->scientific_name, "\t";
+if (defined $taxonid) {
+    my $node = $db->get_Taxonomy_Node(-taxonid => $taxonid);
+    my $kingdom = $node;
+    for (my $lev=0;$lev<7;$lev++) {
+        if(defined  $kingdom->parent_id){
+	$kingdom = $db->get_Taxonomy_Node(-taxonid => $kingdom->parent_id);
+    	print $kingdom->scientific_name,"\t";}
+    }
+}
+print "\n";
+}
+__END__
+https://www.biostars.org/p/62911/#62951
