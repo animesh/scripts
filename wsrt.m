8a0b11410ed02a23a27063ec23d46e9ce1c605c9
Anix64 <animesh@fuzzylife.org>
2016-10-24 15:32:29 +0200

    extract columns for test and write results in xls file

--- wsrt.m
+++ wsrt.m
@@ -1,16 +1,24 @@
-%[data,id,~]=xlsread('Y:\felles\Voin\===Methodology paper===\Supplementary table MO stats.xlsx')
-%[data,id,~]=xlsread('Y:\felles\Voin\===JJN3===\JJN3 for wilcoxon.xlsx')
-[data,id,~]=xlsread('Y:\felles\Voin\t24\Copy of t24 for wilcoxon.xlsx')
-st=9%6%3%7%4%1%26%21%16%11%6%1;
-en=11%8%5%9%6%3%28%23%18%13%8%3;
-pvo=pv;
-clear pv;
-parfor i = 1:size(data,1)
-     if sum(isnan(data(i,st:en)))<3
-        pv(i)=signrank(log2(data(i,st:en)/100));
-        %pv(i)=signrank(data(i,st:en));
-     else
-        pv(i)=1;
-     end
+fo='Y:\felles\PROTEOMICS and XRAY\Ani\Voin\Supplementary wilcoxon.xlsx'
+testtype=' WSRT'
+[data,id,~]=xlsread(fo)
+idx=3;
+edx=20;
+jmp=3;
+pv=zeros(size(data,1),(edx-idx+1)/jmp)
+hdr=''
+for j = idx:jmp:edx
+    pcvt=j/jmp
+    st=j
+    en=j+jmp-1
+    hdr=[hdr strcat(id(1,j),num2str(j),testtype)]
+    for i = 1:size(data,1)
+         if sum(isnan(data(i,st:en)))<(en-st+1)
+            pv(i,pcvt)=signrank(log2(data(i,st:en)/100));
+            %pv(i,pcvt)=signrank(data(i,st:en));
+         else
+            pv(i,pcvt)=1;
+         end
+    end
 end
 hist(pv)
+xlswrite(strcat(fo,testtype,'un.xls'),[hdr;num2cell(pv)])
