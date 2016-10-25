99193c3619e5b8ecb90eb1a6c7347f4b521a08cf
Anix64 <animesh@fuzzylife.org>
2016-10-25 15:28:41 +0200

    add IDs in sign-rank test xls file

--- wsrt.m
+++ wsrt.m
@@ -1,24 +1,32 @@
-fo='Y:\felles\PROTEOMICS and XRAY\Ani\Voin\Supplementary wilcoxon.xlsx'
+fo='Y:\felles\PROTEOMICS and XRAY\Ani\Camilla\For Animesh_25102016.xlsx'
 testtype=' WSRT'
 [data,id,~]=xlsread(fo)
-idx=3;
-edx=20;
+IDX=1;
+idoff=6;
+idx=1;
+edx=9;
 jmp=3;
+idnm=id(:,IDX)
+data(:,all(isnan(data), 1)) = []
+id=id(1,~cellfun('isempty',id(1,:)))
 pv=zeros(size(data,1),(edx-idx+1)/jmp)
-hdr=''
+ln=zeros(size(data,1),1)
+hdr='Line '
 for j = idx:jmp:edx
-    pcvt=j/jmp
+    pcvt=ceil(j/jmp)
     st=j
     en=j+jmp-1
-    hdr=[hdr strcat(id(1,j),num2str(j),testtype)]
+    hdr=[hdr strcat(id(1,j+idoff),num2str(j),testtype)]
     for i = 1:size(data,1)
+        ln(i)=ln(i)+i;
          if sum(isnan(data(i,st:en)))<(en-st+1)
-            pv(i,pcvt)=signrank(log2(data(i,st:en)/100));
-            %pv(i,pcvt)=signrank(data(i,st:en));
+            %pv(i,pcvt)=signrank(log2(data(i,st:en)/100));
+            pv(i,pcvt)=signrank(data(i,st:en));
          else
             pv(i,pcvt)=1;
          end
     end
 end
 hist(pv)
-xlswrite(strcat(fo,testtype,'un.xls'),[hdr;num2cell(pv)])
+ln=ln*jmp/(edx-idx+1)
+xlswrite(strcat(fo,testtype,'un.xls'),[idnm [hdr;num2cell([ln pv])]])
