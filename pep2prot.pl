529c48e289a3799e646402665a398ae9fcda083b
U-WIN-NTNU-NO\animeshs <animeshs@DMED5604.win.ntnu.no>
2017-10-13 14:26:54 +0200

    uniprot PTM covered by peptide check

--- pep2prot.pl
+++ pep2prot.pl
@@ -2,50 +2,47 @@ use strict;
 use warnings;
 use Text::ParseWords;
 
-my %seqh;
-my %sfull;
-my $seqc=1;
-my $seqnm=0;
-my $f1=shift @ARGV;
-my $f2=shift @ARGV;
-my $col=0;
+my $f=shift @ARGV; #Mapped Uniprot File
+my $c1=shift @ARGV; #Protein Sequence
+my $c2=shift @ARGV; #Peptide Sequence
+my $c3=shift @ARGV; #Modification column
+my $c4=shift @ARGV; #Modification position
 
-open(F1,$f1);
-while(my $l1=<F1>){
-	chomp $l1;
-        $l1=~s/\r//g;
-        #if($l1=~/^>/){my @comsplit=split(/;/,$l1);$seqc=$comsplit[0];}
-        #else{my @comsplitseq=split(/;/,$l1);$comsplitseq[0]=~s/[0-9]|\s+//g;$seqh{$seqc}.=uc($comsplitseq[0]);}
-		my @comsplitseq=parse_line('\t',0,$l1);
-		my @snm=split(/\s+/,$comsplitseq[$seqnm]);
-		$seqh{$snm[0]}.=uc($comsplitseq[$seqc]);
-}
-close F1;
-
-print "$f2\t$f1\tCntMatch\n";
-open(F2,$f2);
-while(my $l2=<F2>){
-	if($l2!~m/^Sequence/){
-		chomp $l2;
-        $l2=~s/\r//g;
-        my @temp=parse_line('\t',0,$l2);
-        print "$temp[$col]\t";
-		my $cntmat=0;
-        foreach my $seq (keys %seqh){
-			$seqh{$seq}=~s/X/\./g;
-        	if($seqh{$seq}=~/$temp[$col]/){
-				my $loccntmat=$seqh{$seq}=~s/$temp[$col]/$temp[$col]/g;
-				$seq=~s/^>//g;print "$seq;";
-				$cntmat+=$loccntmat;
+open(F,$f);
+my $ln=0;
+while(my $l=<F>){
+	chomp $l;$l=~s/\r//g;
+	$ln++;
+	my @t=parse_line('\t',0,$l);
+	my $prot=uc($t[$c1]);
+	$prot=~s/\s+//g;
+	my $pep=uc($t[$c2]);
+	$pep=~s/I/L/gi;
+	$prot=~s/I/L/gi;
+	my @tpep=parse_line(';',0,$pep);
+	my $mod=$t[$c3];
+	my @tmod=parse_line(';',0,$mod);
+	for(my $cnt1;$cnt1<=$#tpep;$cnt1++){
+		for(my $cnt2;$cnt2<=$#tmod;$cnt2++){
+			$tpep[$cnt1]=~s/ //g;
+			my $start = index($prot, $tpep[$cnt1], 0)+1; # single match
+			my $end = $start+length($tpep[$cnt1])-1;
+			$tmod[$cnt2]=~s/^\s+//g;
+			my @tmodpos=parse_line(' ',0,$tmod[$cnt2]);
+			#while ( $prot =~ m/($tpep[$cnt1])/g){ #overlapping matches
+			#	my $peptide=$1;
+			#	my $start=pos($prot)-length($1)+1;
+			#	my $end=pos($prot);
+				if($start>0&&$tmodpos[$c4-1]=="MOD_RES"&&$tmodpos[$c4]<=$end&&$tmodpos[$c4]>=$start){
+					print "$ln\t$cnt1\t$cnt2\t$start\t$end\t$tpep[$cnt1]\t$tmod[$cnt2]\n";
 				}
-        }
-        print "\t$cntmat\n";
+			#}
+		}
 	}
 }
-close F2;
+close F;
 
 __END__
 
-perl pep2prot.pl /cygdrive/x/FastaDB/SerpinB3P29508.fasta /cygdrive/x/Qexactive/LARS/2013/november/ole\ jan/SerpinResultsBlast.txt 2>0
-
+perl pep2prot.pl /cygdrive/l/Animesh/Lymphoma/PTMseqList20.txt 1 67 12 1  2>t | less
 
