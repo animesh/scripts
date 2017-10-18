7c553c78a017dea6dd835926cd41c986ed3c51c2
U-WIN-NTNU-NO\animeshs <animeshs@DMED5604.win.ntnu.no>
2017-10-18 12:04:21 +0200

    overlapping patterns, mice imputation check

--- pep2prot.pl
+++ pep2prot.pl
@@ -23,21 +23,21 @@ while(my $l=<F>){
 	my $mod=$t[$c3];
 	my @tmod=parse_line(';',0,$mod);
 	for(my $cnt1;$cnt1<=$#tpep;$cnt1++){
-		for(my $cnt2;$cnt2<=$#tmod;$cnt2++){
+		#for(my $cnt2;$cnt2<=$#tmod;$cnt2++){
 			$tpep[$cnt1]=~s/ //g;
 			my $start = index($prot, $tpep[$cnt1], 0)+1; # single match
 			my $end = $start+length($tpep[$cnt1])-1;
-			$tmod[$cnt2]=~s/^\s+//g;
-			my @tmodpos=parse_line(' ',0,$tmod[$cnt2]);
-			#while ( $prot =~ m/($tpep[$cnt1])/g){ #overlapping matches
-			#	my $peptide=$1;
-			#	my $start=pos($prot)-length($1)+1;
-			#	my $end=pos($prot);
-				if($start>0&&$tmodpos[$c4-1]=="MOD_RES"&&$tmodpos[$c4]<=$end&&$tmodpos[$c4]>=$start){
-					print "$ln\t$cnt1\t$cnt2\t$start\t$end\t$tpep[$cnt1]\t$tmod[$cnt2]\n";
-				}
-			#}
-		}
+			#$tmod[$cnt2]=~s/^\s+//g;
+			#my @tmodpos=parse_line(' ',0,$tmod[$cnt2]);
+			while ($start>0){ #overlapping matches
+				#if($tmodpos[$c4-1]=="MOD_RES"&&$tmodpos[$c4]<=$end&&$tmodpos[$c4]>=$start){
+					#print "$ln\t$cnt1\t$cnt2\t$start\t$end\t$tpep[$cnt1]\t$tmod[$cnt2]\n";
+				#}
+				print "$ln\t$cnt1\t$start\t$end\t$tpep[$cnt1]\t$t[$c2]\n";
+				$start=index($prot, $tpep[$cnt1], $start)+1; # single match
+				$end = $start+length($tpep[$cnt1])-1;
+			}
+		#}
 	}
 }
 close F;
@@ -46,3 +46,4 @@ __END__
 
 perl pep2prot.pl /cygdrive/l/Animesh/Lymphoma/PTMseqList20.txt 1 67 12 1  2>t | less
 
+iiiowareyoulllih
