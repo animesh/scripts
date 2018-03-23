9a9e6b855241a33976bc2836e65e70f2e85ae0c0
Animesh Sharma <animeshs@animeshs-Latitude-E6230.NTNU>
2018-03-23 20:09:05 +0100

    combining ray-meta mapped reads using 21 k-mer over to bacterial genome downloaded with Kraken

--- combineReports.pl
+++ combineReports.pl
@@ -8,20 +8,22 @@ my $i0 = shift @ARGV; #Description
 my $i2 = shift @ARGV; #Value 
 my $cstr = shift @ARGV; #num or str 
 
-my @files=<$path/*$pat>;
+if($cstr){print "$path-$pat-$i1-$i0-$i2-$cstr\tVals\t";}
+else{die("use: perl combineReports.pl ID-col# description-col# value-col# str/num")}
+
+my @files=<$path/*/*/$pat>;
 my %protein;
 my %uniprot;
 my %nc;
 
-print "ID\tStem\tDescription\t";
 foreach my $f1 (@files){
     my @tmp;
     my @name;
     my %pg;
     my $lcnt;
     my $fn=$f1;
-    $fn=~s/$path\///g;
-    $fn=~s/\.csv$|\.txt$//g;
+    $fn=~s/$pat//g;
+    $fn=~s/$path//g;
     print "$fn-$i2\t";
     open (F1, $f1) || die "can't open \"$f1\": $!";
     while (my $line = <F1>) {
@@ -33,12 +35,12 @@ foreach my $f1 (@files){
 				#$line=~s/\s+/_/g;
         $lcnt++;
         if($pat=~/csv/){@tmp=parse_line(',',0,$line);}
-        if($pat=~/txt/){@tmp=parse_line('\t',0,$line);}
+	else{@tmp=parse_line('\t',0,$line);}
         if ($lcnt>1){
             @name=split(/\;/,$tmp[$i1]);
             foreach (@name) {
                 my $key="$_;$f1";
-				$protein{$key}.="$tmp[$i2];";
+		$protein{$key}.="$tmp[$i2];";
                 $nc{"$_"}.="$tmp[$i0];";
 				
             }
@@ -50,10 +52,10 @@ print "ExperimentsDetected\n";
 
 foreach my $g  (keys %nc){
     my $ocg;
-	my @gname= split(/\s+|\-|\_/,$g);
+	my @gname= split(/\s+|\-|\_|\./,$g);
 	my @ncgt=split(/;/,$nc{$g});
 	my @uniq = do { my %seen; grep { !$seen{$_}++ } @ncgt };
-    print "$g\t$gname[0]\t@uniq\t";
+    print "$g\t$gname[1] @uniq\t";
     foreach  my $f (@files){
         my $key="$g;$f";
         if($cstr eq "str" and $protein{$key} ne ""){print "$protein{$key}\t";$ocg++;}
@@ -76,9 +78,7 @@ foreach my $g  (keys %nc){
 }
 
 __END__	
-
-$ ln -s /cygdrive/y/felles/PROTEOMICS\ and\ XRAY/Articles\ in\ prep/AID/WCXlfq ../
-$ perl combinereports.pl ../WCXlfq _Proteins.txt 7 8 30 > /cygdrive/y/felles/PROTEOMICS\ and\ XRAY/Articles\ in\ prep/AID/WCXlfq/WCX_Proteins.txt               
+$ perl combineReports.pl $PWD 0.Profile.bacteria.tsv 0 1 1 num > BactMap                
 
  
 
---
+++ nextflowtest.nf
@@ -0,0 +1,15 @@
+process perlStuff {
+"""
+    #!/usr/bin/env perl
+
+    print 'Hi there!' . '\n';
+"""}
+
+process pyStuff {
+"""
+    #!/usr/bin/env python
+
+    x = 'Hello'
+    y = 'world!'
+    print "%s - %s" % (x,y)
+"""}
