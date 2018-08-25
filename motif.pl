16d82e14fd6a81bde2555c609ebac2e128345a3d
ani <sharma_animesh@hotmail.com>
2018-08-25 03:54:22 +0200

    allow for reversed motif search

--- motif.pl
+++ motif.pl
@@ -2,7 +2,11 @@
 use strict;
 use warnings;
 my $f=shift @ARGV;
-my $motif=shift;#qw/[ILMN]...[QVS]..[RKSE][ILM][QE].[NK][KR]..A[IL].[RLI][RLI]..[KR]/;#https://febs.onlinelibrary.wiley.com/doi/full/10.1111/febs.12867
+unless(-e $f){die "USAGE:perl motif.pl <fasta file> <motif as regular expression>";}
+
+my $motif=shift;
+my $rev=shift;
+if($rev){$motif=reverse($motif);$motif=~tr/][/[]/;}
 my $seqn="";
 my $seq="";
 my $cnt=0;
@@ -20,7 +24,7 @@ sub motifind {
 	return $motf;
 }
 
-print "Name\tMotif(s),Position(s);\tTotalMotifs\tSequenceLines\n";
+print "Sequence Header in $f\t$motif found as Sequence(s),Position(s,0 for 1st);\tTotalMotif(s)\tSequenceLine(s)*60~Len\n";
 open (F,$f);
 while (my $line = <F>) {
 	$line =~ s/[\r\n]+$//;
@@ -32,9 +36,17 @@ while (my $line = <F>) {
 	}
 	else{
 		if($cnt==0){print "$seqn\t";}
-		$line =~ s/[^a-zA-Z0-9,]//g;
+		$line =~ s/[^a-zA-Z]//g;
 		$seq.=$line;
 		$cnt++;
 	}
 }
 close F;
+
+__END__
+#example search for UNG motif as shown in https://febs.onlinelibrary.wiley.com/doi/full/10.1111/febs.12867
+curl "https://www.uniprot.org/uniprot/?query=UNG&sort=score&format=fasta" -o UNG.fasta # create fasta file with API based search for UNG sequences in uniprot sorted by score https://www.uniprot.org/help/api_queries
+perl motif.pl UNG.fasta "[ILMN]...[QVS]..[RKSE][ILM][QE].[NK][KR]..A[IL].[RLI][RLI]..[KR]" | awk -F '\t' '$2!=""' | less
+
+#to search for reverse form of the motif (only works for square bracket expression so far...)
+perl motif.pl UNG.fasta "[ILMN]...[QVS]..[RKSE][ILM][QE].[NK][KR]..A[IL].[RLI][RLI]..[KR]" reverse | awk -F '\t' '$2!=""' | less
