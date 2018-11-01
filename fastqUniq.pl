07fabccd8fb447442abe9d4e0fd467501c637020
ani <sharma_animesh@hotmail.com>
2018-11-01 17:39:11 +0100

    compress DNA string with 2 bit encoding

--- fastqUniq.pl
+++ fastqUniq.pl
@@ -8,6 +8,23 @@ die "$f.uniq.fasta exists, bailing out for $f!\n" if -e "$f.uniq.fasta";
 my $seqn="";
 my %seq;
 my $cnt=0;
+my %twoBit = ('T' => 0b00,'C' => 0b01,'G' => 0b10,'A' => 0b11, 0b00 => 'T',0b01 => 'C',0b10 => 'G',0b11 => 'A');
+
+sub compress2bit{
+  my $fasta=shift;
+  my @bases = split //, $fasta;
+  my $bits = '';
+  for my $i ( 0 .. $#bases ) {vec( $bits, $i, 2 ) = $twoBit{ $bases[$i] };}
+  return $bits;
+}
+
+sub expand2bit{
+	my $bits=shift;
+	my $bitslen=shift;
+  my $strings = '';
+  for my $i (0 .. $bitslen-1){$strings.=$twoBit{vec($bits,$i,2)};}
+  return $strings;
+}
 
 open (F,$f);
 while (my $line = <F>) {
@@ -21,14 +38,18 @@ while (my $line = <F>) {
 	else{
 		if($cnt==0){
 			$cnt++;
+			$line=~s/\s+//g;
+			my $lenseq=length($line);
 			my $rtseq=reverse($line);
 			$rtseq=~tr/ATGC/TACG/;
+			$rtseq=compress2bit($rtseq);
 			if($seq{$rtseq}){
-				$seq{$rtseq}.="revCom($seqn);";
+				$seq{$rtseq}.="revCom($seqn);$lenseq;";
 				#print "$seqn\t$seq{$rtseq}\t$line\t$rtseq\n";
 			}
 			elsif($line!~m/[^ATGC]/){
-				$seq{$line}.="$seqn;";
+				$line=compress2bit($line);
+				$seq{$line}.="$seqn;$lenseq;";
 			}
 			else{next;}
 		}
@@ -43,7 +64,9 @@ print "processed $f and writing non redundant $cnt fasta sequence(s) to $f.uniq.
 open(FO,">$f.uniq.fasta");
 #print FO Dumper(%seq);
 foreach (keys %seq){
-	print FO">$seq{$_}\n$_\n";
+	my @fastalen=split(/;/,$seq{$_});
+	my $fasta=expand2bit($_,$fastalen[-1]);
+	print FO">$seq{$_}\n$fasta\n";
 }
 close FO;
 __END__
--- test.pl
+++ test.pl
@@ -1,23 +1,26 @@
-#! /usr/bin/perl
+my %twoBit = ('T' => 0b00,'C' => 0b01,'G' => 0b10,'A' => 0b11, 0b00 => 'T',0b01 => 'C',0b10 => 'G',0b11 => 'A');
+my $fasta='TATAA';
+print $fasta. length( $fasta ) . "\n";
 
-use strict;
-use warnings;
-use AS::MSG;
-use Devel::Peek;
-
-my $parser = AS::MSG::Parser->new();
-my $i = 0;
-my @filters = ("MDI", "CLK");
-$parser->parser_initialize(*STDIN{IO});
-$parser->{"filter_out"} = \@filters;
-
-@filters = @{$parser->{"filter_in"}};
-print("in: @filters\n");
-@filters = @{$parser->{"filter_out"}};
-print("out: @filters\n");
-
-while($parser->parser_next()) {
-    $i++;
+sub compress2bit{
+  my $fasta=shift;
+  my @bases = split //, $fasta;
+  my $bits = '';
+  for my $i ( 0 .. $#bases ) {vec( $bits, $i, 2 ) = $twoBit{ $bases[$i] };}
+  return $bits;
 }
 
-print($i, "\n");
+sub expand2bit{
+  my $bits=shift;
+  print unpack("b*",$bits), "\n";
+  my $strings = '';
+  for my $i (0 .. oct("0b" . unpack("%0b2",$bits))-1){$strings.=$twoBit{vec($bits,$i,2)};}
+  #print $bits. length($bits) . "\n";
+  return $strings;
+}
+
+my $strs=compress2bit($fasta);
+#print $strs. length($strs) . "\n";
+
+my $exstrs=expand2bit($strs);
+print $exstrs. length($exstrs) . "\n";
