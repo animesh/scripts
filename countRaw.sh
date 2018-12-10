4319d895f692caaee2661a979206469627a12952
Animesh Sharma <ash022@login3-nird.nird.login.svc.cluster.local>
2018-12-10 18:31:45 +0100

    adding cron jobs on nird

---
+++ countRaw.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+watch -n 60 "find $HOME/Data/ -iname "*.raw" | wc"
+
---
+++ dataRaw.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+$HOME/gopath/gopath/bin/drive pull -ignore-conflict -ignore-name-clashes -no-prompt -verbose $HOME/Data/HF/
+$HOME/gopath/gopath/bin/drive pull -ignore-conflict -ignore-name-clashes -no-prompt -verbose $HOME/Data/QE
+$HOME/gopath/gopath/bin/drive pull -ignore-conflict -ignore-name-clashes -no-prompt -verbose $HOME/Data/Elite
+ 
