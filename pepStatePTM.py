1bc7ab86ba6e58272b46d27fa62cd42b047358b6
ani <sharma_animesh@hotmail.com>
2018-12-11 19:56:04 +0100

    checking for PTM occupancy following https://www.youtube.com/watch?v=7EUmMttFIfo&feature=youtu.be&t=1774

---
+++ pepStatePTM.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+pathFiles = Path('L:/promec/Animesh/Kathleen')
+fileName='allPeptides.txt'
+trainList=list(pathFiles.rglob(fileName))
+
+import pandas as pd
+df=pd.read_table(trainList[0])
+
+df.columns.values
+
+
+df['DP Mass Difference'].hist()
+
+df['DP Modification'].value_counts().plot(kind='bar').figure.savefig('foo.png',dpi=100,bbox_inches = "tight")
+
+dfDP=df.loc[:, df.columns.str.startswith('DP')]
+dfDP.rename(columns = lambda x : str(x)[3:])
+dfDP.to_csv('dfDP.csv')
+
+fileName='Phospho (STY)Sites.txt'
+trainList=list(pathFiles.rglob(fileName))
+dfPTM=pd.read_table(trainList[0], low_memory=False)
+dfPTM.columns.values
+
+
+dfPTM['Occupancy HT'].hist()
+
+dfPTM['Occupancy HT'].value_counts().plot(kind='bar').figure.savefig('foo.png',dpi=100,bbox_inches = "tight")
+
+dfPTMocc=dfPTM.loc[:, dfPTM.columns.str.startswith('Occupancy')]
+dfPTMocc.rename(columns = lambda x : str(x)[3:])
+dfPTMocc.to_csv('dfPTMocc.csv')
+
+#https://photos.app.goo.gl/Ja8mn8c4pwnajA7k8
+a=(z-y)/(x-z)
+p=a/(1+a)
+
+or=(1-(y/z))/((x/z)-1)
+st=or/(1+or)
+
+dE=dR*((1/((1-v)**2)+(1/((1-w)**2))**(1/2)
+v=rUModPep/rProtein
+v=rModPep/rProtein
