3794a44e4d5fc43e91fb24e1faec6a2d98f697c7
ani <sharma_animesh@hotmail.com>
2018-12-13 18:39:05 +0100

    heatmap with sample median scaling and row average, median aggregate

--- dePep.py
+++ dePep.py
@@ -1,20 +1,21 @@
 from pathlib import Path
-pathFiles = Path('L:/promec/Animesh/Tobias/HeLaIpAndrea')
+pathFiles = Path('L:/promec/Animesh/Tobias/HeLaIpAndrea/')
 fileName='allPeptides.txt'
 trainList=list(pathFiles.rglob(fileName))
 
 import pandas as pd
-df=pd.read_table(trainList[3])
-df_indi = (pd.read_table(f, low_memory=False) for f in trainList)
-concatenated_df   = pd.concat(df_indi, ignore_index=True)
+df=pd.read_table(trainList[0], low_memory=False)
+df.columns.get_loc("DP Proteins")
+#awk -F '\t' '{print $47}' promec/promec/USERS/MarianneNymark/181009/Charlotte/HF/combined/txt/allPeptides.txt | sort | uniq -c
+dfDP=df.loc[:, df.columns.str.startswith('DP')]
+dfDP=dfDP[dfDP['DP Proteins'].notnull()]
+dfDP=dfDP.rename(columns = lambda x : str(x)[3:])
+dfDP['Mass Difference'].hist()
+writeDPpng=pathFiles/(fileName+"DP.png")
+dfDP['Modification'].value_counts().plot(kind='bar')
+dfDPcnt=dfDP['Modification'].value_counts()
 
-concatenated_df.columns.values
-P13051=concatenated_df[concatenated_df['DP Proteins'].str.contains("P13051")==True]
+dfDPcnt[dfDPcnt>1].plot(kind='bar').figure.savefig(writeDPpng,dpi=100,bbox_inches = "tight")
 
-P13051['DP Mass Difference'].hist()
-P13051['DP Modification'].plot.pie()
-P13051['DP Modification'].value_counts().plot(kind='bar').figure.savefig('foo.png',dpi=100,bbox_inches = "tight")
-
-P13051DP=P13051.loc[:, P13051.columns.str.startswith('DP')]
-P13051DP.rename(columns = lambda x : str(x)[3:])
-P13051DP.to_csv('P13051DP.csv')
+writeDPcsv=pathFiles/(fileName+"DP.csv")
+dfDP.to_csv(writeDPcsv)
--- pepStatePTM.py
+++ pepStatePTM.py
@@ -4,18 +4,15 @@ fileName='allPeptides.txt'
 trainList=list(pathFiles.rglob(fileName))
 
 import pandas as pd
-df=pd.read_table(trainList[0])
-
-df.columns.values
-
-
-df['DP Mass Difference'].hist()
-
-df['DP Modification'].value_counts().plot(kind='bar').figure.savefig('foo.png',dpi=100,bbox_inches = "tight")
-
+df=pd.read_table(trainList[0], low_memory=False)
+df.columns.get_loc("DP Proteins")
+#awk -F '\t' '{print $47}' promec/promec/USERS/MarianneNymark/181009/Charlotte/HF/combined/txt/allPeptides.txt | sort | uniq -c
 dfDP=df.loc[:, df.columns.str.startswith('DP')]
-dfDP.rename(columns = lambda x : str(x)[3:])
-dfDP.to_csv('dfDP.csv')
+dfDP=dfDP[dfDP['DP Proteins'].notnull()]
+dfDP=dfDP.rename(columns = lambda x : str(x)[3:])
+dfDP['Mass Difference'].hist()
+dfDPcnt=dfDP['Modification'].value_counts()
+dfDPcnt[(dfDPcnt>2)&(dfDPcnt<800)].plot(kind='pie')
 
 fileName='Phospho (STY)Sites.txt'
 trainList=list(pathFiles.rglob(fileName))
