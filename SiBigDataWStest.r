bd5987a43976db7c98183e4ab7069cb49829d115
ani <sharma.animesh@gmail.com>
2019-06-08 22:09:40 +0200

    diabetes/status prediction, from deep-leaning to logistic-regression using Baez/Linda approach

--- SiBigDataWStest.r
+++ SiBigDataWStest.r
@@ -1,46 +1,94 @@
+rm(list=ls())
 fileP<-"/home/animeshs/Downloads/"
 fileN<-"diabetes.csv"
 yy<-read.csv(paste0(fileP, fileN),header=T)
 summary(yy[,9])
-#diab[,9]=as.numeric(diab[,9]-1)
+
 plot(yy,col=yy$Outcome,main="diabetes")
 
 yyt=as.matrix(yy)
-#yyt[,9]=as.factor(yyt[,9])
 library(ggfortify)
 log.yyt=log(yyt[,1:8]+1)
-yyt.pca=prcomp(log.yyt,center=TRUE,scale.=TRUE) 
+yyt.pca=prcomp(log.yyt,center=TRUE,scale.=TRUE)
 autoplot(yyt.pca,data=yyt,colour='Outcome',main="dataset")
 
-dimnames(yyt)=NULL
-summary(yyt)
+data<-yy
+str(data)
+
+# removing lines with zeros:
+data1 = data[!data$Glucose==0, ]
+data1 = data1[!data1$BMI==0, ]
+data1$Outcome <- factor(data1$Outcome)
+str(data1)
+summary(data1)
+
+# split the data
+ind=sample(2,nrow(data1),replace=TRUE,prob=c(0.8,0.2))
+data.training=data1[ind==1,]
+data.test=data1[ind==2,]
+
+# notmality of the data
+pairs(data.training)
+summary(data.training)
+
+shapiro.test(data.training$Pregnancies)
+hist(data.training$Pregnancies)
+
+shapiro.test(data.training$Glucose)
+hist(data.training$Glucose)
+
+shapiro.test(data.training$BloodPressure)
+hist(data.training$BloodPressure)
+
+shapiro.test(data.training$SkinThickness)
+hist(data.training$SkinThickness)
+
+shapiro.test(data.training$Insulin)
+hist(data.training$Insulin)
+
+shapiro.test(data.training$BMI)
+hist(data.training$BMI)
+
+shapiro.test(data.training$DiabetesPedigreeFunction)
+hist(data.training$DiabetesPedigreeFunction)
+
+shapiro.test(data.training$Age)
+hist(data.training$Age)
 
 
-library(keras)
-#install_keras()
-use_session_with_seed(2)
-ind=sample(2,nrow(yyt),replace=TRUE,prob=c(0.75,0.25))
+model  <- glm(data=data.training, Outcome ~ BMI  , family  = "binomial")
+model1 <- glm(data=data.training, Outcome ~ Glucose  , family  = "binomial")
+model2 <- glm(data=data.training, Outcome ~ Glucose + BMI  , family  = "binomial")
+model3 <- glm(data=data.training, Outcome ~ Age , family  = "binomial")
+model4 <- glm(data=data.training, Outcome ~ Glucose + BMI + Age , family  = "binomial")
 
-yyt.training=yyt[ind==1,1:8]
-yyt.test=yyt[ind==2,1:8]
 
-yyt.trainingtarget=yyt[ind==1,9]
-yyt.testtarget=yyt[ind==2,9]
-
-yyt.trainLabels=to_categorical(yyt.trainingtarget)
-yyt.testLabels=to_categorical(yyt.testtarget)
-
-model=keras_model_sequential()
-model %>%
-  layer_dense(input_shape=c(8),units=8,activation='relu',kernel_initializer="glorot_normal",use_bias=TRUE) %>%
-      layer_dense(input_shape=c(6),units=6) %>%
-      layer_dense(input_shape=c(4),units=4) %>%
-  layer_dense(units=2,activation='softmax',kernel_initializer="glorot_normal",use_bias=TRUE)
 summary(model)
+summary(model1)
+summary(model2)
+summary(model3)
+summary(model4) #best model
 
-model %>% compile(loss='categorical_crossentropy',optimizer='adam',metrics='accuracy')
-history=model %>% fit(yyt.training,yyt.trainLabels,epochs=100,batch_size=nrow(yyt.training),validation_split=0.2)
+plot(fitted(model1) ~ Glucose , data=data.training)
+curve(exp(-5.690114+0.040457 *x)/
+        (1+exp(-5.690114+0.040457 *x)), col= "green",add=TRUE) #estimates of the summery (model1)
 
-plot(history)
-predicted.classes=model %>% predict_classes(yyt.test)
-table(yyt.testtarget,predicted.classes)
+
+model4 <- glm(data=data.training, Outcome ~ Glucose + BMI + Age, family  = "binomial")
+summary(model4)
+
+predict(model4, data.test, type = "response")
+
+prob <- data.frame(predict(model4, data.test, type = "response"))
+names(prob) <- c('var1')
+prob$new <- ifelse(prob$var1 > 0.5, 1,0)
+prob$Outcome <- data.test$Outcome
+
+table = table(prob$new,prob$Outcome)
+
+perc =(table[1]+table[4]) / sum(table)
+perc
+
+library(caret)
+confusionMatrix(as.factor(as.numeric(prob$var1>0.5)), as.factor(prob$Outcome))
+
