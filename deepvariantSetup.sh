03b319ae5eaeaf84566c2b06240c4da2b73cb880
Animesh Sharma <animeshs@ntnu.no>
2019-08-02 13:23:14 +0200

    Alanine scanning as mutation, hap.py and VEP install and test for deepvariant 0.8

--- deepvariantSetup.sh
+++ deepvariantSetup.sh
@@ -1,4 +1,8 @@
 #https://github.com/google/deepvariant/blob/r0.8/docs/deepvariant-quick-start.md from https://github.com/google/deepvariant/blob/r0.6/docs/deepvariant-quick-start.md , skipping https://github.com/google/deepvariant/blob/r0.7/docs/deepvariant-quick-start.md
+#pip install tensorflow==1.13.1 --user
+#cleanup
+sudo apt -y remove containerd.io
+sudo apt -y autoremove
 #install gcloud
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt-get install apt-transport-https ca-certificates
@@ -6,74 +10,79 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyr
 sudo apt-get update && sudo apt-get install google-cloud-sdk
 gcloud init
 gcloud auth login
-#run in parallel
+#to run in parallel
 sudo apt -y install parallel
 #clone deepvariant
 git clone https://github.com/animesh/deepvariant.git
 cd deepvariant
 #install deepvariant
-./build-prereq.sh
 #ERROR: markdown 3.1.1 has requirement setuptools>=36, but you'll have setuptools 20.7.0 which is incompatible.
-#pip install setuptools-markdown --upgrade --user
+pip install setuptools-markdown --upgrade --user
+./build-prereq.sh
+#test build
 ./build_and_test.sh
 ./run-prereq.sh
+#sudo ln -s $PWD/bazel-bin/deepvariant /opt/deepvariant
+ls bazel-bin/deepvariant/
+#sudo ln -s /home/animeshs/deepvariant/bazel-bin/deepvariant /opt/deepvariant/bin
+#https://github.com/google/deepvariant/issues/199
+scripts/run_wes_case_study_binaries.sh
+#running on example dataset
 gsutil cp -R gs://deepvariant/quickstart-testdata .
 mkdir -p quickstart-output
 gsutil cp -R gs://deepvariant/models/DeepVariant/0.8.0 .
-sudo ln -s $PWD/bazel-bin/deepvariant /opt/deepvariant
-ls bazel-bin/deepvariant/
-sudo ln -s /home/animeshs/deepvariant/bazel-bin/deepvariant /opt/deepvariant/bin
-python bazel-bin/deepvariant/make_examples.zip --mode calling  --ref quickstart-testdata/ucsc.hg19.chr20.unittest.fasta --reads quickstart-testdata/NA12878_S1.chr20.10_10p1mb.bam -regions "chr20:10,000,000-10,010,000" --examples quickstart-output/examples.tfrecord.gz
-python ./scripts/run_deepvariant.py --model_type=WGS --ref=quickstart-testdata/ucsc.hg19.chr20.unittest.fasta --reads=quickstart-testdata/NA12878_S1.chr20.10_10p1mb.bam -regions "chr20:10,000,000-10,010,000" --output_vcf=quickstart-output/output.vcf.gz  --output_gvcf=quickstart-output/output.g.vcf.gz --num_shards=1
-python bazel-bin/deepvariant/make_examples.zip --mode calling --ref "quickstart-testdata/ucsc.hg19.chr20.unittest.fasta" --reads "quickstart-testdata/NA12878_S1.chr20.10_10p1mb.bam" --examples "/tmp/deepvariant_tmp_output/make_examples.tfrecord@1.gz" --regions "chr20:10,000,000-10,010,000" --gvcf "/tmp/deepvariant_tmp_output/gvcf.tfrecord@1.gz"
-python ./scripts/run_deepvariant.py --model_type=WGS --ref=quickstart-testdata/ucsc.hg19.chr20.unittest.fasta --reads=quickstart-testdata/NA12878_S1.chr20.10_10p1mb.bam -regions "chr20:10,000,000-10,010,000" --output_vcf=quickstart-output/output.vcf.gz  --output_gvcf=quickstart-output/output.g.vcf.gz --num_shards=1
-#https://github.com/google/deepvariant/issues/199
-sudo apt autoremove
-sudo apt remove containerd.io
-scripts/run_wes_case_study_binaries.sh
-#https://github.com/google/deepvariant/blob/r0.6/docs/deepvariant-quick-start.md
-#source deepvariantSetup.sh
-#cd deepvariant
-PATH=$PATH:$HOME/HeLa/rtg-tools-3.10.1-linux-x64/rtg-tools-3.10.1
-BUCKET="gs://deepvariant"
-BIN_VERSION="0.6.1"
-MODEL_VERSION="0.6.0"
-MODEL_CL="191676894"
-BIN_BUCKET="${BUCKET}/binaries/DeepVariant/${BIN_VERSION}/DeepVariant-${BIN_VERSION}+cl-*"
-MODEL_NAME="DeepVariant-inception_v3-${MODEL_VERSION}+cl-${MODEL_CL}.data-wgs_standard"
-MODEL_BUCKET="${BUCKET}/models/DeepVariant/${MODEL_VERSION}/${MODEL_NAME}"
-DATA_BUCKET="${BUCKET}/quickstart-testdata"
-OUTPUT_DIR=quickstart-output
-REF=quickstart-testdata/ucsc.hg19.chr20.unittest.fasta
-BAM=quickstart-testdata/NA12878_S1.chr20.10_10p1mb.bam
-MODEL="${MODEL_NAME}/model.ckpt"
-LOGDIR=./logs
-N_SHARDS=3
-CALL_VARIANTS_OUTPUT="${OUTPUT_DIR}/call_variants_output.tfrecord.gz"
-FINAL_OUTPUT_VCF="${OUTPUT_DIR}/output.vcf.gz"
+#make examples
+python bazel-bin/deepvariant/make_examples.zip --mode calling  --ref quickstart-testdata/ucsc.hg19.chr20.unittest.fasta --reads quickstart-testdata/NA12878_S1.chr20.10_10p1mb.bam --regions "chr20:10,000,000-10,010,000" --examples quickstart-output/examples.tfrecord.gz #--gvcf quickstart-output/examples.tfrecord.vcf.gz
+#call variants
+python ./bazel-bin/deepvariant/call_variants.zip --outfile quickstart-output/examples.tfrecord.cvo.gz --examples quickstart-output/examples.tfrecord.gz --checkpoint 0.8.0/DeepVariant-inception_v3-0.8.0+data-wgs_standard/model.ckpt
+#postprocess/generate vcf
+python ./bazel-bin/deepvariant/postprocess_variants.zip --ref quickstart-testdata/ucsc.hg19.chr20.unittest.fasta --infile  quickstart-output/examples.tfrecord.cvo.gz --outfile  quickstart-output/examples.vcf
+#compare results with gold-standard
+#git clone https://github.com/Illumina/hap.py
+#cd hap.py
+#sudo apt-get install default-jdk ant
+#src/sh/make_hg19.sh
+#export HGREF=hg19.fa
+#python install.py . --with-rtgtools
+hap.py/bin/hap.py quickstart-testdata/test_nist.b37_chr20_100kbp_at_10mb.vcf.gz quickstart-output/examples.vcf -f quickstart-testdata/test_nist.b37_chr20_100kbp_at_10mb.bed -r quickstart-testdata/u
+csc.hg19.chr20.unittest.fasta -o happyeg.out --engine=vcfeval -l chr20:10000000-10010000
 #for loop through entire bam folder and run for chromosome in parallel
 #for i in  bam/vcf/*fastq.sam.bam.hg38.tfrecord.1.gz ; do echo $i; j=$(basename $i);   j=${j%%.*}; echo $j ; parallel -j24 python bin/call_variants.zip --outfile bam/vcf/$j.{}.VO  --examples bam/vcf/$j.fastq.sam.bam.hg38.tfrecord.{}.gz --checkpoint  DeepVariant-inception_v3-0.6.0+cl-191676894.data-wgs_standard/model.ckpt ::: {1..22} X Y   ; done
 #for i in  bam/vcf/*fastq.sam.bam.hg38.tfrecord.1.gz ; do echo $i; j=$(basename $i);   j=${j%%.*}; echo $j ; parallel -j24 python bin/postprocess_variants.zip --ref ../hg38chr3bwaidx.fasta   --infile bam/vcf/$j.{}.VO --outfile bam/vcf/$j.{}.vcf ::: {1..22} X Y   ; done
 #awk '{print $1}' bam/vcf/SRR2185909.vcf | sort -r | uniq -c
 #check effect of detected variants
-#git clone https://github.com/Ensembl/ensembl-vep.git
-#cd ensembl-vep
+cd $HOME
+git clone https://github.com/Ensembl/ensembl-vep.git
+cd ensembl-vep
+#sudo apt-cache search perl | grep dbi
+sudo apt install libdbi-perl
+sudo apt install libmodule-build-perl
+perl INSTALL.pl
+#NB: Remember to use --refseq when running the VEP with this cache!
+#300
+# - downloading ftp://ftp.ensembl.org/pub/release-96/variation/indexed_vep_cache/homo_sapiens_vep_96_GRCh38.tar.gz
+#303
+# - downloading ftp://ftp.ensembl.org/pub/release-97/variation/indexed_vep_cache/homo_sapiens_refseq_vep_97_GRCh37.tar.gz
+# ftp://ftp.ensembl.org/pub/grch37/release-97/gtf/homo_sapiens/Homo_sapiens.GRCh37.87.gtf.gz
+# ftp://ftp.ensembl.org/pub/grch37/release-97/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.dna.primary_assembly.fa.gz
+#306
+# - downloading ftp://ftp.ensembl.org/pub/release-97/variation/indexed_vep_cache/homo_sapiens_vep_97_GRCh38.tar.gz
+# https://www.ensembl.org/info/docs/tools/vep/script/vep_tutorial.html
+#install ensembl-tools
+cd $HOME
+git clone https://github.com/Ensembl/ensembl-tools.git
+cd ensembl-tools/scripts/variant_effect_predictor
+git checkout release/88
+git pull
+export PERL5LIB=$HOME/ensembl-vep:$PERL5LIB
+sudo apt install libdbd-mysql-perl
+#example file
+./variant_effect_predictor.pl -i ../../../deepvariant/quickstart-output/examples.vcf --plugin ProteinSeqs,ref.fasta,mut.fasta -o mutated --cache
+#overwrite
+$HOME/ensembl-tools/scripts/variant_effect_predictor/variant_effect_predictor.pl -i SRR2185909.vcf  --plugin ProteinSeqs,ref.fasta,mut.fasta -o mutated --cache --force_overwrite
+#test files
 #for i in  ../deepvariant/bam/*.1.vcf ; do echo $i; j=$(basename $i);   j=${j%%.*}; echo $j ; parallel -j24 ../ensembl-tools/scripts/variant_effect_predictor/variant_effect_predictor.pl -i ../deepvariant/bam/$j.{}.vcf --plugin ProteinSeqs,mutated/$j.{}.ref.fasta,mutated/$j.{}.mut.fasta -o mutated/$j.{}. --cache ::: {1..22} X Y   ; done
 #collect variants in a single file with FILENAME in fasta header
 #cd mutated
 #for i in  *.mut.fasta ;  do  awk '{if(/^>/){print $1,FILENAME}else print}' $i; done >> HeLa.deepvariant.vep.mutated.fasta
 #grep "ENSP00000354040.4:p.Ala250GlyfsTer9" -B1 -A1 *fasta
-#/mnt/f/HeLa/hap.py/bin/hap.py quickstart-testdata/test_nist.b37_chr20_100kbp_at_10mb.vcf.gz SRR2185909.vcf -f quickstart-testdata/test_nist.b37_chr20_100kbp_at_10mb.bed -r quickstart-testdata/ucsc.hg19.chr20.unittest.fasta -o happyeg.out --engine=vcfeval -l chr20:1-10010000
-#python bin/make_examples.zip   --mode calling     --ref "${REF}"     --reads "${BAM}"   --regions "chr20:10,000,000-10,010,000"   --examples "${OUTPUT_DIR}/examples.tfrecord.gz"
-#python bin/call_variants.zip  --outfile "${CALL_VARIANTS_OUTPUT}"  --examples "${OUTPUT_DIR}/examples.tfrecord.gz"  --checkpoint 0.6.0/DeepVariant-inception_v3-0.6.0+cl-191676894.data-wgs_standard/model.ckpt
-#python bin/postprocess_variants.zip   --ref "${REF}"   --infile "${CALL_VARIANTS_OUTPUT}"   --outfile "${FINAL_OUTPUT_VCF}"
-#python $HOME/hap.py-install/bin/hap.py quickstart-testdata/test_nist.b37_chr20_100kbp_at_10mb.vcf.gz   "${FINAL_OUTPUT_VCF}"   -f quickstart-testdata/test_nist.b37_chr20_100kbp_at_10mb.bed   -r "${REF}"   -o "${OUTPUT_DIR}/happy.output"   --engine=vcfeval   -l chr20:10000000-10010000
-#git clone https://github.com/Illumina/hap.py
-#sudo apt install openjdk-8-jdk
-#python install.py . --with-rtgtools
-#/mnt/f/HeLa/hap.py/bin/hap.py quickstart-testdatatest_nist.b37_chr20_100kbp_at_10mb.vcf.gz examplesroot.vcf -f quickstart-testdata/test_nist.b37_chr20_100kbp_at_10mb.bed -r quickstart-testdata/ucsc.hg19.chr20.unittest.fasta -o happyeg.out --engine=vcfeval -l chr20:10000000-10010000
-#for i in /mnt/z/ncbi/sra/dbGaP-12668/*.fastq ; do echo $i ; bwa mem -M -t 8 /home/animeshs/HeLa/hg38chr3bwaidx $i > $i.sam ; done
-#for i in bam/* ; do echo $i; j=$(basename $i);   j=${j%%.*} ; echo $j ; time seq 0 $((N_SHARDS-1)) | parallel --eta --halt 2 --joblog "${LOGDIR}/log" --res "${LOGDIR}" python bin/make_examples.zip --mode calling   --ref ../hg38chr3bwaidx.fasta  --reads $i --examples $i.hg38.tfrecord@${N_SHARDS}.gz --sample_name $j --regions '"chr1"'  --task {} ; done
-#for i in bam/*.sam.bam ; do echo $i; j=$(basename $i);   j=${j%%.*} ; echo $j ; parallel python bin/make_examples.zip --mode calling   --ref ../hg38chr3bwaidx.fasta  --reads $i --examples $i.hg38.tfrecord.{}.gz --sample_name $j --regions '"chr{}"'  ::: {2..22} X Y   ; done
-#for i in bam/*.sam.bam ; do echo $i; j=$(basename $i);   j=${j%%.*} ; echo $j ; parallel python bin/make_examples.zip --mode calling   --ref ../hg38chr3bwaidx.fasta  --reads $i --examples $i.hg38.tfrecord.{}.gz --sample_name $j --regions '"chr{}"'  ::: {1..22} X Y   ; done
-#bcftools mpileup -Ou -P 1.1e-5 --max-depth 1000 -f ../hg38chr3bwaidx.fasta /mnt/z/ncbi/sra/dbGaP-12668/SRR*.fastq.sam.bam | bcftools call -mv -Oz -o calls.vcf.gz
--- extractGTFseqMutate.pl
+++ extractGTFseqMutate.pl
@@ -5,6 +5,28 @@ my %seqh;
 my %seqm;
 my $seqc;
 my $f1=shift @ARGV;
+
+my %codon =
+(
+  "TTT" => "F", "TTC" => "F", "TTA" => "L", "TTG" => "L",
+  "TCT" => "S", "TCC" => "S", "TCA" => "S", "TCG" => "S",
+  "TAT" => "Y", "TAC" => "Y", "TAA" => "*", "TAG" => "*",
+  "TGT" => "C", "TGC" => "C", "TGA" => "*", "TGG" => "W",
+  "CTT" => "L", "CTC" => "L", "CTA" => "L", "CTG" => "L",
+  "CCT" => "P", "CCC" => "P", "CCA" => "P", "CCG" => "P",
+  "CAT" => "H", "CAC" => "H", "CAA" => "Q", "CAG" => "Q",
+  "CGT" => "R", "CGC" => "R", "CGA" => "R", "CGG" => "R",
+  "ATT" => "I", "ATC" => "I", "ATA" => "I", "ATG" => "M",
+  "ACT" => "T", "ACC" => "T", "ACA" => "T", "ACG" => "T",
+  "AAT" => "N", "AAC" => "N", "AAA" => "K", "AAG" => "K",
+  "AGT" => "S", "AGC" => "S", "AGA" => "R", "AGG" => "R",
+  "GTT" => "V", "GTC" => "V", "GTA" => "V", "GTG" => "V",
+  "GCT" => "A", "GCC" => "A", "GCA" => "A", "GCG" => "A",
+  "GAT" => "D", "GAC" => "D", "GAA" => "E", "GAG" => "E",
+  "GGT" => "G", "GGC" => "G", "GGA" => "G", "GGG" => "G",
+);
+
+
 open(F1,$f1);
 while(my $l1=<F1>){
 	chomp $l1;
@@ -43,10 +65,22 @@ while(my $l2=<F2>){
 close F2;
 
 if($mut){
-	print ">mut|$gn|$chr|$mut|","\n";
+	print ">mut|$gn|$chr|$mut=>A|","\n";
 	my $seq=substr($seqh{$chr},$min+1,$max-$min+1);
-	$seq="T".$seq;
-	print "$seq\n";
+	my $mutseq="";
+	if($seq=~/^ATG/){
+		for (my $i = 0; $i+2 < length($seq); $i+=3)
+		{
+			my $aa = $codon{uc(substr($seq, $i, 3))};
+			if($aa eq $mut){
+				$mutseq.="GCC";#https://www.genscript.com/tools/codon-frequency-table 0.4 for "A"
+			}
+			else{
+				$mutseq.=uc(substr($seq, $i, 3));
+			}
+		}
+	}
+	print "$mutseq\n";
 }
 
 if($utr){
@@ -56,9 +90,9 @@ if($utr){
 
 __END__
 perl extractGTFseqMutate.pl fastaFile GTFfile Gene mutatePosition lengthUTR
-#example "perl extractGTFseqMutate.pl ../Homo_sapiens.GRCh38.dna.primary_assembly.fa ../Homo_sapiens.GRCh38.96.gtf UNG 1 100 2>0"
+#example "perl extractGTFseqMutate.pl  "F:\promec\Animesh\Homo_sapiens.GRCh38.dna.primary_assembly.fa"  "F:\promec\Animesh\Homo_sapiens.GRCh38.96.gtf" UNG V 100 2>0"
 fastaFile: download ftp://ftp.ensembl.org/pub/release-96/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz and gunzip
 #GTF: download ftp://ftp.ensembl.org/pub/release-96/gtf/homo_sapiens/Homo_sapiens.GRCh38.96.gtf.gz and gunzip
 #Gene: UNG https://www.ensembl.org/Homo_sapiens/Gene/Summary?g=ENSG00000076248;r=12:109097574-109110992
-#mutatePosition: 1
+#mutateAminoAcid: V for Valine to A=>Alanine  https://en.wikipedia.org/wiki/Alanine_scanning
 #UTR: 100 https://www.ensembl.org/Homo_sapiens/Location/View?db=core;g=ENSG00000076248;r=12:109097474-109111002
