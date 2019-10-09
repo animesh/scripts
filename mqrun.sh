dad0837e68ab0f7b5545d26659dfe5680cdf8251
ani <sharma.animesh@gmail.com>
2019-10-09 18:25:34 +0200

    run maxquant in shell

---
+++ mqrun.sh
@@ -0,0 +1,2 @@
+mono $HOME/data/NORSTORE_OSL_DISK/NS9036K/promec/MaxQuant_1.6.8.0/MaxQuant/bin/MaxQuantCmd.exe -n $HOME/promec/Qexactive/Mirta/QExactive/Imen_Belhaj/RawData/out1678.xml
+#mono $HOME/data/NORSTORE_OSL_DISK/NS9036K/promec/MaxQuant_1.6.8.0/MaxQuant/bin/MaxQuantCmd.exe -p 7 $HOME/promec/Qexactive/Mirta/QExactive/Imen_Belhaj/RawData/out1678.xml
