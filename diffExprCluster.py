f3817f123d2460d29e7418d9d220443e3c409b04
ani <sharma_animesh@hotmail.com>
2019-12-13 18:21:57 +0100

    parsing pho score, normalize with method yeo-johnson

--- diffExprCluster.py
+++ diffExprCluster.py
@@ -19,24 +19,30 @@ df = pd.read_table(inpD/midF/inpF)
 df.describe()
 
 import numpy as np
-dfLFQ=df.loc[:, df.columns.str.startswith('LFQ')&df.columns.str.contains('pim')]
-(dfLFQ+1).apply(np.log2).hist()
+dfLFQ=df.loc[:, df.columns.str.startswith('LFQ')]#&df.columns.str.contains('pim')]
+#(dfLFQ+1).apply(np.log2).hist()
 
 dfLFQ=dfLFQ.rename(columns = lambda x : str(x)[21:])
 rowName= df["Protein IDs"].str.split(';').str[0]
 dfLFQ=dfLFQ.rename(index=rowName)
-print(dfLFQ.columns)
+#print(dfLFQ.columns)
 #https://github.com/BIMSBbioinfo/maui/blob/master/vignette/maui_vignette.ipynb
 import maui
 import maui.utils
 print(f'Maui version: {maui.__version__}')
-dfLFQ = maui.utils.scale(dfLFQ)
-(dfLFQ+1).hist() #really weird scaling
+#https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.PowerTransformer.html
+from sklearn.preprocessing import PowerTransformer
+pt = PowerTransformer()
+dfLFQlog2=np.log2(dfLFQ+1) #really weird scaling
+print(pt.fit(dfLFQlog2))
+dfLFQtf=pt.transform(dfLFQlog2)
+print(pt.lambdas_)
+#dfLFQ = maui.utils.scale(dfLFQ)
 from keras import backend as K
 import tensorflow as tf
-K.set_session(K.tf.Session(config=K.tf.ConfigProto(intra_op_parallelism_threads=12, inter_op_parallelism_threads=12)))
+#K.set_session(K.tf.Session(config=K.tf.ConfigProto(intra_op_parallelism_threads=12, inter_op_parallelism_threads=12)))
 maui_model = maui.Maui(n_hidden=[1100], n_latent=70, epochs=400)
-z = maui_model.fit_transform({'mRNA': dfLFQ})
+z = maui_model.fit_transform({'mRNA': dfLFQlog2tf})
 
 maui_model.hist.plot()
 maui_model.cluster(ami_y = z)
