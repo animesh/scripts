e82319e784ac037891d60234e9f76c014e29d790
ani <animesh@fuzzylife.org>
2019-12-29 21:56:10 +0100

    play with sonicpi, tfp, ts

--- ann_tf.py
+++ ann_tf.py
@@ -34,3 +34,52 @@ for epoch in range(epochs):
     print(t.gradient(loss, [w2, bias[1]]))
     #weights.assign_sub(lr * dW)
     #bias.assign_sub(lr * dB)
+
+#https://www.tensorflow.org/tensorboard/scalars_and_keras
+import numpy as np
+data_size = 1000
+# 80% of the data is for training.
+train_pct = 0.8
+
+train_size = int(data_size * train_pct)
+
+# Create some input data between -1 and 1 and randomize it.
+x = np.linspace(-1, 1, data_size)
+np.random.shuffle(x)
+
+# Generate the output data.
+# y = 0.5x + 2 + noise
+y = 0.5 * x + 2 + np.random.normal(0, 0.05, (data_size, ))
+
+# Split into test and train pairs.
+x_train, y_train = x[:train_size], y[:train_size]
+x_test, y_test = x[train_size:], y[train_size:]
+
+
+from datetime import datetime
+from tensorflow import keras
+logdir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+
+model = keras.models.Sequential([
+    keras.layers.Dense(16, input_dim=1),
+    keras.layers.Dense(1),
+])
+
+model.compile(
+    loss='mse', # keras.losses.mean_squared_error
+    optimizer=keras.optimizers.SGD(lr=0.2),
+)
+
+print("Training ... With default parameters, this takes less than 10 seconds.")
+training_history = model.fit(
+    x_train, # input
+    y_train, # output
+    batch_size=train_size,
+    verbose=0, # Suppress chatty output; use Tensorboard instead
+    epochs=100,
+    validation_data=(x_test, y_test),
+    callbacks=[tensorboard_callback],
+)
+
+print("Average test loss: ", np.average(training_history.history['loss']))
