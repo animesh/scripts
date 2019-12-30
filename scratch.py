64e8c5c4720b199aedcb3dfdf616630d9a92703e
ani <animesh@fuzzylife.org>
2019-12-30 18:37:22 +0100

    check neural struct learning, clustering comparison with diceR, ts analysis

--- scratch.py
+++ scratch.py
@@ -1,5 +1,44 @@
 system("jupyter" "notebook" "list")
 
+#https://www.machinelearningplus.com/time-series/time-series-analysis-python/
+from dateutil.parser import parse
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+plt.rcParams.update({'figure.figsize': (10, 7), 'figure.dpi': 120})
+
+# Import as Dataframe
+df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv', parse_dates=['date'])
+df.head()
+ser = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv', parse_dates=['date'], index_col='date')
+ser.head()
+
+# dataset source: https://github.com/rouseguy
+df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/MarketArrivals.csv')
+df = df.loc[df.market=='MUMBAI', :]
+df.head()
+
+# Time series data source: fpp pacakge in R.
+import matplotlib.pyplot as plt
+df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv', parse_dates=['date'], index_col='date')
+
+# Draw Plot
+def plot_df(df, x, y, title="", xlabel='Date', ylabel='Value', dpi=100):
+    plt.figure(figsize=(16,5), dpi=dpi)
+    plt.plot(x, y, color='tab:red')
+    plt.gca().set(title=title, xlabel=xlabel, ylabel=ylabel)
+    plt.show()
+
+plot_df(df, x=df.index, y=df.value, title='Monthly anti-diabetic drug sales in Australia from 1992 to 2008.')
+
+
+from statsmodels.tsa.stattools import grangercausalitytests
+df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv', parse_dates=['date'])
+df['month'] = df.date.dt.month
+grangercausalitytests(df[['value', 'month']], maxlag=2)
+
 #https://peerj.com/preprints/27736.pdf
 from pyopenms import *
 seq = AASequence.fromString("DFPIANGER")
