c7274888e1816aaa3558dd27fc6aaf3a7722c190
ani <sharma_animesh@hotmail.com>
2020-08-28 13:36:16 +0200

    sign-rank test over 3 controls in log2 space subtraction

--- wsrt.m
+++ wsrt.m
@@ -1,7 +1,7 @@
 fo='L:\promec\HF\Lars\2020\AUGUST\siri\combined\txt_PHOSTY\proteinGroups.txt';
 testtype=' WSRT';
 data=readtable(fo);
-IDX=1;
+IDX=[1 7 8];%Uniprots, Gene Name, Fasta header
 sdx=108;
 edx=119;
 rep=3;
@@ -9,6 +9,7 @@ log2data=log2(table2array(data(:,sdx:edx))+1);
 log2ctr=log2data(:,[1:ceil((edx-sdx+1)/rep):size(log2data,2)]);
 log2ctr=repelem(log2ctr,1,ceil((edx-sdx+1)/rep));
 log2data=log2data-log2ctr;
+log2data(log2data==0)=NaN;
 hist(log2data)
 idnm=data(:,IDX);
 id=cell(data.Properties.VariableNames(sdx:edx));
@@ -16,15 +17,13 @@ pv=zeros(size(data,1),ceil((edx-sdx+1)/rep)-1);
 ln=zeros(size(data,1),1);
 hdr='Line ';
 pcvt=0;
-for j = 1:ceil((edx-sdx+1)/rep):size(log2data,2)
-    hdr=[hdr strcat(num2str(j),id(j),testtype)]
+for j = 1:size(log2data,2)/rep
+    hdr=[hdr strcat(num2str(j),id(j),testtype)];
     pcvt=pcvt+1;
     for i = 1:size(data,1)
+        i,j%=1 %test with 1%
         %tmparr=log2(table2array(data(i,st:en))+1)-log2(table2array(data(i,st))+1);
-        %tmparr(tmparr==0)=nan;
-        %tmparr=tmparr(2:4)
-        i%=1 %test with 1%
-        tmparr=log2data(i,j+1:j+rep)
+        tmparr=log2data(i,[j:size(log2data,2)/rep:size(log2data,2)])
         ln(i)=i;
          if sum(isnan(tmparr))<length(tmparr)
             pv(i,pcvt)=signrank(tmparr);
@@ -34,4 +33,4 @@ for j = 1:ceil((edx-sdx+1)/rep):size(log2data,2)
     end
 end
 hist(pv)
-xlswrite(strcat(fo,testtype,'pval.xls'),table2cell([[cell(data.Properties.VariableNames(IDX));idnm] [hdr;num2cell([ln pv])] [id;num2cell(log2data)]]))
+writetable(([[cell(data.Properties.VariableNames(IDX));idnm] [hdr;num2cell([ln pv])] [id;num2cell(log2data)]]),strcat(fo,testtype,'pVal.csv'))
