720b6a4a971227d7ee3d53b62b2cd0938e62cf1d
ani <sharma_animesh@hotmail.com>
2020-10-26 14:30:39 +0100

    track and update bioarxiv results with *metabolism(purine, pyrimidine,...)plots mapped using wikipathways-20201010-gmt-Homo_sapiens.gmt, DFT over integer MZ1s, checking LinqInfer

--- RawReadMZMS1.py
+++ RawReadMZMS1.py
@@ -4,21 +4,43 @@ import sys
 from pathlib import Path
 if len(sys.argv)!=2:    sys.exit("REQUIRED: pandas, pathlib; tested with Python 3.8.5\n","USAGE: python RawReadMZMS1.py <path to folder containing profile.intensity0.charge0.MS.txt file(s) like \"Z:/RawRead/\" >")
 pathFiles = Path(sys.argv[1])
-#pathFiles = Path("C:/Users/animeshs/Desktop/RawRead")
-fileName='20150512_BSA_The-PEG-envelope.raw.intensityThreshold1000.errTolDecimalPlace3.MZ1R.txt'
+pathFiles = Path("L:/promec/Animesh/RawRead")
+fileName='20150512_BSA_The-PEG-envelope.raw.intensityThreshold1000.PPM10.errTolDecimalPlace3.Time20201022145302.MZ1R.csv'
 trainList=list(pathFiles.rglob(fileName))
 import pandas as pd
 df=pd.DataFrame()
 for f in trainList:
-    peptideHits=pd.read_csv(f,low_memory=False,sep='\t')
+    peptideHits=pd.read_csv(f,low_memory=False)
     print(f)
     peptideHits['Name']=f
     df=pd.concat([df,peptideHits],sort=False)
 print(df.tail())
 print(df.columns)
 df['sumIntensity'].sum()
-df['MZ'].hist(bins=10000)
-
+#df['MZ1'].hist(bins=1000)
+#df['log2sumIntensity'].hist(bins=10000)
+import numpy as np
+import scipy.spatial.distance
+df=df[df['log2sumIntensity']>20]
+mz1=np.array(df['MZ1']).reshape(-1, 1)
+dataD = np.int32(scipy.spatial.distance.pdist(mz1, 'euclidean'))
+np.min(dataD)
+np.max(dataD)
+#plt.hist(dataD)
+binD=np.zeros(np.max(dataD)+1)
+binD[np.max(dataD)]
+for i in range(dataD.size):
+        binD[dataD[i]]+=1
+import matplotlib.pyplot as plt
+plt.hist(binD,bins=1000)
+binDfft=np.fft.fft(binD)
+binDfft.shape
+plt.plot(binDfft[100:1000])
+binDfftPos=np.where(binDfft>20000000)
+np.size(binDfftPos)
+plt.hist(binDfftPos[10:1000])
+binDfft[np.where(binDfft>9000000)]
+import matplotlib.pyplot as plt
 df=pd.read_('F:/promec/Animesh/hmdb_metabolites/hmdb_metabolites.xml')
 import xml.etree.ElementTree as et
 #https://medium.com/@robertopreste/from-xml-to-pandas-dataframes-9292980b1c1c
--- scratch.cs
+++ scratch.cs
@@ -1,32 +1,25 @@
 ﻿using System;
-
+using LinqInfer;
 namespace cSharpTest
 {
     class scratch
     {
         static void Main(string[] args)
         {
-            double[] tmpArray = { 445.12057,445.11612,445.12502};
-            int errTol = 2;
-            int ppmTol = 10;
-            for (int j = 0; j < tmpArray.LongLength; j++)
-            {
-                double val = tmpArray[j];
-                double ppmValA = val * (1 + ppmTol * 1e-6);
-                double ppmValB = val * (1 - ppmTol * 1e-6);
-                double tmpVal = Math.Round(val, errTol, MidpointRounding.AwayFromZero);
-                double tmpValA = Math.Round(ppmValA, errTol, MidpointRounding.AwayFromZero);
-                double tmpValB = Math.Round(ppmValB, errTol, MidpointRounding.AwayFromZero);
-                /*
-                double tmpVal = Math.Round(val, errTol, MidpointRounding.ToZero);
-                double tmpValA = Math.Round(ppmValA, errTol, MidpointRounding.ToZero);
-                double tmpValB = Math.Round(ppmValB, errTol, MidpointRounding.ToZero);
-                double tmpVal = Math.Round(val, errTol, MidpointRounding.ToEven);
-                double tmpValA = Math.Round(ppmValA, errTol, MidpointRounding.ToEven);
-                double tmpValB = Math.Round(ppmValB, errTol, MidpointRounding.ToEven);
-                */
-                Console.WriteLine("{0}\t{1}\t{2}", tmpVal, tmpValA, tmpValB);
-            }
+            // https://github.com/roberino/linqinfer Sample space
+
+            var sampleSpace = queryableSampleDataset.AsSampleSpace();
+
+            var p = sampleSpace.ProbabilityOfEvent(p => p.Age > 25);
+
+            // Hypotheses
+
+            var die = new[] { 4, 6, 8, 12, 20 };
+            var hypos = die.Select(n => P.Of(n).Is(1).OutOf(die.Length)).AsHypotheses();
+
+            hypos.Update(x => x < 6 ? Fraction.Zero : (1).OutOf(x));
+
+            hypos.ProbabilityOf(4);
         }
     }
 }
