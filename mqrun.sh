88e5bfe12d8dd08f982329f742be13fd2fdc1637
ani <sharma.animesh@gmail.com>
2020-11-01 18:37:36 +0100

    MaxQuant_1.6.8.0 over Ubuntu 18.04.4 LTS with copy of raw file and combined folder per raw file run with GNU parallel

--- mqrun.sh
+++ mqrun.sh
@@ -1,32 +1,23 @@
 #USAGE: bash mqrun.sh
-#make sure mqrun.xml is in the directory where the script is!
-#-n for dryrun, -p <#checkpoint>
-#mono mono /mnt/c/Users/animeshs/MaxQuant_1.6.17.0.zip/MaxQuant/bin/MaxQuantCmd.exe 200925_SINTEF_sample-T2-M3_positionA8.xml
-#change the following paths according to the MaxQuant Installation, directory containing experiment raw files, fasta file and representative parameter file for that version respectively
-MAXQUANTCMD=/mnt/c/Users/animeshs/MaxQuant_1.6.17.0.zip/MaxQuant/bin/MaxQuantCmd.exe
-DATADIR=/mnt/f/SINTEF/mqrun
-FASTAFILE=/mnt/f/SINTEF/mqrun/AP-004_translations.fa
+#make sure mqrun.xml is in the directory where the script is and CHANGE following paths according to the MaxQuant Installation, directory containing experiment raw files, fasta file and representative parameter file for that version respectively
+MAXQUANTCMD=$HOME/MaxQuant_1.6.8.0/MaxQuant/bin/MaxQuantCmd.exe
+CPU=16
+DATADIR=$HOME/HF/BSA/
+FASTAFILE=$HOME/FastaDB/crap.fasta
 PARAMFILE=mqpar.xml
 #leave following empty to include ALL files
 PREFIXRAW=
 SEARCHTEXT=TestFile.raw
 SEARCHTEXT2=SequencesFasta
+SEARCHTEXT3=LocalCombinedFolder
 LDIR=$PWD
-for i in $DATADIR/*.raw
-	do cd $DATADIR
-	echo $i
-	j=$(basename $i)
-	k=${j%%.*}
-	sed "s|$SEARCHTEXT2|$FASTAFILE|" $LDIR/$PARAMFILE > tmp
-	sed "s|$SEARCHTEXT|$DATADIR/$j|"  tmp > $k.xml
-	mono $MAXQUANTCMD $k.xml
-	cp -rf ./combined/txt $k.REP
-	echo $k
-	cd $LDIR
-done
-
-: '
-https://gist.github.com/elrubio/4e7797d7d0d9add96ce82f0472f17908
-https://askubuntu.com/questions/76808/how-do-i-use-variables-in-a-sed-command
-update at sharma.animesh@gmail.com :)
-'
+CURRENTEPOCTIME=`date +%s`
+WRITEDIR=$PARAMFILE.$CURRENTEPOCTIME.results
+mkdir $WRITEDIR
+for i in $DATADIR/*.raw ; do echo $i ; 	j=$(basename $i) ; 	k=${j%%.*} ; mkdir $WRITEDIR/$k ; cp $i $WRITEDIR/$k ; sed "s|$SEARCHTEXT2|$FASTAFILE|" $LDIR/$PARAMFILE > $WRITEDIR/$k/$PARAMFILE.tmp1 ; 	sed "s|$SEARCHTEXT|$LDIR/$WRITEDIR/$k/$j|"  $WRITEDIR/$k/$PARAMFILE.tmp1 > $WRITEDIR/$k/$PARAMFILE.tmp2 ; sed "s|$SEARCHTEXT3|$LDIR/$WRITEDIR/$k|"  $WRITEDIR/$k/$PARAMFILE.tmp2 > $WRITEDIR/$k/$k.xml ; rm $WRITEDIR/$k/$PARAMFILE.tmp*  ;done
+#mono $MAXQUANTCMD $k.xml ; cp -rf ./combined/txt $k.REP ; echo $k ; cd $LDIR 
+find $WRITEDIR -name "*.xml" | parallel -j $CPU "mono $MAXQUANTCMD {}"
+#perl -pe 's/\r$//' < mqrun.sh  > tmp
+#mv tmp  mqrun.sh
+#mono $HOME/data/NORSTORE_OSL_DISK/NS9036K/promec/MaxQuant_1.6.8.0/MaxQuant/bin/MaxQuantCmd.exe -n $HOME/promec/Qexactive/Mirta/QExactive/Imen_Belhaj/RawData/out1678.xml
+#-n for dryrun, -p <#checkpoint>
