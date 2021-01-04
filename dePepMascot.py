e1c9fed22aaf9ffa2b4224935f4a3313ecc2ace9
ani <sharma_animesh@hotmail.com>
2021-01-04 16:24:59 +0100

    mass tolerant search/parse

--- dePepMascot.py
+++ dePepMascot.py
@@ -1,39 +1,31 @@
 import sys
 from pathlib import Path
-if len(sys.argv)!=2:    sys.exit("USAGE: python dePep.py <path to folder containing mascot exported csv file(s)>, for example,\npython dePepMascot.py L:/promec/HF/Lars/2019/november/ung\n")
+if len(sys.argv)!=2:    sys.exit("USAGE: python dePepMascot.py <path to peptides containing mascot exported file>, \n e.g.,\npython dePepMascot.py L:/promec/Animesh/Raw/New Study/20150512_BSA_The-PEG-envelope (2)-(8)_PeptideGroups.txt\n")
 pathFiles = Path(sys.argv[1])
-#pathFiles = Path("L:/promec/HF/Lars/2019/november/ung")
-fileName='*.csv'
-trainList=list(pathFiles.rglob(fileName))
-
+#pathFiles = Path("L:/promec/HF/Lars/2020/oktober/KATHLEENPHOSTOT/201105_kath_phostot_14B_PeptideGroups.txt")
 import pandas as pd
-df=pd.DataFrame()
-for f in trainList:
-    peptideHits=pd.read_csv(f,low_memory=False,skiprows=66,doublequote=True)#,sep='\t')
-    print(f)
-    peptideHits['Name']=f
-    peptideHits.fillna(method='ffill',inplace=True)
-    df=pd.concat([df,peptideHits],sort=False)
-print(df.head())
+df=pd.read_csv(pathFiles,low_memory=False,doublequote=True,sep='\t')
+import numpy as np
 print(df.columns)
-fileName=fileName.replace("*.","")
-writecsv=pathFiles/(fileName+"combined.csv")
-df.to_csv(writecsv)
-print(df.columns.get_loc("prot_acc"))
-
-dfDP=df.loc[:, df.columns.str.startswith('prot')|df.columns.str.startswith('pep')]
-#dfDP=dfDP[dfDP['DP Proteins'].notnull()]
-#dfDP=dfDP.rename(columns = lambda x : str(x)[3:])
-#dfDP[dfDP['Modification']=="Phosphorylation"]
-#dfDP=dfDP[dfDP['Modification'].str.contains('hosphor')==True]
-writeDPcsv=pathFiles/(fileName+"DP.csv")
-dfDP.to_csv(writeDPcsv)
-print("writing output to ... ")
-print(writeDPcsv)
-
-dfDPcnt=dfDP['prot_acc'].value_counts()
-print(dfDPcnt)
-
-writeDPpng=pathFiles/(fileName+"DP.png")
-dfDPcnt[dfDPcnt>0].plot(kind='pie').figure.savefig(writeDPpng,dpi=100,bbox_inches = "tight")
-print(writeDPpng)
+dfG=df.groupby(['Modifications','Delta M in ppm by Search Engine A20 Mascot'],as_index=False).agg({'Modifications':pd.Series.mode,'Delta M in ppm by Search Engine A20 Mascot':np.mean})
+dfG["Delta M in ppm by Search Engine A20 Mascot"].hist()
+dfG["Modifications"]
+dfGm0ds=dfG["Modifications"].str.split('];', expand=True)
+dfGm0ds=dfGm0ds.replace('^ ','',regex=True)
+dfGm0ds=dfGm0ds.fillna(value=' ')
+#dfGm0ds=dfGm0ds.applymap(lambda x: x.replace('^ ','',regex=True))
+dfGm0ds=dfGm0ds.applymap(lambda x: x.split(' '))
+writecsv=pathFiles.with_suffix('.group.csv')
+dfGm0ds.to_csv(writecsv)
+dfGreps=dfGm0ds.applymap(lambda x: x[0])
+dfGreps=dfGreps.applymap(lambda x: x.split('x'))
+dfGreps=dfGreps.applymap(lambda x: x[0])
+dfGtype=dfGm0ds.applymap(lambda x: x[1])
+dfGtype[0].str.repeat(dfGreps[0].astype(int))
+writecsv=pathFiles.with_suffix('.group.counts.csv')
+dfGtypeCont=dfGtype[0].str.repeat(dfGreps[0].astype(int)).value_counts()
+print(dfGtypeCont)
+dfGtypeCont.to_csv(writecsv)
+plotcsv=pathFiles.with_suffix('.group.counts.plot.svg')
+dfGtypeCont[dfGtypeCont>0].plot(kind='pie').figure.savefig(plotcsv,dpi=100,bbox_inches = "tight")
+print("wrtten",pathFiles,"groups*")
