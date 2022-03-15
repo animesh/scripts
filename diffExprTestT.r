921574c68de2462639ea97308a3dde321fe09fa9
ani <sharma_animesh@hotmail.com>
2022-03-15 18:02:19 +0100

    UCHL1 control/mutated/knock-out MN/MNP/iPSC t-tests, combine t-test report for IDH1 MUT/WT-controls, explore surivval curve, decrease RAM for WSL

--- diffExprTestT.r
+++ diffExprTestT.r
@@ -1,24 +1,25 @@
-#cf. F:\GD\OneDrive\Dokumenter\GitHub\scripts\proteinGroupsTtest.r
-#Rscript C:\Users\animeshs\R\bin\Rscript.exe
+print("USAGE:<path to>Rscript diffExprTestT.r <complete path to directory containing proteinGroups.txt AND Groups.txt files> <name of group column in Groups.txt annotating data/rows to be used for analysis> <name of column in Groups.txt marking data NOT to be considered in analysis>")
 args = commandArgs(trailingOnly=TRUE)
+print(paste("supplied argument(s):", length(args)))
 print(args)
-#git checkout 6bc63d134527cf6972218c3d4d39f881ea883753 diffExprTestT.r
-if (length(args) != 2) {stop("\n\nNeeds the full path of the directory containing BOTH proteinGroups.txt from MaxQuant & Groups.txt files followed by the name of GROUP column in Groups.txt file whch will be used for the t-test, for example
-\"c:/Users/animeshs/R/bin/Rscript.exe diffExprTestT.r L:/promec/TIMSTOF/LARS/2022/februar/Sigrid/combined/txt/ Bio\"
-                             ", call.=FALSE)}
+if (length(args) != 3) {stop("\n\nNeeds THREE arguments, the full path of the directory containing BOTH proteinGroups.txt AND Groups.txt files followed by the name of GROUP-to-compare and data-to-REMOVE columns in Groups.txt file, for example:
+
+c:/Users/animeshs/R/bin/Rscript.exe diffExprTestT.r L:/promec/Qexactive/Mirta/20220210_UCHL1_iPSC-MNP-MN/RawData/combined/txt/ Bio Rem
+", call.=FALSE)}
 #setup####
-#install.packages("ggplot2", repos = "https://cloud.r-project.org/")
-#install.packages("svglite", repos = "https://cloud.r-project.org/")
-#install.packages("writexl", repos = "https://cloud.r-project.org/")
-#install.packages("BiocManager", repos = "https://cloud.r-project.org/")
-#BiocManager::install(version = "3.14")
-#BiocManager::install("limma")
-#BiocManager::install("pheatmap")
-#BiocManager::install("UniprotR")
+#install.packages("ggplot2")
+#install.packages("svglite")
+#install.packages("limma")
+#install.packages("writexl")
+#install.packages("pheatmap")
+#install.packages("devtools")
+#devtools::install_github("jdstorey/qvalue")
 inpD <- args[1]
-#inpD <-"L:/promec/Elite/LARS/2015/january/Ishita/combined/txt/"
+#inpD <-"L:/promec/Qexactive/Mirta/20220210_UCHL1_iPSC-MNP-MN/RawData/txt/"
 lGroup <- args[2]
 #lGroup<-"Bio"
+rGroup <- args[3]
+#lGroup<-"Remove"
 inpF<-paste0(inpD,"proteinGroups.txt")
 inpL<-paste0(inpD,"Groups.txt")
 selection<-"LFQ.intensity."
@@ -38,7 +39,9 @@ data <- read.table(inpF,stringsAsFactors = FALSE, header = TRUE, quote = "", com
 row.names(data)<-paste(row.names(data),data$Fasta.headers,data$Protein.IDs,data$Protein.names,data$Gene.names,data$Score,data$Peptide.counts..unique.,sep=";;")
 summary(data)
 dim(data)
-hist(as.matrix(log2(data[,grep("Intensity",colnames(data))])))
+log2Int<-as.matrix(log2(data[,grep("Intensity",colnames(data))]))
+log2Int[log2Int==-Inf]=NA
+hist(log2Int,main=paste("Mean:",mean(log2Int,na.rm=T),"SD:",sd(log2Int,na.rm=T)),breaks=round(max(log2Int,na.rm=T)),xlim=range(min(log2Int,na.rm=T),max(log2Int,na.rm=T)))
 summary(log2(data[,grep("Intensity",colnames(data))]))
 LFQ<-as.matrix(data[,grep(selection,colnames(data))])
 #protNum<-1:ncol(LFQ)
@@ -50,7 +53,7 @@ log2LFQ<-log2(LFQ)
 log2LFQ[log2LFQ==-Inf]=NA
 log2LFQ[log2LFQ==0]=NA
 summary(log2LFQ)
-hist(log2LFQ)
+hist(log2LFQ,main=paste("Mean:",mean(log2LFQ,na.rm=T),"SD:",sd(log2LFQ,na.rm=T)),breaks=round(max(log2Int,na.rm=T)),xlim=range(min(log2Int,na.rm=T),max(log2Int,na.rm=T)))
 rowName<-paste(sapply(strsplit(paste(sapply(strsplit(data$Fasta.headers, "|",fixed=T), "[", 2)), "-"), "[", 1))
 writexl::write_xlsx(as.data.frame(cbind(rowName,log2LFQ,rownames(data))),paste0(inpD,"log2LFQ.xlsx"))
 data$geneName<-paste(sapply(strsplit(paste(sapply(strsplit(data$Gene.names, ";",fixed=T), "[", 1)), " "), "[", 1))
@@ -60,14 +63,14 @@ data[data$geneName=="NA","geneName"]=data[data$geneName=="NA","uniprotID"]
 label<-read.table(inpL,header=T,sep="\t",row.names=1)#, colClasses=c(rep("factor",3)))
 rownames(label)=sub(selection,"",rownames(label))
 label["pair2test"]<-label[lGroup]
-#label["pair2test"]<-label["Bio"]
+label["removed"]<-label[rGroup]
 print(label)
 #corHC####
 scale=3
 log2LFQimp<-matrix(rnorm(dim(log2LFQ)[1]*dim(log2LFQ)[2],mean=mean(log2LFQ,na.rm = T)-scale,sd=sd(log2LFQ,na.rm = T)/(scale)), dim(log2LFQ)[1],dim(log2LFQ)[2])
 log2LFQimp[log2LFQimp<0]<-0
-bk1 <- c(seq(-1,-0.01,by=0.01))
-bk2 <- c(seq(0.01,1,by=0.01))
+bk1 <- c(seq(-3,-0.01,by=0.01))
+bk2 <- c(seq(0.01,3,by=0.01))
 bk <- c(bk1,bk2)  #combine the break limits for purpose of graphing
 my_palette <- c(colorRampPalette(colors = c("darkblue", "white"))(n = length(bk1)-1),"gray", "gray",c(colorRampPalette(colors = c("white","darkred"))(n = length(bk2)-1)))
 colnames(log2LFQimp)<-colnames(log2LFQ)
@@ -75,13 +78,13 @@ log2LFQimpCorr<-cor(log2LFQ,use="pairwise.complete.obs",method="spearman")
 colnames(log2LFQimpCorr)<-colnames(log2LFQ)
 rownames(log2LFQimpCorr)<-colnames(log2LFQ)
 svgPHC<-pheatmap::pheatmap(log2LFQimpCorr,clustering_distance_rows = "euclidean",clustering_distance_cols = "euclidean",fontsize_row=8,cluster_cols=T,cluster_rows=T,fontsize_col  = 8)
-ggplot2::ggsave(paste0(inpF,selection,selThr,selThrFC,cvThr,lGroup,"HeatMap.svg"), svgPHC)
 #test####
 testT <- function(log2LFQ,sel1,sel2,cvThr){
-  #sel1<-"MINE"
-  #sel2<-"WT"
-  #log2LFQ<-medianLog2LFQ
-  log2LFQ<-sapply(log2LFQ, as.numeric)
+  #sel1<-"MNPHCKO"#"MNPSC"
+  #sel2<-"MNPHC"
+  #log2LFQ<-log2LFQsel#[,gsub("-",".",rownames(label[label$Remove!="Y",]))]
+  #log2LFQ<-sapply(log2LFQ, as.numeric)
+  #colnames(log2LFQ)
   d1<-log2LFQ[,gsub("-",".",rownames(label[label$pair2test==sel1,]))]
   d2<-log2LFQ[,gsub("-",".",rownames(label[label$pair2test==sel2,]))]
   dataSellog2grpTtest<-as.matrix(cbind(d1,d2))
@@ -160,141 +163,21 @@ testT <- function(log2LFQ,sel1,sel2,cvThr){
     ggplot2::ggsave(paste0(inpF,selection,sCol,eCol,comp,selThr,selThrFC,cvThr,lGroup,"VolcanoTest.svg"), p)
     print(p)
     return(ttest.results.return)
-    }
+  }
 }
-#medianMINEandWT####
+#compare####
 colnames(log2LFQ)
-table(label$Bio)
-medianLog2LFQ <- data.frame(matrix(ncol=length(names(table(label$Bio))),nrow=nrow(log2LFQ)))
-colnames(medianLog2LFQ) <- names(table(label$Bio))
-rownames(medianLog2LFQ)<-rownames(log2LFQ)
-for(i in names(table(label$Bio))){
-  print(i)
-  log2LFQvals<-log2LFQ[,gsub("-",".",rownames(label[label$Bio==i,]))]
-  print(summary(log2LFQvals))
-  medianLog2LFQ[i]<-apply(log2LFQvals,1, function(x) median(x,na.rm=T))
-  print(summary(medianLog2LFQ[i]))
-}
-#GO####
-selGO <- function(uniProt,term) {
-  #uniProt<-ttMINE2WT$Uniprot "Q9LF24"#orgID
-  #term<-"GO"
-  GeneOntologyObj <- UniprotR::GetProteinGOInfo(uniProt)
-  GeneOntologyObj$ID <- rownames(GeneOntologyObj)
-  write.csv(GeneOntologyObj,paste0(inpF,term,"GeneOntologyObj.csv"))
-  length(grep(term,GeneOntologyObj$Gene.ontology..cellular.component.,ignore.case=T))
-  GeneOntologyObj$term <- apply(GeneOntologyObj, 1, function(x)as.integer(any(grep(term,x,ignore.case=T))))
-  sum(GeneOntologyObj$term)
-  return(GeneOntologyObj)
-}
-#testMINE2WT####
-label <- data.frame(matrix(ncol=1,nrow=length(colnames(medianLog2LFQ))))
-label$pair2test<-sapply(strsplit(colnames(medianLog2LFQ), "_",fixed=T), "[", 1)
-rownames(label)<-colnames(medianLog2LFQ)
-rownames(label)
+log2LFQsel=log2LFQ[,gsub("-",".",rownames(label[label$removed!="Y",]))]
+colnames(log2LFQsel)
+dim(log2LFQsel)
+label=label[label$removed!="Y",]
 table(label$pair2test)
-rownames(label[label$pair2test=="MINE",])
-rownames(label[label$pair2test=="WT",])
-ttMINE2WT=testT(medianLog2LFQ,"MINE","WT",cvThr)
-GeneOntologyObj=selGO(ttMINE2WT$Uniprot,"GO")
-GeneOntologyObj$Uniprot<-rownames(GeneOntologyObj)
-resultsGO<-merge(ttMINE2WT,GeneOntologyObj,by="Uniprot")
-writexl::write_xlsx(resultsGO,paste0(inpF,selection,selThr,selThrFC,cvThr,lGroup,"tTestBHGO.xlsx"))
-write.csv(resultsGO,paste0(inpF,selection,selThr,selThrFC,cvThr,lGroup,"tTestBHGO.csv"))
-#dist####
-#proteinGroups.txtLFQ.intensity.0.050.50.05BiotTestBHGO.csv
-#resultsGO<-read.csv(paste0(inpF,selection,selThr,selThrFC,cvThr,lGroup,"tTestBHGO.csv"),row.names=1)
-row.names(resultsGO)<-paste0(row.names(resultsGO),resultsGO$Uniprot)
-#log2LFQ<-sigList[,c(grep("MINE|WT",colnames(sigList)))]
-log2LFQ<-resultsGO[,c(grep("MINE|WT",colnames(resultsGO)))]
-row.names(log2LFQ)<-row.names(resultsGO)
-#log2LFQimpCorr<-cor(t(log2LFQ),use="pairwise.complete.obs",method="spearman")
-#hist(log2LFQimpCorr)
-log2LFQimpCorP<-cor(t(log2LFQ),use="pairwise.complete.obs",method="pearson")
-hist(log2LFQimpCorP)
-dsubCor<-as.dist(log2LFQimpCorP)
-#dsubCor[is.na(dsubCor)]<-0
-hist(dsubCor)
-#https://stackoverflow.com/questions/5813156/convert-and-save-distance-matrix-to-a-specific-format/5815379#5815379
-dsubCorM <- data.frame(t(combn(rownames(log2LFQ),2)), as.numeric(dsubCor))
-write.csv(data.frame(dsubCorM),paste0(inpD,"dsubCorM.csv"))
-names(dsubCorM) <- c("P1", "P2", "dist")
-dsubCorMna<-dsubCorM[!is.na(dsubCorM$dist),]
-hist(dsubCorMna$dist)
-write.csv(data.frame(dsubCorMna),paste0(inpD,"dsubCorMna.csv"))
-corThr<-0
-#dsubCorSel<-dsubCorMna[abs(dsubCorMna$dist)>corThr,]
-dsubCorSel<-dsubCorMna[dsubCorMna$dist>corThr,]
-dsubCorSel$SOURCE<-paste(sapply(strsplit(paste(sapply(strsplit(dsubCorSel$P1, ";",fixed=T), "[", 1)), "-"), "[", 1))
-dsubCorSel$TARGET<-paste(sapply(strsplit(paste(sapply(strsplit(dsubCorSel$P2, ";",fixed=T), "[", 1)), "-"), "[", 1))
-write.csv(data.frame(dsubCorSel),paste0(inpD,"dsubCorSel",corThr,"p.csv"),quote = F)
-#sigList####
-sigList<-resultsGO
-sigList[is.na(sigList)]<-(Inf)
-log2Thr<-0.5
-sigList<-sigList[sigList$CorrectedPValueBH<0.1&abs(sigList$Log2MedianChange)>log2Thr,]
-hist(sigList$Log2MedianChange)
-hist(sigList$CorrectedPValueBH)
-plot(sigList$Log2MedianChange,-log10(sigList$CorrectedPValueBH))
-sigListUP<-sigList[sigList$CorrectedPValueBH<0.1&sigList$Log2MedianChange>log2Thr,]
-write.csv(data.frame(sigListUP),paste0(inpD,"sigListUP",log2Thr,"fc.csv"),quote = F)
-uniList<-unlist(strsplit(unlist(strsplit(unlist(strsplit(unlist(strsplit(sigListUP$RowGeneUniProtScorePeps, split = ";")),split = "|",fixed=T)),split = "=",fixed=T)),split = " ",fixed=T))
-uniList<-uniList[nchar(uniList)>5&nchar(uniList)<15]
-uniList<-uniList[grepl('^[A-Z]', uniList)]
-write.csv(uniList,paste0(inpD,"sigListUP",log2Thr,"fcUnlist.csv"),quote = F)
-sigListDN<-sigList[sigList$CorrectedPValueBH<0.1&sigList$Log2MedianChange<(-log2Thr),]
-write.csv(data.frame(sigListDN),paste0(inpD,"sigListDN",log2Thr,"fc.csv"),quote = F)
-uniList<-unlist(strsplit(unlist(strsplit(unlist(strsplit(unlist(strsplit(sigListDN$RowGeneUniProtScorePeps, split = ";")),split = "|",fixed=T)),split = "=",fixed=T)),split = " ",fixed=T))
-uniList<-uniList[nchar(uniList)>5&nchar(uniList)<15]
-uniList<-uniList[grepl('^[A-Z]', uniList)]
-write.csv(uniList,paste0(inpD,"sigListDN",log2Thr,"fcUnlist.csv"),quote = F)
-#fisher####
-sigListGO <- data.frame(do.call(rbind, Map(cbind, strsplit(sigList$Gene.ontology..GO., "; "), sigList$RowGeneUniProtScorePeps)))
-colnames(sigListGO) <- c("GO","ID")
-sigListGOtab=as.data.frame(table(sigListGO$GO))
-#sigListGO=sigListGO[!is.infinite(sigListGO)]
-fulListGO <- data.frame(do.call(rbind, Map(cbind, strsplit(resultsGO$Gene.ontology..GO., "; "), resultsGO$RowGeneUniProtScorePeps)))
-colnames(fulListGO) <- c("GO","ID")
-fulListGO=fulListGO[!is.na(fulListGO$GO),]
-fulListGOtab=as.data.frame(table(fulListGO$GO))
-goTab<-merge(fulListGOtab,sigListGOtab,by='Var1')
-plot(log2(goTab$Freq.x),log2(goTab$Freq.y))
-hist(log2(goTab$Freq.x))
-#thioglucosidase https://www.wolframalpha.com/input?i=%28Binomial%5B3787%2C7%5D*Binomial%5B320%2C15%5D%29%2FBinomial%5B4107%2C22%5D
-#(15/320)/(22/4107)#enrichment
-#(choose(3787,7)*choose(320,15))/choose(4107,22)#(choose(3787,3780)*choose(320,305))/choose(4107,4085)
-#fisher.test(rbind(c(7,3780),c(15,305)))$p.value#fisher.test(rbind(c(3460,3482),c(305,3780)))$p.value
-#(choose(3787,7)*choose(320,15))/choose(4107,22)+(choose(3787,6)*choose(320,16))/choose(4107,22)+(choose(3787,5)*choose(320,17))/choose(4107,22)+(choose(3787,4)*choose(320,18))/choose(4107,22)+(choose(3787,3)*choose(320,19))/choose(4107,22)+(choose(3787,2)*choose(320,20))/choose(4107,22)+(choose(3787,1)*choose(320,21))/choose(4107,22)+(choose(3787,0)*choose(320,22))/choose(4107,22)
-#nitrile
-#(choose(3787,6)*choose(320,5))/choose(4107,11)
-#fisher.test(rbind(c(6,3781),c(5,305)),alternative="less")$p.value
-goTab["lo-enrichment"]=(goTab$Freq.y/nrow(sigList))/(goTab$Freq.x/nrow(resultsGO))
-goTab["p-binomial"]=(choose(nrow(sigList),goTab$Freq.y)*choose((nrow(resultsGO)-nrow(sigList)),goTab$Freq.x-goTab$Freq.y))/choose(nrow(resultsGO),(goTab$Freq.x))
-hist(as.matrix(goTab["p-binomial"]))
-goTab["p-binomBH"]=sapply(goTab["p-binomial"],p.adjust)
-hist(as.matrix(goTab["p-binomBH"]))
-#goTab["p-fisher"]=fisher.test(rbind(c(goTab$Freq.x-goTab$Freq.y,nrow(resultsGO)-(nrow(sigList)+goTab$Freq.y+goTab$Freq.x)),c(goTab$Freq.y,nrow(sigList)-goTab$Freq.y)))$p.value
-pvFE=apply(
-  goTab, 1, function(x)
-    if(sum(is.na(x[c(2:3)]))==0){fisher.test(rbind(c(as.numeric(x[2])-as.numeric(x[3]),nrow(resultsGO)-(nrow(sigList)+as.numeric(x[2])+as.numeric(x[3]))),c(as.numeric(x[3]),nrow(sigList)-as.numeric(x[3]))))$p.value}
-    else{0}
-)
-goTab["p-fisher"]=pvFE
-pvFEBH=p.adjust(pvFE,method="BH")
-#fisher(choose(nrow(sigList),goTab$Freq.y)*choose((nrow(resultsGO)-nrow(sigList)),goTab$Freq.x-goTab$Freq.y))/choose(nrow(resultsGO),(goTab$Freq.x))
-goTab["p-fisherBH"]=pvFEBH
-plot(goTab)#$`lo-enrichment,goTab$`p-binomial`)
-sigListGOagg=aggregate(sigListGO["ID"], by=sigListGO["GO"],paste)
-goTabID<-merge(goTab,sigListGOagg,by.x="Var1",by.y='GO')
-goTabID$ID <- paste0(sapply(goTabID$ID,as.character))
-write.csv(goTabID,paste0(inpF,"FisherGOtabID.csv"))
-goTabID=goTabID[order(goTabID["p-binomial"]),]
-writexl::write_xlsx(goTabID,paste0(inpF,"FisherGOtabID.xlsx"))
-#checkFE####
-#perGO<-read.csv(paste0(inpF,"FisherGOall.txt"),sep="\t")
-#compareGO<-merge(goTab,perGO,by.x="Var1",by.y='C..Category.value',all.x=T)
-#sum(compareGO["p-binomial"]-compareGO$N..P.value,na.rm=T)
-#pvDiff=compareGO["p-binomial"]-compareGO$N..P.value
-#pvDiff[is.na(pvDiff)]=0
-#pvDiff=sapply(pvDiff,as.numeric)
-#hist(pvDiff)
+for(i in rownames(table(label$pair2test))){
+  for(j in rownames(table(label$pair2test))){
+    if(i!=j){
+      print(paste(i,j))
+      ttPair=testT(log2LFQsel,i,j,cvThr)
+      #assign(paste0(i,j),ttPair)
+    }
+  }
+}
---
+++ proteinGroupsTtestCombine.py
@@ -0,0 +1,112 @@
+#python proteinGroupsTtestCombine.py L:\promec\TIMSTOF\LARS\2022\februar\Sigrid\combined\txt\reports\CTRL
+import sys
+#!pip3 install pathlib --user
+from pathlib import Path
+if len(sys.argv)!=2: sys.exit("\n\nREQUIRED: pandas, pathlib; tested with Python 3.9 \n\nUSAGE: python proteinGroupsTtestCombine.py <path to folder containing Benjamini-Hochberg corrected csv/*BH.csv* file(s) like \"L:\promec\TIMSTOF\LARS\2022\februar\Sigrid\combined\txt\reports\CTRL\" >\n\nExample\n\npython proteinGroupsTtestCombine.py L:\promec\TIMSTOF\LARS\2022\februar\Sigrid\combined\txt\reports\CTRL")
+pathFiles = Path(sys.argv[1])
+#pathFiles=Path("L:/promec/TIMSTOF/LARS/2022/februar/Sigrid/combined/txt/reports/CTRL")
+fileName='*BH.csv'
+trainList=list(pathFiles.rglob(fileName))
+#trainList=list([Path('L:/promec/TIMSTOF/LARS/2022/februar/Sigrid/combined/txt/reports/CTRL/proteinGroups.txtLFQ.intensity.16MUT CtrlWT Ctrl0.050.50.05BiotTestBH.csv'),Path('L:/promec/TIMSTOF/LARS/2021/Desember/211207_Nilu/combined/txt/proteinGroups.txt'),Path('L:/promec/TIMSTOF/LARS/2022/februar/Sigrid/combined/txt/reports/CTRL/proteinGroups.txtLFQ.intensity.16MUTctrlWTctrl0.050.50.05grouptTestBH.csv')])
+#!pip3 install pandas --user
+import pandas as pd
+#df = pd.concat(map(pd.read_table, trainList))
+#df.to_csv(pathFiles.with_suffix('.combinedT.txt'),sep="\t")#,rownames=FALSE)
+#f=trainList[1]
+dfC=pd.DataFrame()
+dfB=pd.DataFrame()
+i=0
+for f in trainList:
+    i=i+1
+    if Path(f).stat().st_size > 0:
+        proteinHits=pd.read_csv(f)
+        print(i,f.parts)
+        proteinHits.rename({'RowGeneUniProtScorePeps':'ID'},inplace=True,axis='columns')
+        proteinHitsC=proteinHits.ID.str.split(';;', expand=True).set_index(proteinHits.Log2MedianChange).stack().reset_index(level=0, name='ID')
+        proteinHitsC=proteinHitsC[proteinHitsC.index==2]
+        proteinHitsC=proteinHitsC.ID.str.split(';', expand=True).set_index(proteinHits.Log2MedianChange).stack().reset_index(level=0, name='ID')
+        proteinHitsC=proteinHitsC[proteinHitsC['ID']!=""]
+        proteinHitsB=proteinHits.ID.str.split(';;', expand=True).set_index(proteinHits.CorrectedPValueBH).stack().reset_index(level=0, name='ID')
+        proteinHitsB=proteinHitsB[proteinHitsB.index==2]
+        proteinHitsB=proteinHitsB.ID.str.split(';', expand=True).set_index(proteinHits.CorrectedPValueBH).stack().reset_index(level=0, name='ID')
+        proteinHitsB=proteinHitsB[proteinHitsB['ID']!=""]
+        proteinHitsC['Name']=f.parts[-1]+f.parts[4]+f.parts[5]+str(i)
+        proteinHitsB['Name']=f.parts[-1]+f.parts[4]+f.parts[5]+str(i)
+        dfC=pd.concat([dfC,proteinHitsC],sort=False)
+        dfB=pd.concat([dfB,proteinHitsB],sort=False)
+print(dfC.columns)
+print(dfB.columns)
+dfC=dfC.pivot(index='ID', columns='Name', values='Log2MedianChange')
+dfC.to_csv(pathFiles.with_suffix('.Log2MedianChange.combined.csv'))
+dfB=dfB.pivot(index='ID', columns='Name', values='CorrectedPValueBH')
+import numpy as np
+dfBmLog10=dfB+sys.float_info.min
+dfBmLog10=dfBmLog10.fillna(1)
+dfBmLog10=-1*np.log10(dfBmLog10)
+dfBmLog10.to_csv(pathFiles.with_suffix('.dfBmLog10CorrectedPValueBH.combined.csv'))#df.to_csv(pathFiles.with_suffix('.combined.txt'),sep="\")#,rownames=FALSE)
+#writeDPpng=pathFiles/(fileName+"Score.png")
+#df.plot(kind='hist').figure.savefig(writeDPpng.absolute(),dpi=100,bbox_inches = "tight")
+#print("Histogram of Score in",writeDPpng)
+dfB['SumB']=dfB.sum(axis=1)
+dfC['SumC']=dfC.sum(axis=1)
+dfB=dfB.sort_values('SumB',ascending=False)
+dfC=dfC.sort_values('SumC',ascending=False)
+dfI.to_csv(pathFiles/(fileName+"Combo.intensity.csv"))#.with_suffix('.combo.csv'))
+histScores=pathFiles/(fileName+"HistInt.svg")
+import numpy as np
+log2dfI=dfI.fillna(0)
+log2dfI=np.log2(log2dfI+1)
+log2dfI=log2dfI.drop('SumI',axis=1)
+log2dfI.plot(kind='hist',alpha=0.5,bins=100).figure.savefig(histScores,dpi=100,bbox_inches = "tight")
+log2dfI.columns
+print("Scatter of First 2 Intensities in",pathFiles/(fileName+"Scatter.svg"))
+import seaborn as sns
+sns.jointplot(y=log2dfI.iloc[:,0],x=log2dfI.iloc[:,1], ).figure.savefig(pathFiles/(fileName+"Scatter.svg"),dpi=100,bbox_inches = "tight")#,kind="reg")
+#sns.jointplot(data=log2dfI,y='Oslo220119_ELise_rerunjanuar2', x='combined220119_ELise_rerunjanuar1').figure.savefig(pathFiles/(fileName+"Scatter.svg"),dpi=100,bbox_inches = "tight")#,kind="reg")
+#sns.jointplot(data=log2dfI[:,1:2])#,kind="reg")
+dfB.to_csv(pathFiles/(fileName+"Combo.ibaq.csv"))#.with_suffix('.combo.csv'))
+print("Histogram of iBAQ in",pathFiles/(fileName+"iBAQhist.svg"))
+dfC.plot(kind='hist',alpha=0.5,bins=100).figure.savefig(pathFiles/(fileName+"iBAQhist.svg"),dpi=100,bbox_inches = "tight")
+dfC.to_csv(pathFiles/(fileName+"Combo.score.csv"))#.with_suffix('.combo.csv'))
+print("Histogram of Scores in",pathFiles/(fileName+"ScoresHist.svg"))
+dfC.plot(kind='hist',alpha=0.5,bins=100).figure.savefig(pathFiles/(fileName+"ScoresHist.svg"),dpi=100,bbox_inches = "tight")
+#dfID=df.assign(ID=df.ID.str.split(';')).explode('ID')
+dfLFQ=dfC
+dfLFQ=dfI
+i=0
+for f in trainList:
+    i=i+1
+    if Path(f).stat().st_size > 0:
+        print(i,f.parts[-3],f.parts[-4],f.parts[-5])
+        proteinHits=pd.read_csv(f,low_memory=False,sep='\t')
+        proteinHitsLFQ=proteinHits.assign(ID=proteinHits['Protein IDs'].str.split(';')).explode('ID')
+        proteinHitsLFQ.index=proteinHitsLFQ['ID']
+        proteinHitsLFQ=proteinHitsLFQ.add_suffix(f.parts[-3]+f.parts[-4]+f.parts[-5]+'F'+str(i))
+        dfLFQ=pd.concat([dfLFQ,proteinHitsLFQ],axis=1)
+print(dfLFQ.columns)
+#dfLFQ['211207_NiluDesember2'].hist()
+#(dfLFQ['211221_NiluDesember3']-dfLFQ['Score211221_NiluDesemberF3']).hist()
+dfLFQ.to_csv(pathFiles/(fileName+"Combo.csv"))
+dfS=dfLFQ.filter(like='Peptide sequence', axis=1)
+#dfS=dfLFQ[:,[dfLFQ.filter(like='Peptide sequence', axis=1)]].apply(lambda x: ''.join(x), axis=1)
+dfLFQPeptides=dfS[dfS.columns].apply(lambda x:','.join(x.dropna().astype(str)),axis=1)
+#dfLFQPeptides['C9J1R6']#EFPDLGAHCSEPSCQR
+#dfLFQPeptides.filter(like='HTSALCNSCR')#EFPDLGAHCSEPSCQR,HPLDHDCSGEGHPTSR;HRHPLDHDCSGEGHPTSR,HPLDHDCSGEGHPTSR;HRHPLDHDCSGEGHPTSR
+dfLFQPeptides.to_csv(pathFiles/(fileName+"dfLFQPeptides.csv"))
+dfLFQvals=dfLFQ.filter(like='LFQ', axis=1)
+#dfLFQvals=dfLFQ.filter(like='Intensity', axis=1)
+dfLFQvals.to_csv(pathFiles/(fileName+"dfLFQvals.csv"))
+dfLFQvalsSeqs=pd.concat([dfLFQvals,dfLFQPeptides],axis=1)
+dfLFQvalsSeqs.to_csv(pathFiles/(fileName+"dfLFQvalsSeqs.csv"))
+dfLFQvalsSeqsMedian=dfLFQvalsSeqs.groupby(0).median()
+dfLFQid=dfLFQPeptides.reset_index()
+dfLFQidC=dfLFQid.groupby(0).agg({'ID': ';'.join})
+dfLFQvalsSeqsUni=pd.concat([dfLFQidC,dfLFQvalsSeqsMedian],axis=1)
+dfLFQvalsSeqsUni.to_csv(pathFiles/(fileName+"dfLFQvalsSeqsUni.csv"))
+log2dfLFQvalsSeqsUni=dfLFQvalsSeqsUni.fillna(0)
+log2dfLFQvalsSeqsUni.index=log2dfLFQvalsSeqsUni['ID']
+log2dfLFQvalsSeqsUni=log2dfLFQvalsSeqsUni.drop(['ID'], axis=1)
+log2dfLFQvalsSeqsUni=np.log2(log2dfLFQvalsSeqsUni+1)
+#log2dfLFQvalsSeqsUni.hist()#.figure.savefig(pathFiles/(fileName+"log2dfLFQvalsSeqsUni.hist.svg"),dpi=100,bbox_inches = "tight")
+log2dfLFQvalsSeqsUni.to_csv(pathFiles/(fileName+"log2dfLFQvalsSeqsUni.csv"))
+log2dfLFQvalsSeqsUni.plot(kind='hist',alpha=0.5,bins=100).figure.savefig(pathFiles/(fileName+"log2dfLFQvalsSeqsUni.svg"),dpi=100,bbox_inches = "tight")
