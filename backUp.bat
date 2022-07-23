a1fb02c956ca504ad41036ccbbf185cd6aee5a03
ani <sharma_animesh@hotmail.com>
2022-07-23 21:04:28 +0200

    hourly backup qe with cmd via task scheduler

--- backUp.bat
+++ backUp.bat
@@ -1,3 +1,3 @@
 @echo off
-REM https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy https://techrando.com/2019/06/22/how-to-execute-a-task-hourly-in-task-scheduler/#:~:text=To%20set%20the%20script%20to,indefinite%20under%20the%20duration%20option.
-robocopy \\promec01.win.ntnu.no\promec\promec\ z:\promec\ /e /mt /z  /log:L:\logCrusher.txt
+REM https://datacornering.com/run-batch-file-minimized/  /c start /min "backup" C:\Users\QE-User\backUp.bat   ^& exit https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy https://techrando.com/2019/06/22/how-to-execute-a-task-hourly-in-task-scheduler/#:~:text=To%20set%20the%20script%20to,indefinite%20under%20the%20duration%20option.
+robocopy C:\Xcalibur\data L:\Qexactive\Raw /e /mt /z  /log:L:\logs\logQexactive.txt
