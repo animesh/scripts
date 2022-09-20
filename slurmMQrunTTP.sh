51286babe630e875f0333570eb86edbacc27eee0
ani <animesh.sharma@ntnu.no>
2022-09-20 17:47:02 +0200

    copy and create MaxQaunt slurm batch file for each timsTOF-pro DDA folder and submit

---
+++ slurmMQrunTTP.sh
@@ -0,0 +1,54 @@
+#bash slurmMQrunTTP.sh /cluster/projects/nn9036k/MaxQuant_2.0.3.1/bin/MaxQuantCmd.exe /trd-project1/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/MQTest123/ /cluster/projects/nn9036k/FastaDB/uniprot-proteome-human-iso-june22.fasta  mqparTTP.phoSTY.xml scratch.slurm
+#make sure mqrun.xml is in the directory where the script is and CHANGE following paths according to the MaxQuant Installation and representative parameter file for that version respectively CANNOT handle path containing spaces! create a symlink in such cases e.g. 
+#for i in $HOME/PD/TIMSTOF/LARS/2021/November/*.d ; do echo $i; j=${i// /_}; echo $j; k=$(basename $j) ; echo $k; ln -s "$i" "$k"; done
+#or make a local link without space for root directory
+#ln -s /trd-project1/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2022/september/220908\ PHos/ phos
+#bash slurmMQrunTTP.sh /cluster/projects/nn9036k/MaxQuant_2.0.3.1/bin/MaxQuantCmd.exe $PWD/phos /cluster/projects/nn9036k/FastaDB/uniprot-proteome-human-iso-june22.fasta  mqparTTP.phoSTY.xml scratch.slurm
+MAXQUANTCMD=$1
+DATADIR=$2
+FASTAFILE=$3
+PARAMFILE=$4
+MQSLURMFILE=$5
+CPU=40
+FDR=0.01
+#leave following empty to include ALL files
+PREFIXRAW=
+SEARCHTEXT=TestFile.d
+SEARCHTEXT2=SequencesFasta
+SEARCHTEXT3=LocalCombinedFolder
+SEARCHTEXT4="Fdr>0.01"
+LDIR=$PWD
+CURRENTEPOCTIME=`date +%s`
+WRITEDIR=$PARAMFILE.$CURRENTEPOCTIME.results
+mkdir $WRITEDIR
+#perl -pe 's/\r$//' < mqrun.sh  > tmp
+dos2unix $PARAMFILE
+for i in $DATADIR/*.d ; do echo $i ; 	j=$(basename $i) ; 	k=${j%%.*} ; mkdir $WRITEDIR/$k ; cp -r $i $WRITEDIR/$k ; sed "s|$SEARCHTEXT2|$FASTAFILE|" $LDIR/$PARAMFILE > $WRITEDIR/$k/$PARAMFILE.tmp1 ; sed "s|$SEARCHTEXT|$LDIR/$WRITEDIR/$k/$j|"  $WRITEDIR/$k/$PARAMFILE.tmp1 > $WRITEDIR/$k/$PARAMFILE.tmp2 ; sed "s|$SEARCHTEXT4|Fdr>$FDR|"  $WRITEDIR/$k/$PARAMFILE.tmp2 > $WRITEDIR/$k/$PARAMFILE.tmp3 ; sed "s|$SEARCHTEXT3|$LDIR/$WRITEDIR/$k|"  $WRITEDIR/$k/$PARAMFILE.tmp3 > $WRITEDIR/$k/$k.xml ; rm $WRITEDIR/$k/$PARAMFILE.tmp*  ;done
+#mono $MAXQUANTCMD $k.xml ; cp -rf ./combined/txt $k.REP ; echo $k ; cd $LDIR 
+echo $WRITEDIR
+#mv tmp  mqrun.sh
+#mono $HOME/data/NORSTORE_OSL_DISK/NS9036K/promec/MaxQuant_1.6.8.0/MaxQuant/bin/MaxQuantCmd.exe -n $HOME/promec/Qexactive/Mirta/QExactive/Imen_Belhaj/RawData/out1678.xml
+#-n for dryrun, -p <#checkpoint>
+#date -d @1604251727
+#find $WRITEDIR -name "proteinGroups.txt"  | xargs ls -ltrh
+for i in $PWD/$WRITEDIR/*/*.xml
+#dos2unix scratch.slurm 
+#for i in $PWD/mqparTTP.phoSTY.xml.1663657305.results/*/*.xml 
+    do echo $i
+    j=$(basename $i)
+    k=${j%%.*}
+    d=$(dirname $i) 
+    printf "$i\t$j\t$kt\t$d\n" | tee $d/$k.txt
+    cat $d/$k.txt
+    #cp MQSLURMFILE $d/$k.slurm 
+    cp scratch.slurm $d/$k.slurm 
+    sed "s|MQSLURMNAME|$k|" $d/$k.slurm  > slurm.tmp
+    sed "s|MQSLURMLOG|$d/$k.txt|" slurm.tmp > slurm.tmp2
+    sed "s|MQCMD|$MAXQUANTCMD|" slurm.tmp2 > slurm.tmp
+    sed "s|MQWD|$d|" slurm.tmp > slurm.tmp2
+    sed "s|MQPARF|$i|" slurm.tmp2 > $d/$k.slurm
+    cat $d/$k.slurm
+    sbatch $d/$k.slurm
+done
+tail -n 4 $WRITEDIR/*/*.slurm
+ls $WRITEDIR/*/*.slurm | wc
