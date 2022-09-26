c0553925bac2dbebc6915c8eff3ffece3e15ac6f
ani <animesh.sharma@ntnu.no>
2022-09-26 13:33:01 +0200

    cp and run fragpipe over local directory per data

--- slurmFragpipe.sh
+++ slurmFragpipe.sh
@@ -1,28 +1,35 @@
-
 #git checkout bbcce483bbea5e66edb16213f3f502f135c94941 scratch.slurm
-#dos2unix scratch.slurm
-#bash slurmFragpipe.sh /cluster/projects/nn9036k/220908PHos/d2/ /cluster/projects/nn9036k/FastaDB/2022-04-21-decoys-isoforms-contam-UP000005640.fas scratch.slurm fp.workflow.txt out.slurm.d2.txt
+#ln -s /trd-project1/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2022/september/220926\ ida\ beate arthro
+#bash slurmFragpipe.sh $PWD/arthro /cluster/projects/nn9036k/FastaDB/2022-04-10-decoys-contam-uniprot_sprot.fasta scratch.slurm fp.workflow.txt
 DATADIR=$1
 FASTA=$2
 SLURM=$3
 WORKFLOW=$4
-LOG=$5
-ls -ltrh $DATADIR/*.d > $LOG
-sed "s|FPFASTAFILE|$FASTA|" $WORKFLOW > $DATADIR/fp.workflow.txt
-for i in $DATADIR/*.d
+CURRENTEPOCTIME=`date +%s`
+WRITEDIR=$WORKFLOW.$CURRENTEPOCTIME.results
+mkdir $WRITEDIR
+#perl -pe 's/\r$//' < mqrun.sh  > tmp
+dos2unix $SLURM
+cp -rf $DATADIR/*.d $WRITEDIR/.
+ls -ltrh $WRITEDIR/*.d > $LOG
+sed "s|FPFASTAFILE|$FASTA|" $WORKFLOW > $WRITEDIR/fp.workflow.txt
+for i in $PWD/$WRITEDIR/*.d
     do echo $i
     j=$(basename $i)
     k=${j%%.*}
     d=$(dirname $i) 
     printf "$i\t$k\t\tDDA\n" | tee $d/$k.txt
+    LOG=$i.out.slurm.txt
+    #touch $LOG
     cat $d/$k.txt
-    sed "s|FPSLURMNAME|$k|" $SLURM > slurm.tmp
-    sed "s|FPMANIFESTFILE|$d\/$k\.txt|" slurm.tmp > slurm.tmp2
-    sed "s|FPWORKDIR|$d\/$k|" slurm.tmp2 > slurm.tmp
-    sed "s|FPSLURMLOG|$LOG|" slurm.tmp > slurm.tmp2
-    sed "s|FPWORKFLOW|$d\/fp.workflow.txt|" slurm.tmp2 > $d/$k.slurm
+    sed "s|FPSLURMNAME|$k|" $SLURM > $d/$k.slurm.tmp
+    sed "s|FPMANIFESTFILE|$d/$k\.txt|" $d/$k.slurm.tmp > $d/$k.slurm.tmp2
+    sed "s|FPWORKDIR|$d/$k|" $d/$k.slurm.tmp2 > $d/$k.slurm.tmp
+    sed "s|FPSLURMLOG|$LOG|" $d/$k.slurm.tmp > $d/$k.slurm.tmp2
+    sed "s|FPWORKFLOW|$d/fp.workflow.txt|" $d/$k.slurm.tmp2 > $d/$k.slurm
+    ls -ltrh $d/$k.slurm
     cat $d/$k.slurm
     sbatch $d/$k.slurm
 done
-head -n 9 $DATADIR/fp.workflow.txt
+head -n 9 $WRITEDIR/fp.workflow.txt
 
