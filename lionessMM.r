39172c1c5d4becb411a00bf3830a5a7e7070bd00
Animesh Sharma <animeshs@ntnu.no>
2022-10-07 16:55:48 +0200

    fix correlation by adding noise, overlay differentially expressed proteins over network

--- lionessMM.r
+++ lionessMM.r
@@ -1,15 +1,20 @@
 #setup####
 #sudo apt install gfortran libblas-dev liblapack-dev
-dirLocal <- "/home/ash022/scripts/Singh/"
-libLocal <- paste0(dirLocal,"rLib")
-install.packages("infotheo", lib = libLocal)
+options(nwarnings = 1000000)
+summary(warnings(1000000))
+set.seed(42)
+dirLocal <- getwd()
+libLocal <- paste(dirLocal,"rLib",sep="/")
 install.packages("BiocManager", lib = libLocal)
+install.packages("infotheo", lib = libLocal)
 BiocManager::install("GenomicRanges", lib = libLocal)
 BiocManager::install("MatrixGenerics", lib = libLocal)
 BiocManager::install("matrixStats", lib = libLocal)
 BiocManager::install("SummarizedExperiment", lib = libLocal)
 BiocManager::install("lionessR", lib = libLocal)
 BiocManager::install("igraph", lib = libLocal)
+BiocManager::install("limma", lib = libLocal)
+BiocManager::install("reshape2", lib = libLocal)
 library("infotheo", lib = libLocal)
 library("matrixStats", lib = libLocal)
 library("GenomicRanges", lib = libLocal)
@@ -17,10 +22,10 @@ library("MatrixGenerics", lib = libLocal)
 library("SummarizedExperiment", lib = libLocal)
 library("lionessR", lib = libLocal)
 library("igraph", lib = libLocal)
-options(nwarnings = 1000000)
-summary(warnings(1000000))
+library("limma", lib = libLocal)
+library("reshape2", lib = libLocal)
 #data####
-M <- read.csv(paste0(dirLocal,"dataTmm.csv"), header = T, row.names = 1)
+M <- read.csv("C:\\Users\\animeshs\\OneDrive - NTNU\\Singh\\dataTmm.csv", header = T, row.names = 1)
 #select####
 cvar <- apply(as.array(as.matrix(M)), 1, sd)
 # https://github.com/orgs/community/discussions/26316 for plot , unblock cookies
@@ -37,7 +42,7 @@ rN <- rownames(dat)
 cN <- colnames(dat)
 #MI####
 library("infotheo")
-datDisc <- discretize(t(dat), disc = "equalwidth", nbins = 100)
+datDisc <- discretize(t(dat), disc = "equalwidth", nbins = 46)
 summary(datDisc)
 rownames(datDisc) <- cN
 colnames(datDisc) <- rN
@@ -97,13 +102,211 @@ lionessOutput[,1+2] <- nrsamples*(agg-ss)+ss
 summary(lionessOutput[,1+2])
 fName<-gsub("[^[:alnum:]]", "", strsplit(as.character(list(netFunMI))," "))#[[1]][7:10])
 saveRDS(cormat,paste0(dirLocal,fName,".cormat.RDS"))
+cormat<-readRDS("C:\\Users\\animeshs\\OneDrive - NTNU\\Singh\\cfunctionxnnstatscortxmethodpearsonn.cormat.RDS")
 head(cormat)
 z <- cormat@assays@data[[1]]
 rownames(z) <- rownames(cormat)
 head(z)
 toptable_edges <- t(matrix(unlist(c(strsplit(row.names(z), "_"))), 2))
 head(toptable_edges)
+tail(toptable_edges)
+z1 <- cbind(toptable_edges, z)
+head(z1)
+plot(z1[2,])
 #sampleN####
+## label sample group names
+sampleN <- c('MGUS1','MGUS2','MGUS3','MGUS4','MGUS5','MGUS6','MGUS7','MGUS8','MGUS9','Ml1','Ml2','Ml3','Ml4','Ml5','Ml6','MM7','MM8','MM9','MM10','MM11','MM12','MM13','MM14','MM15','MM16','MM17','MM18','MM19','MM20','MM21','MM22','MM23','MM24','MM25','MM26','MM27','MM28','MM29','MM30','MM31','MM32','MM33','MM34','MM35','MM36','MM37')
+grpSample <- c('MGUS','MGUS','MGUS','MGUS','MGUS','MGUS','MGUS','MGUS','MGUS','Ml','Ml','Ml','Ml','Ml','Ml','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM','MM')
+## add matching sample names in trait object
+trait<-data.frame(cbind(sampleN,grpSample))
+head(trait)
+dim(M)
+colnames(M) <- sampleN
+#[1] 3956   46
+head(M)
+randM<-matrix(rnorm(nrow(M)*ncol(M), 1e-16,1e-15),nrow=nrow(M),ncol=ncol(M))+M
+summary(randM)
+summary(M)
+corMM <- cor(t(M))
+summary(warnings())
+dim(corMM)
+hist(corMM)
+corMMrand <- cor(t(randM))
+summary(warnings())
+dim(corMMrand)
+hist(corMMrand)
+hist(corMMrand-corMM)
+
+datMM<-data.frame(randM[,trait$grpSample == "MM"])
+summary(datMM)
+dim(datMM)
+nsel = nrow(M)
+#nsel <- 500
+cvar <- apply(as.array(as.matrix((datMM))), 1, sd)
+sum(cvar==0)
+length(cvar)
+hist(cvar)
+datMM <- cbind(cvar, datMM)
+datMM <- datMM[order(datMM[,1], decreasing=F),]
+#datMM <- datMM[cvar>0, -1]
+datMM <- datMM[1:nsel, -1]
+datMM <- as.matrix(datMM)
+summary(datMM)
+dim(datMM)
+hist(datMM)
+sum(datMM==0)
+netyes <- cor(t(datMM))
+summary(warnings())
+dim(netyes)
+hist(netyes)
+datMG<-randM[,trait$grpSamp == "MGUS"]
+summary(datMG)
+cvar <- apply(as.array(as.matrix((datMG))), 1, sd)
+sum(cvar==0)
+hist(cvar)
+datMG <- cbind(cvar, datMG)
+datMG <- datMG[order(datMG[,1], decreasing=F),]
+#datMG <- datMG[cvar>0, -1]
+datMG <- datMG[1:nsel, -1]
+datMG <- as.matrix(datMG)
+summary(datMG)
+dim(datMG)
+netno  <- cor(t(datMG))
+summary(warnings())
+dim(netno)
+hist(netno)
+netdiff <- netyes-netno
+hist(netdiff)
+dim(netdiff)
+zeroDiff=netdiff==0
+sum(zeroDiff)
+## convert these adjacency matrices to edgelists
+cormat2 <- rep(1:nsel, each=nsel)
+cormat1 <- rep(1:nsel,nsel)
+el      <- cbind(cormat1, cormat2, c(netdiff))
+melted  <- melt(upper.tri(netdiff))
+melted  <- melted[which(melted$value),]
+values  <- netdiff[which(upper.tri(netdiff))]
+melted  <- cbind(melted[,1:2], values)
+summary(melted)
+genes   <- row.names(netdiff)
+melted[,1] <- genes[melted[,1]]
+melted[,2] <- genes[melted[,2]]
+row.names(melted) <- paste(melted[,1], melted[,2], sep="_")
+tosub <- melted
+hist(tosub[,3])
+length(tosub[,3])
+corThr<-1
+tosel <- row.names(tosub[which(abs(tosub[,3])>corThr),])
+length(tosel)
+corsub <- cormat[which(row.names(cormat)  %in% tosel) ,]
+CC <- corsub@assays@data[[1]]
+rownames(CC) <- rownames(corsub)
+head(CC)
+hist(CC)
+dim(CC)
+group <- factor(trait[,2])
+design <- model.matrix(~0+group)
+cont.matrix <- makeContrasts(yesvsno = (groupMM - groupMGUS), levels = design)
+fit <- lmFit(CC, design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2e <- eBayes(fit2)
+toptable <- topTable(fit2e, number=nrow(CC), adjust.method="fdr")
+head(toptable,10)
+hist(toptable$logFC)
+hist(toptable$P.Value)
+hist(toptable$adj.P.Val)
+hist(toptable$t)
+summary(toptable$t)
+toptable_edges <- t(matrix(unlist(c(strsplit(row.names(toptable), "_"))),2))
+N <- nrow(CC)
+N<-100
+z <- cbind(toptable_edges[1:N,], as.data.frame(toptable$logFC[1:N]))
+fdrThr<-0.1
+z <- cbind(toptable_edges[toptable$adj.P.Val<fdrThr,], as.data.frame(toptable$logFC[toptable$adj.P.Val<fdrThr]))
+summary(z)
+hist(z[,3])
+tThr<-5
+z <- cbind(toptable_edges[abs(toptable$t)>tThr,], as.data.frame(toptable$logFC[abs(toptable$t)>tThr]))
+summary(z)
+hist(z[,3])
+#z <- z[abs(z[,3]) > 1.5, ]
+g <- graph.data.frame(z, directed=F)
+cl <- components(g)
+ccG<-lapply(seq_along(cl$csize)[cl$csize > 2], function(x) V(g)$name[cl$membership %in% x])
+length(ccG)
+ccG
+ccG1<-ccG[[1]]
+labelOffset <- strwidth(names(V(g)),units = 'in')
+vertex.label.dist = labelOffset*2.6
+E(g)$weight <- abs(z[,3])
+tt <- unlist(ccG)
+tt <- tt[34:length(ccG)]
+g <- delete_vertices(g, tt)
+plot(g)
+E(g)$weight
+<- as.numeric(z[,3])
+E(g)$color[E(g)$weight<0] <- "blue"
+E(g)$color[E(g)$weight>0] <- "red"
+plot(g, vertex.label.cex=0.7,
+     vertex.size=6,
+     vertex.label.color = "black",
+     vertex.label.font=3, vertex.label.dist = labelOffset*1.6,
+     edge.width=10*(abs(as.numeric(z[,3]))-0.7),
+     vertex.color=V(g)$color,
+     layout = layout_as_tree)
+#(g)
+#Next, we perform a LIMMA analysis on gene expression so that we can also color nodes based on their differential expression:
+
+topgeneslist <- unique(c(toptable_edges[1:N,]))
+fit <- lmFit(M, design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2e <- eBayes(fit2)
+topDE <- topTable(fit2e, number=nrow(M), adjust="fdr")
+topDE <- topDE[which(row.names(topDE) %in% topgeneslist),]
+topgenesDE <- cbind(row.names(topDE), topDE$t)
+hist(as.numeric(topgenesDE))
+#We color nodes based on the t-statistic from the LIMMA analysis:
+
+# add t-statistic to network nodes
+nodeorder <- cbind(V(g)$name, 1:length(V(g)))
+nodes <- merge(nodeorder, topgenesDE, by.x=1, by.y=1)
+nodes <- nodes[order(as.numeric(as.character(nodes[,2]))),]
+nodes[,3] <- as.numeric(as.character(nodes[,3]))
+nodes <- nodes[,-2]
+V(g)$weight <- nodes[,2]
+
+# make a color palette
+mypalette4 <- colorRampPalette(c("blue","white","white","red"), space="Lab")(256)
+breaks2a <- seq(min(V(g)$weight), 0, length.out=128)
+breaks2b <- seq(0.00001, max(V(g)$weight)+0.1,length.out=128)
+breaks4 <- c(breaks2a,breaks2b)
+
+# select bins for colors
+bincol <- rep(NA, length(V(g)))
+for(i in 1:length(V(g))){
+  bincol[i] <- min(which(breaks4>V(g)$weight[i]))
+}
+bincol <- mypalette4[bincol]
+
+# add colors to nodes
+V(g)$color <- bincol
+
+#Finally, we visualize these results in a network diagram of the 50 most significant edges from the LIMMA analysis:
+library(igraph)
+par(mar=c(0,0,0,0))
+plot(g, vertex.label.cex=0.7, vertex.size=6,
+     vertex.label.color = "black",
+     vertex.label.font=3,
+     edge.width=10*(abs(as.numeric(z[,3]))-0.7), vertex.color=V(g)$color)
+#plot(simplify(g)) # remove loops and multiple edges
+#plot(delete.vertices(simplify(g))) # additionally delete isolated nodes
+
+
+
+
+
+
 chkGene <- "LAGE3"
 for (patients in 1:ncol(z)) {
     print(paste0("patient", patients, chkGene))
