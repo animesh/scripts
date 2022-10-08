08b1efe77e01c5378dd1d88693f3f86555bd5721
Animesh Sharma <animeshs@ntnu.no>
2022-10-08 13:51:45 +0200

    check for network difference between MGUS and MM-like

--- lionessMM.r
+++ lionessMM.r
@@ -136,7 +136,7 @@ summary(warnings())
 dim(corMMrand)
 hist(corMMrand)
 hist(corMMrand-corMM)
-
+#sampleMM####
 datMM<-data.frame(randM[,trait$grpSample == "MM"])
 summary(datMM)
 dim(datMM)
@@ -159,6 +159,7 @@ netyes <- cor(t(datMM))
 summary(warnings())
 dim(netyes)
 hist(netyes)
+#sampleMGUS####
 datMG<-randM[,trait$grpSamp == "MGUS"]
 summary(datMG)
 cvar <- apply(as.array(as.matrix((datMG))), 1, sd)
@@ -181,10 +182,13 @@ dim(netdiff)
 zeroDiff=netdiff==0
 sum(zeroDiff)
 ## convert these adjacency matrices to edgelists
+#rep(1:3,nsel)
 cormat2 <- rep(1:nsel, each=nsel)
 cormat1 <- rep(1:nsel,nsel)
 el      <- cbind(cormat1, cormat2, c(netdiff))
+hist(el[,3])
 melted  <- melt(upper.tri(netdiff))
+summary(melted)
 melted  <- melted[which(melted$value),]
 values  <- netdiff[which(upper.tri(netdiff))]
 melted  <- cbind(melted[,1:2], values)
@@ -263,6 +267,7 @@ fit <- lmFit(M, design)
 fit2 <- contrasts.fit(fit, cont.matrix)
 fit2e <- eBayes(fit2)
 topDE <- topTable(fit2e, number=nrow(M), adjust="fdr")
+hist(topDE$logFC)
 topDE <- topDE[which(row.names(topDE) %in% topgeneslist),]
 topgenesDE <- cbind(row.names(topDE), topDE$t)
 hist(as.numeric(topgenesDE))
@@ -301,12 +306,264 @@ plot(g, vertex.label.cex=0.7, vertex.size=6,
      edge.width=10*(abs(as.numeric(z[,3]))-0.7), vertex.color=V(g)$color)
 #plot(simplify(g)) # remove loops and multiple edges
 #plot(delete.vertices(simplify(g))) # additionally delete isolated nodes
+#sampleMl####
+datMl<-randM[,trait$grpSamp == "Ml"]
+summary(datMl)
+cvar <- apply(as.array(as.matrix((datMl))), 1, sd)
+sum(cvar==0)
+hist(cvar)
+datMl <- cbind(cvar, datMl)
+datMl <- datMl[order(datMl[,1], decreasing=F),]
+#datMl <- datMl[cvar>0, -1]
+datMl <- datMl[1:nsel, -1]
+datMl <- as.matrix(datMl)
+summary(datMl)
+dim(datMl)
+netno  <- cor(t(datMl))
+summary(warnings())
+dim(netno)
+hist(netno)
+netdiff <- netyes-netno
+hist(netdiff)
+dim(netdiff)
+zeroDiff=netdiff==0
+sum(zeroDiff)
+## convert these adjacency matrices to edgelists
+#rep(1:3,nsel)
+cormat2 <- rep(1:nsel, each=nsel)
+cormat1 <- rep(1:nsel,nsel)
+el      <- cbind(cormat1, cormat2, c(netdiff))
+hist(el[,3])
+melted  <- melt(upper.tri(netdiff))
+summary(melted)
+melted  <- melted[which(melted$value),]
+values  <- netdiff[which(upper.tri(netdiff))]
+melted  <- cbind(melted[,1:2], values)
+summary(melted)
+genes   <- row.names(netdiff)
+melted[,1] <- genes[melted[,1]]
+melted[,2] <- genes[melted[,2]]
+row.names(melted) <- paste(melted[,1], melted[,2], sep="_")
+tosub <- melted
+hist(tosub[,3])
+length(tosub[,3])
+corThr<-1
+tosel <- row.names(tosub[which(abs(tosub[,3])>corThr),])
+length(tosel)
+corsub <- cormat[which(row.names(cormat)  %in% tosel) ,]
+CC <- corsub@assays@data[[1]]
+rownames(CC) <- rownames(corsub)
+head(CC)
+hist(CC)
+dim(CC)
+group <- factor(trait[,2])
+design <- model.matrix(~0+group)
+cont.matrix <- makeContrasts(yesvsno = (groupMM - groupMl), levels = design)
+fit <- lmFit(CC, design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2e <- eBayes(fit2)
+toptable <- topTable(fit2e, number=nrow(CC), adjust.method="fdr")
+head(toptable,10)
+hist(toptable$logFC)
+hist(toptable$P.Value)
+hist(toptable$adj.P.Val)
+hist(toptable$t)
+summary(toptable$t)
+toptable_edges <- t(matrix(unlist(c(strsplit(row.names(toptable), "_"))),2))
+N <- nrow(CC)
+N<-100
+z <- cbind(toptable_edges[1:N,], as.data.frame(toptable$logFC[1:N]))
+fdrThr<-0.1
+z <- cbind(toptable_edges[toptable$adj.P.Val<fdrThr,], as.data.frame(toptable$logFC[toptable$adj.P.Val<fdrThr]))
+summary(z)
+hist(z[,3])
+tThr<-5
+z <- cbind(toptable_edges[abs(toptable$t)>tThr,], as.data.frame(toptable$logFC[abs(toptable$t)>tThr]))
+summary(z)
+hist(z[,3])
+#z <- z[abs(z[,3]) > 1.5, ]
+g <- graph.data.frame(z, directed=F)
+cl <- components(g)
+ccG<-lapply(seq_along(cl$csize)[cl$csize > 2], function(x) V(g)$name[cl$membership %in% x])
+length(ccG)
+ccG
+ccG1<-ccG[[1]]
+labelOffset <- strwidth(names(V(g)),units = 'in')
+vertex.label.dist = labelOffset*2.6
+E(g)$weight <- abs(z[,3])
+tt <- unlist(ccG)
+tt <- tt[34:length(ccG)]
+g <- delete_vertices(g, tt)
+plot(g)
+E(g)$weight <- as.numeric(z[,3])
+E(g)$color[E(g)$weight<0] <- "blue"
+E(g)$color[E(g)$weight>0] <- "red"
+plot(g, vertex.label.cex=0.7,
+     vertex.size=6,
+     vertex.label.color = "black",
+     vertex.label.font=3, vertex.label.dist = labelOffset*1.6,
+     edge.width=10*(abs(as.numeric(z[,3]))-0.7),
+     vertex.color=V(g)$color,
+     layout = layout_as_tree)
+#(g)
+#Next, we perform a LIMMA analysis on gene expression so that we can also color nodes based on their differential expression:
 
+topgeneslist <- unique(c(toptable_edges[1:N,]))
+fit <- lmFit(M, design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2e <- eBayes(fit2)
+topDE <- topTable(fit2e, number=nrow(M), adjust="fdr")
+hist(topDE$logFC)
+topDE <- topDE[which(row.names(topDE) %in% topgeneslist),]
+topgenesDE <- cbind(row.names(topDE), topDE$t)
+hist(as.numeric(topgenesDE))
+#We color nodes based on the t-statistic from the LIMMA analysis:
 
+# add t-statistic to network nodes
+nodeorder <- cbind(V(g)$name, 1:length(V(g)))
+nodes <- merge(nodeorder, topgenesDE, by.x=1, by.y=1)
+nodes <- nodes[order(as.numeric(as.character(nodes[,2]))),]
+nodes[,3] <- as.numeric(as.character(nodes[,3]))
+nodes <- nodes[,-2]
+V(g)$weight <- nodes[,2]
+mypalette4 <- colorRampPalette(c("blue","white","white","red"), space="Lab")(256)
+breaks2a <- seq(min(V(g)$weight), 0, length.out=128)
+breaks2b <- seq(0.00001, max(V(g)$weight)+0.1,length.out=128)
+breaks4 <- c(breaks2a,breaks2b)
+bincol <- rep(NA, length(V(g)))
+for(i in 1:length(V(g))){
+  bincol[i] <- min(which(breaks4>V(g)$weight[i]))
+}
+bincol <- mypalette4[bincol]
+V(g)$color <- bincol
+par(mar=c(0,0,0,0))
+plot(g, vertex.label.cex=0.7, vertex.size=6,
+     vertex.label.color = "black",
+     vertex.label.font=3,
+     edge.width=10*(abs(as.numeric(z[,3]))-0.7), vertex.color=V(g)$color)
+#sampleMLMG####
+netyes  <- cor(t(datMl))
+netno  <- cor(t(datMG))
+summary(warnings())
+netdiff <- netyes-netno
+hist(netdiff)
+dim(netdiff)
+zeroDiff=netdiff==0
+sum(zeroDiff)
+## convert these adjacency matrices to edgelists
+#rep(1:3,nsel)
+cormat2 <- rep(1:nsel, each=nsel)
+cormat1 <- rep(1:nsel,nsel)
+el      <- cbind(cormat1, cormat2, c(netdiff))
+hist(el[,3])
+melted  <- melt(upper.tri(netdiff))
+summary(melted)
+melted  <- melted[which(melted$value),]
+values  <- netdiff[which(upper.tri(netdiff))]
+melted  <- cbind(melted[,1:2], values)
+summary(melted)
+genes   <- row.names(netdiff)
+melted[,1] <- genes[melted[,1]]
+melted[,2] <- genes[melted[,2]]
+row.names(melted) <- paste(melted[,1], melted[,2], sep="_")
+tosub <- melted
+hist(tosub[,3])
+length(tosub[,3])
+corThr<-1
+tosel <- row.names(tosub[which(abs(tosub[,3])>corThr),])
+length(tosel)
+corsub <- cormat[which(row.names(cormat)  %in% tosel) ,]
+CC <- corsub@assays@data[[1]]
+rownames(CC) <- rownames(corsub)
+head(CC)
+hist(CC)
+dim(CC)
+group <- factor(trait[,2])
+design <- model.matrix(~0+group)
+cont.matrix <- makeContrasts(yesvsno = (groupMl - groupMGUS), levels = design)
+fit <- lmFit(CC, design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2e <- eBayes(fit2)
+toptable <- topTable(fit2e, number=nrow(CC), adjust.method="fdr")
+head(toptable,10)
+hist(toptable$logFC)
+hist(toptable$P.Value)
+hist(toptable$adj.P.Val)
+hist(toptable$t)
+summary(toptable$t)
+toptable_edges <- t(matrix(unlist(c(strsplit(row.names(toptable), "_"))),2))
+N <- nrow(CC)
+N<-100
+z <- cbind(toptable_edges[1:N,], as.data.frame(toptable$logFC[1:N]))
+fdrThr<-0.1
+z <- cbind(toptable_edges[toptable$adj.P.Val<fdrThr,], as.data.frame(toptable$logFC[toptable$adj.P.Val<fdrThr]))
+summary(z)
+hist(z[,3])
+tThr<-5
+z <- cbind(toptable_edges[abs(toptable$t)>tThr,], as.data.frame(toptable$logFC[abs(toptable$t)>tThr]))
+summary(z)
+hist(z[,3])
+#z <- z[abs(z[,3]) > 1.5, ]
+g <- graph.data.frame(z, directed=F)
+cl <- components(g)
+ccG<-lapply(seq_along(cl$csize)[cl$csize > 2], function(x) V(g)$name[cl$membership %in% x])
+length(ccG)
+ccG
+ccG1<-ccG[[1]]
+labelOffset <- strwidth(names(V(g)),units = 'in')
+vertex.label.dist = labelOffset*2.6
+E(g)$weight <- abs(z[,3])
+tt <- unlist(ccG)
+tt <- tt[34:length(ccG)]
+g <- delete_vertices(g, tt)
+plot(g)
+E(g)$weight <- as.numeric(z[,3])
+E(g)$color[E(g)$weight<0] <- "blue"
+E(g)$color[E(g)$weight>0] <- "red"
+plot(g, vertex.label.cex=0.7,
+     vertex.size=6,
+     vertex.label.color = "black",
+     vertex.label.font=3, vertex.label.dist = labelOffset*1.6,
+     edge.width=10*(abs(as.numeric(z[,3]))-0.7),
+     vertex.color=V(g)$color,
+     layout = layout_as_tree)
+#(g)
+#Next, we perform a LIMMA analysis on gene expression so that we can also color nodes based on their differential expression:
 
+topgeneslist <- unique(c(toptable_edges[1:N,]))
+fit <- lmFit(M, design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2e <- eBayes(fit2)
+topDE <- topTable(fit2e, number=nrow(M), adjust="fdr")
+hist(topDE$logFC)
+topDE <- topDE[which(row.names(topDE) %in% topgeneslist),]
+topgenesDE <- cbind(row.names(topDE), topDE$t)
+hist(as.numeric(topgenesDE))
+#We color nodes based on the t-statistic from the LIMMA analysis:
 
-
-
+# add t-statistic to network nodes
+nodeorder <- cbind(V(g)$name, 1:length(V(g)))
+nodes <- merge(nodeorder, topgenesDE, by.x=1, by.y=1)
+nodes <- nodes[order(as.numeric(as.character(nodes[,2]))),]
+nodes[,3] <- as.numeric(as.character(nodes[,3]))
+nodes <- nodes[,-2]
+V(g)$weight <- nodes[,2]
+mypalette4 <- colorRampPalette(c("blue","white","white","red"), space="Lab")(256)
+breaks2a <- seq(min(V(g)$weight), 0, length.out=128)
+breaks2b <- seq(0.00001, max(V(g)$weight)+0.1,length.out=128)
+breaks4 <- c(breaks2a,breaks2b)
+bincol <- rep(NA, length(V(g)))
+for(i in 1:length(V(g))){
+  bincol[i] <- min(which(breaks4>V(g)$weight[i]))
+}
+bincol <- mypalette4[bincol]
+V(g)$color <- bincol
+par(mar=c(0,0,0,0))
+plot(g, vertex.label.cex=0.7, vertex.size=6,
+     vertex.label.color = "black",
+     vertex.label.font=3,
+     edge.width=10*(abs(as.numeric(z[,3]))-0.7), vertex.color=V(g)$color)
+#personNet####
 chkGene <- "LAGE3"
 for (patients in 1:ncol(z)) {
     print(paste0("patient", patients, chkGene))
@@ -323,6 +580,7 @@ for (patients in 1:ncol(z)) {
     write.csv(dg_df, file = paste0("patient", patients, ".MI.csv"))
 }
 summary(warnings(1000000))
+#save####
 savehistory(file = "setup.Rhistory")
 #python####
 import pandas as pd
