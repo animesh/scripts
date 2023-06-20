fe5cce062e43922487d62a10466d317dc7dbfc94
Animesh Sharma <animesh.sharma@ntnu.no>
2023-06-20 12:20:10 +0200

    simpler xl-file with comparisons of which proteins go up, down or are unchanged in treated versus control group

--- diffExprTestT.r
+++ diffExprTestT.r
@@ -1,30 +1,36 @@
-#C:\Users\animeshs\R-4.2.3\bin\Rscript.exe diffExprTestT.r L:\promec\TIMSTOF\LARS\2023\230217_Caroline\combined\txt\ Bio
+#git checkout c16022351e13c77dae02926044c2bf59cdf4d7ec diffExprTestT.r
+#C:\Users\animeshs\OneDrive\Desktop\Scripts>C:\Users\animeshs\R-4.2.3\bin\Rscript.exe diffExprTestT.r "L:\promec\TIMSTOF\LARS\2023\230614 Anders Sundan\combined\txt\proteinGroups.txt" "L:\promec\TIMSTOF\LARS\2023\230614 Anders Sundan\combined\txt\Groups.txt" Bio Rem
+#setup
+#install.packages(c("readxl","writexl","svglite","ggplot2","BiocManager"),repos="http://cran.us.r-project.org",lib=.libPaths())
+#BiocManager::install(c("limma","pheatmap"),repos="http://cran.us.r-project.org",lib=.libPaths())
+#install.packages("devtools")
+#devtools::install_github("jdstorey/qvalue")
+print("USAGE:<path to Rscript.exe> diffExprTestT.r <complete path to directory containing proteinGroups.txt> <complete path to directory containing Groups.txt files> <name of group column in Groups.txt annotating data/rows to be used for analysis> <name of column in Groups.txt marking data NOT to be considered in analysis>")
 args = commandArgs(trailingOnly=TRUE)
+print(paste("supplied argument(s):", length(args)))
 print(args)
-if (length(args) != 2) {stop("\n\nNeeds the full path of the directory containing BOTH proteinGroups.txt from MaxQuant & Groups.txt files followed by the name of GROUP column in Groups.txt file whch will be used for the rank-test, for example
-\"c:/Users/animeshs/R/bin/Rscript.exe diffExprTestRank.r F:/combined/txt/ Bio\"
-                             ", call.=FALSE)}
-#setup####
-#install.packages("writexl")
-#install.packages("pheatmap")
-#install.packages("ggplot2")
-#install.packages("svglite")
-#install.packages("BiocManager")
-#BiocManager::install("limma")
-inpD <- args[1]
-#inpD <-"L:/promec/TIMSTOF/LARS/2023/230217_Caroline/combined/txt/"
-lGroup <- args[2]
+if (length(args) != 4) {stop("\n\nNeeds FOUR arguments, the full path of the directory containing BOTH proteinGroups.txt AND Groups.txt files followed by the name of GROUP-to-compare and data-to-REMOVE columns in Groups.txt file, for example:
+
+c:/Users/animeshs/R-4.2.1-win/bin/Rscript.exe diffExprTestT.r \"L:/promec/TIMSTOF/LARS/2022/september/220928 Ida Beate/Brusk/combined/txt/proteinGroups.txt\" \"L:/promec/TIMSTOF/LARS/2022/september/220928 Ida Beate/Brusk/combined/txt/Groups.txt\" Bio Rem
+", call.=FALSE)}
+inpF <- args[1]
+#inpF <-"L:/promec/TIMSTOF/LARS/2023/230614 Anders Sundan/combined/txt/proteinGroups.txt"
+inpL <- args[2]
+#inpL <-"L:/promec/TIMSTOF/LARS/2023/230614 Anders Sundan/combined/txt/Groups.txt"
+lGroup <- args[3]
 #lGroup<-"Bio"
-inpF<-paste0(inpD,"proteinGroups.txt")
-inpL<-paste0(inpD,"Copy of Groups_CP.txt")
+rGroup <- args[4]
+#rGroup<-"Rem"
+inpD<-dirname(inpF)
+fName<-basename(inpF)
+lName<-basename(inpL)
 selection<-"LFQ.intensity."
 thr=0.0#count
-selThr=0.1#pValue-rTest
+selThr=0.05#pValue-tTest
 selThrFC=0.5#log2-MedianDifference
-cvThr=Inf#threshold for coefficient-of-variation
-testName="1way"
-hdr<-gsub("[^[:alnum:] ]", "",inpD)
-outP=paste(inpF,selection,selThr,selThrFC,cvThr,hdr,lGroup,"VolcanoTestT","pdf",sep = ".")
+cvThr=0.05#threshold for coefficient-of-variation
+hdr<-gsub("[^[:alnum:]]", "",inpD)
+outP=paste(inpF,selection,selThr,selThrFC,cvThr,hdr,lGroup,rGroup,lName,"VolcanoTestT","pdf",sep = ".")
 pdf(outP)
 #data####
 data <- read.table(inpF,stringsAsFactors = FALSE, header = TRUE, quote = "", comment.char = "", sep = "\t")
@@ -35,8 +41,28 @@ data <- read.table(inpF,stringsAsFactors = FALSE, header = TRUE, quote = "", com
 row.names(data)<-paste(row.names(data),data$Fasta.headers,data$Protein.IDs,data$Protein.names,data$Gene.names,data$Score,data$Peptide.counts..unique.,sep=";;")
 summary(data)
 dim(data)
-hist(as.matrix(log2(data[,grep("Intensity",colnames(data))])))
+log2Int<-as.matrix(log2(data[,grep("Intensity",colnames(data))]))
+log2Int[log2Int==-Inf]=NA
+hist(log2Int,main=paste("Mean:",mean(log2Int,na.rm=T),"SD:",sd(log2Int,na.rm=T)),breaks=round(max(log2Int,na.rm=T)),xlim=range(min(log2Int,na.rm=T),max(log2Int,na.rm=T)))
 summary(log2(data[,grep("Intensity",colnames(data))]))
+par(mar=c(12,3,1,1))
+boxplot(log2Int,las=2)
+#corHCint####
+scale=3
+log2Intimp<-matrix(rnorm(dim(log2Int)[1]*dim(log2Int)[2],mean=mean(log2Int,na.rm = T)-scale,sd=sd(log2Int,na.rm = T)/(scale)), dim(log2Int)[1],dim(log2Int)[2])
+log2Intimp[log2Intimp<0]<-0
+par(mar=c(12,3,1,1))
+boxplot(log2Intimp,las=2)
+bk1 <- c(seq(-3,-0.01,by=0.01))
+bk2 <- c(seq(0.01,3,by=0.01))
+bk <- c(bk1,bk2)  #combine the break limits for purpose of graphing
+my_palette <- c(colorRampPalette(colors = c("darkblue", "white"))(n = length(bk1)-1),"gray", "gray",c(colorRampPalette(colors = c("white","darkred"))(n = length(bk2)-1)))
+colnames(log2Intimp)<-colnames(log2Int)
+log2IntimpCorr<-cor(log2Int,use="pairwise.complete.obs",method="spearman")
+colnames(log2IntimpCorr)<-colnames(log2Int)
+rownames(log2IntimpCorr)<-colnames(log2Int)
+svgPHC<-pheatmap::pheatmap(log2IntimpCorr,clustering_distance_rows = "euclidean",clustering_distance_cols = "euclidean",fontsize_row=8,cluster_cols=T,cluster_rows=T,fontsize_col  = 8)
+#maxLFQ####
 LFQ<-as.matrix(data[,grep(selection,colnames(data))])
 #protNum<-1:ncol(LFQ)
 #protNum<-"LFQ intensity"#1:ncol(LFQ)
@@ -44,11 +70,12 @@ LFQ<-as.matrix(data[,grep(selection,colnames(data))])
 colnames(LFQ)=sub(selection,"",colnames(LFQ))
 dim(LFQ)
 log2LFQ<-log2(LFQ)
-colnames(log2LFQ)<-colnames(LFQ)
 log2LFQ[log2LFQ==-Inf]=NA
 log2LFQ[log2LFQ==0]=NA
 summary(log2LFQ)
-hist(log2LFQ)
+hist(log2LFQ,main=paste("Mean:",mean(log2LFQ,na.rm=T),"SD:",sd(log2LFQ,na.rm=T)),breaks=round(max(log2Int,na.rm=T)),xlim=range(min(log2Int,na.rm=T),max(log2Int,na.rm=T)))
+par(mar=c(12,3,1,1))
+boxplot(log2LFQ,las=2)
 rowName<-paste(sapply(strsplit(paste(sapply(strsplit(data$Fasta.headers, "|",fixed=T), "[", 2)), "-"), "[", 1))
 writexl::write_xlsx(as.data.frame(cbind(rowName,log2LFQ,rownames(data))),paste0(inpD,"log2LFQ.xlsx"))
 data$geneName<-paste(sapply(strsplit(paste(sapply(strsplit(data$Gene.names, ";",fixed=T), "[", 1)), " "), "[", 1))
@@ -58,32 +85,27 @@ data[data$geneName=="NA","geneName"]=data[data$geneName=="NA","uniprotID"]
 label<-read.table(inpL,header=T,sep="\t",row.names=1)#, colClasses=c(rep("factor",3)))
 rownames(label)=sub(selection,"",rownames(label))
 label["pair2test"]<-label[lGroup]
-#label["pair2test"]<-label["Bio"]
+if(rGroup %in% colnames(label)){label["removed"]<-label[rGroup]} else{label["removed"]=NA}
 print(label)
-#corHC####
-scale=3
+#corHClfq####
 log2LFQimp<-matrix(rnorm(dim(log2LFQ)[1]*dim(log2LFQ)[2],mean=mean(log2LFQ,na.rm = T)-scale,sd=sd(log2LFQ,na.rm = T)/(scale)), dim(log2LFQ)[1],dim(log2LFQ)[2])
 log2LFQimp[log2LFQimp<0]<-0
-bk1 <- c(seq(-1,-0.01,by=0.01))
-bk2 <- c(seq(0.01,1,by=0.01))
-bk <- c(bk1,bk2)  #combine the break limits for purpose of graphing
-my_palette <- c(colorRampPalette(colors = c("darkblue", "white"))(n = length(bk1)-1),"gray", "gray",c(colorRampPalette(colors = c("white","darkred"))(n = length(bk2)-1)))
+par(mar=c(12,3,1,1))
+boxplot(log2LFQimp,las=2)
 colnames(log2LFQimp)<-colnames(log2LFQ)
 log2LFQimpCorr<-cor(log2LFQ,use="pairwise.complete.obs",method="spearman")
 colnames(log2LFQimpCorr)<-colnames(log2LFQ)
 rownames(log2LFQimpCorr)<-colnames(log2LFQ)
 svgPHC<-pheatmap::pheatmap(log2LFQimpCorr,clustering_distance_rows = "euclidean",clustering_distance_cols = "euclidean",fontsize_row=8,cluster_cols=T,cluster_rows=T,fontsize_col  = 8)
-ggplot2::ggsave(paste0(inpF,selection,selThr,selThrFC,cvThr,lGroup,"HeatMap.svg"), svgPHC)
 #test####
-testT <- function(log2LFQt,sel1,sel2,cvThr,testName){
-  #sel1<-"Omego1"
-  #sel2<-"Cntr1"
-  #log2LFQt<-log2LFQ
-  #testName<-"paired"
-  colnames(log2LFQt)
-  #log2LFQt<-sapply(log2LFQt, as.numeric)
-  d1<-log2LFQt[,gsub("-",".",rownames(label[label$pair2test==sel1,]))]
-  d2<-log2LFQt[,gsub("-",".",rownames(label[label$pair2test==sel2,]))]
+testT <- function(log2LFQ,sel1,sel2,cvThr){
+  #sel1<-"MNPHCKO"#"MNPSC"
+  #sel2<-"MNPHC"
+  #log2LFQ<-log2LFQsel#[,gsub("-",".",rownames(label[label$Remove!="Y",]))]
+  #log2LFQ<-sapply(log2LFQ, as.numeric)
+  #colnames(log2LFQ)
+  d1<-log2LFQ[,gsub("-",".",rownames(label[label$pair2test==sel1,]))]
+  d2<-log2LFQ[,gsub("-",".",rownames(label[label$pair2test==sel2,]))]
   dataSellog2grpTtest<-as.matrix(cbind(d1,d2))
   if(sum(!is.na(d1))>1&sum(!is.na(d2))>1){
     hist(d1,breaks=round(max(dataSellog2grpTtest,na.rm=T)))
@@ -99,18 +121,18 @@ testT <- function(log2LFQt,sel1,sel2,cvThr,testName){
     mCol<-ncol(d1)#ceiling((eCol-sCol+1)/2)
     dim(dataSellog2grpTtest)
     options(nwarnings = 1000000)
-    d1c<-d1
-    d2c<-d2
-    d1c[is.na(d1c)]<-0
-    d2c[is.na(d2c)]<-0
-    logFCt =d1c-d2c
-    logFCt[logFCt==0]<-NA
     pValNA = apply(
-      logFCt, 1, function(x)
-      if(sum(!is.na(x))<2){NA}
-      #else if(sum(!is.na(x))<2){1}
-      else{
-        t.test(as.numeric(x),na.rm=T)$p.value}
+      dataSellog2grpTtest, 1, function(x)
+        if(sum(!is.na(x[c(sCol:mCol)]))<2&sum(!is.na(x[c((mCol+1):eCol)]))<2){NA}
+      else if(sum(is.na(x[c(sCol:mCol)]))==0&sum(is.na(x[c((mCol+1):eCol)]))==0){
+        t.test(as.numeric(x[c(sCol:mCol)]),as.numeric(x[c((mCol+1):eCol)]),var.equal=T)$p.value}
+      else if(sum(!is.na(x[c(sCol:mCol)]))>1&sum(!is.na(x[c((mCol+1):eCol)]))<1&(sd(x[c(sCol:mCol)],na.rm=T)/mean(x[c(sCol:mCol)],na.rm=T))<cvThr){0}
+      else if(sum(!is.na(x[c(sCol:mCol)]))<1&sum(!is.na(x[c((mCol+1):eCol)]))>1&(sd(x[c((mCol+1):eCol)],na.rm=T)/mean(x[c((mCol+1):eCol)],na.rm=T))<cvThr){0}
+      else if(sum(!is.na(x[c(sCol:mCol)]))>=2&sum(!is.na(x[c((mCol+1):eCol)]))>=1){
+        t.test(as.numeric(x[c(sCol:mCol)]),as.numeric(x[c((mCol+1):eCol)]),na.rm=T,var.equal=T)$p.value}
+      else if(sum(!is.na(x[c(sCol:mCol)]))>=1&sum(!is.na(x[c((mCol+1):eCol)]))>=2){
+        t.test(as.numeric(x[c(sCol:mCol)]),as.numeric(x[c((mCol+1):eCol)]),na.rm=T,var.equal=T)$p.value}
+      else{NA}
     )
     summary(warnings())
     hist(pValNA)
@@ -127,25 +149,27 @@ testT <- function(log2LFQt,sel1,sel2,cvThr,testName){
     hist(pValBHna)
     pValBHnaMinusLog10 = -log10(pValBHna+.Machine$double.xmin)
     hist(pValBHnaMinusLog10)
-    logFCmedianGrp1 = apply(dataSellog2grpTtest[,c(sCol:mCol)],1, function(x) median(x,na.rm=T))
     logFCmedianGrp1=if(is.null(dim(dataSellog2grpTtest[,c(sCol:mCol)]))){dataSellog2grpTtest[,c(sCol:mCol)]} else{apply(dataSellog2grpTtest[,c(sCol:mCol)],1,function(x) median(x,na.rm=T))}
+    grp1CV=if(is.null(dim(dataSellog2grpTtest[,c(sCol:mCol)]))){dataSellog2grpTtest[,c(sCol:mCol)]} else{apply(dataSellog2grpTtest[,c(sCol:mCol)],1,function(x) sd(x,na.rm=T)/mean(x,na.rm=T))}
     #summary(logFCmedianGrp11-logFCmedianGrp1)
     logFCmedianGrp2=if(is.null(dim(dataSellog2grpTtest[,c((mCol+1):eCol)]))){dataSellog2grpTtest[,c((mCol+1):eCol)]} else{apply(dataSellog2grpTtest[,c((mCol+1):eCol)],1,function(x) median(x,na.rm=T))}
+    grp2CV=if(is.null(dim(dataSellog2grpTtest[,c((mCol+1):eCol)]))){dataSellog2grpTtest[,c((mCol+1):eCol)]} else{apply(dataSellog2grpTtest[,c((mCol+1):eCol)],1,function(x) sd(x,na.rm=T)/mean(x,na.rm=T))}
     logFCmedianGrp1[is.na(logFCmedianGrp1)]=0
     logFCmedianGrp2[is.na(logFCmedianGrp2)]=0
     hda<-cbind(logFCmedianGrp1,logFCmedianGrp2)
     plot(hda)
     limma::vennDiagram(hda>0)
-    logFCmedian = apply(logFCt,1,function(x) median(x,na.rm=T))
-    logFCaverage = apply(logFCt,1,function(x) mean(x,na.rm=T))
+    logFCmedian = logFCmedianGrp1-logFCmedianGrp2
     logFCmedianFC = 2^(logFCmedian+.Machine$double.xmin)
     logFCmedianFC=squish(logFCmedianFC,c(0.01,100))
     hist(logFCmedianFC)
     log2FCmedianFC=log2(logFCmedianFC)
     hist(log2FCmedianFC)
-    ttest.results = data.frame(Uniprot=rowName,Gene=data$Gene.names,Protein=data$Protein.names,PValueMinusLog10=pValNAminusLog10,FoldChanglog2median=logFCmedianFC,logFCmedianGrp1,logFCmedianGrp2,tTestPval=pValNA,CorrectedPValueBH=pValBHna,Log2MedianChange=logFCmedian,Log2AverageChange=logFCaverage,dataSellog2grpTtest,RowGeneUniProtScorePeps=rownames(dataSellog2grpTtest))
-    writexl::write_xlsx(ttest.results,paste0(inpF,selection,sCol,eCol,comp,selThr,selThrFC,cvThr,lGroup,testName,"tTestBH.xlsx"))
-    write.csv(ttest.results,paste0(inpF,selection,sCol,eCol,comp,selThr,selThrFC,cvThr,lGroup,testName,"tTestBH.csv"),row.names = F)
+    ttest.results = data.frame(Uniprot=rowName,Gene=data$Gene.names,Protein=data$Protein.names,logFCmedianGrp1,logFCmedianGrp2,PValueMinusLog10=pValNAminusLog10,FoldChanglog2median=logFCmedianFC,CorrectedPValueBH=pValBHna,TtestPval=pValNA,dataSellog2grpTtest,Log2MedianChange=logFCmedian,grp1CV,grp2CV,RowGeneUniProtScorePeps=rownames(dataSellog2grpTtest))
+    writexl::write_xlsx(ttest.results,paste0(inpF,selection,sCol,eCol,comp,selThr,selThrFC,cvThr,lGroup,rGroup,lName,"tTestBH.xlsx"))
+    write.csv(ttest.results,paste0(inpF,selection,sCol,eCol,comp,selThr,selThrFC,cvThr,lGroup,rGroup,lName,"tTestBH.csv"),row.names = F)
+    ttest.results.return<-ttest.results
+    #volcano
     ttest.results$RowGeneUniProtScorePeps<-data$geneName
     ttest.results[is.na(ttest.results)]=selThr
     Significance=ttest.results$CorrectedPValueBH<selThr&ttest.results$CorrectedPValueBH>0&abs(ttest.results$Log2MedianChange)>selThrFC
@@ -155,15 +179,24 @@ testT <- function(log2LFQt,sel1,sel2,cvThr,testName){
     p<-p + ggplot2::theme_bw(base_size=8) + ggplot2::geom_text(data=dsub,ggplot2::aes(label=RowGeneUniProtScorePeps),hjust=0, vjust=0,size=1,position=ggplot2::position_jitter(width=0.5,height=0.1)) + ggplot2::scale_fill_gradient(low="white", high="darkblue") + ggplot2::xlab("Log2 Median Change") + ggplot2::ylab("-Log10 P-value")
     #f=paste(file,proc.time()[3],".jpg")
     #install.packages("svglite")
-    ggplot2::ggsave(paste0(inpF,selection,sCol,eCol,comp,selThr,selThrFC,cvThr,lGroup,testName,"VolcanoTest.svg"), p)
+    ggplot2::ggsave(paste0(inpF,selection,sCol,eCol,comp,selThr,selThrFC,cvThr,lGroup,rGroup,lName,"VolcanoTest.svg"), p)
     print(p)
-    return(ttest.results)
+    return(ttest.results.return)
   }
 }
-#testttOmego1Cntr12####
+#compare####
+colnames(log2LFQ)
+log2LFQsel=log2LFQ[,gsub("-",".",rownames(label[is.na(label$removed)|label$removed==" "|label$removed=='',]))]
+colnames(log2LFQsel)
+dim(log2LFQsel)
+label=label[is.na(label$removed)|label$removed==" "|label$removed=='',]
 table(label$pair2test)
-rownames(label[label$pair2test=="Omego1",])
-rownames(label[label$pair2test=="Cntr1",])
-label<-label[order(label$Rep),]
-log2LFQs <- log2LFQ[,order(label$Rep)]
-ttOmego1Cntr12=testT(log2LFQs,"Omego1","Cntr1",cvThr,testName)
+for(i in rownames(table(label$pair2test))){
+  for(j in rownames(table(label$pair2test))){
+    if(i!=j){
+      print(paste(i,j))
+      ttPair=testT(log2LFQsel,i,j,cvThr)
+      #assign(paste0(i,j),ttPair)
+    }
+  }
+}
