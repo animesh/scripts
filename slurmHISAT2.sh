d7f942ab93fed3168416a60ee87adad277f71d94
ani <animesh.sharma@ntnu.no>
2023-07-07 12:21:45 +0200

    run microbiome data against human using hisat2

--- slurmHISAT2.sh
+++ slurmHISAT2.sh
@@ -1,4 +1,5 @@
-#perl -pi -e's/\015\012/\012/g' slurmHISAT2.sh
+#git checkout 6e2f060162f001dc7e98492f1a5f37f3a2d2162a slurmHISAT2.sh scratch.slurm
+#dos2unix slurmHISAT2.sh scratch.slurm#perl -pi -e's/\015\012/\012/g' slurmHISAT2.sh
 #bash slurmHISAT2.sh /cluster/projects/nn9036k/rnaSeqChk /cluster/projects/nn9036k/rnaSeqChk/grch38_tran/genome_tran
 DATADIR=$1
 MAPFILE=$2
@@ -8,6 +9,6 @@ CPU=40
 CURRENTEPOCTIME=`date +%s`
 WRITEDIR=$RUNCMD.$CURRENTEPOCTIME.results
 mkdir $DATADIR/$WRITEDIR
-for i in $DATADIR/*fastq.gz ; do echo $i; j=$(basename $i); k=${j%%.fastq*}; echo  $k ; sed "s|seqRNA|$k|g" $PARAMFILE > $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 ; 	sed "s|FASTFILE|$i|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 ; sed "s|FASTFILE|$DATADIR/$WRITEDIR/$k|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 ; sed "s|HISATGENOMEDIR|$MAPFILE|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 > $DATADIR/$WRITEDIR/$k.$PARAMFILE ; rm $DATADIR/$WRITEDIR/$PARAMFILE.tmp*  ; cat $DATADIR/$WRITEDIR/$k.$PARAMFILE ; echo $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; sbatch $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; done
-
+for i in $DATADIR/*.fastq* ; do echo $i; j=$(basename $i); k=${j%%.fastq*}; echo  $k ; sed "s|seqRNA|$k.$RUNCMD.$CPU|g" $PARAMFILE > $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 ; 	sed "s|FASTFILE|$i|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 ; sed "s|FASTFILE|$DATADIR/$WRITEDIR/$k|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 ; sed "s|HISATGENOMEDIR|$MAPFILE|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 > $DATADIR/$WRITEDIR/$k.$PARAMFILE ; rm $DATADIR/$WRITEDIR/$PARAMFILE.tmp*  ; cat $DATADIR/$WRITEDIR/$k.$PARAMFILE ; echo $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; sbatch $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; done
+echo $DATADIR/$WRITEDIR
 
