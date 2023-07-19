7243207be51cffa7633344530419e2790d382afb
ani <animesh.sharma@ntnu.no>
2023-07-19 15:58:02 +0200

    count all reads in paired mapping

--- slurmSR.sh
+++ slurmSR.sh
@@ -1,6 +1,6 @@
-#git checkout 8da41fec0e5f683e9d6b1b0a18e5cad84d3c7c59 slurmSR.sh scratch.slurm
-#dos2unix slurmSR.sh scratch.slurm#perl -pi -e's/\015\012/\012/g' slurmSR.sh
-#bash slurmSR.sh /cluster/projects/nn9036k/rnaSeqChk/hisat2.1688649889.results/samtools.1688653985.results/Homo_sapiens.GRCh38.109.gtf $PWD/AYU/hisat2.1688724920.results/samtools.1688725428.results
+#git checkout 71a2f5b100d3cc6c3e543098d234ebaafdd88d4e slurmSR.sh
+#dos2unix slurmSR.sh
+#bash slurmSR.sh /cluster/projects/nn9036k/rnaSeqChk/hisat2.1688649889.results/samtools.1688653985.results/Homo_sapiens.GRCh38.109.gtf  TK/hisat2.1689410858.results/samtools.1689594992.results
 RUNCMD=subread
 MAPFILE=$1
 mfj=$(basename $MAPFILE); 
@@ -17,8 +17,9 @@ mkdir $DATADIR/$WRITEDIR
 sed "s|SLURMJOB|$RUNCMD.$mfk.$CPU|g" $PARAMFILE > $DATADIR/$WRITEDIR/$PARAMFILE
 echo "module purge" >> $DATADIR/$WRITEDIR/$PARAMFILE
 echo "module load Subread/2.0.3-GCC-11.2.0" >> $DATADIR/$WRITEDIR/$PARAMFILE
-echo "featureCounts -T $CPU -p --countReadPairs -C -t exon -g gene_id -O -a $MAPFILE -o $DATADIR/$WRITEDIR/$mfk.$CPU.countOnlyPairs.txt $SORTBAMFILES" >> $DATADIR/$WRITEDIR/$PARAMFILE
 
+echo "featureCounts -T $CPU -p -t exon -g gene_id -O -a $MAPFILE -o $DATADIR/$WRITEDIR/$mfk.$CPU.countOnlyPairs.txt $SORTBAMFILES" >> $DATADIR/$WRITEDIR/$PARAMFILE
+#echo "featureCounts -T $CPU -p --countReadPairs -C -t exon -g gene_id -O -a $MAPFILE -o $DATADIR/$WRITEDIR/$mfk.$CPU.countOnlyPairs.txt $SORTBAMFILES" >> $DATADIR/$WRITEDIR/$PARAMFILE
 cat $DATADIR/$WRITEDIR/$PARAMFILE
 echo $DATADIR/$WRITEDIR/$PARAMFILE
 sbatch $DATADIR/$WRITEDIR/$PARAMFILE
