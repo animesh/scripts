988bb0621204d9bb9cc27121356c4758386489fd
Animesh Sharma <ash022@login-1.saga>
2023-07-27 16:47:42 +0200

    qualimap TK/multibamqc

--- slurmHISAT2.sh
+++ slurmHISAT2.sh
@@ -1,4 +1,4 @@
-#git checkout a8968108825874dc8d658697ad4dd8d5190bc149  scratch.slurm
+#git checkout 92ab61e6b132b843a1fc5c2b3578566205a52a13 scratch.slurm
 #dos2unix slurmHISAT2.sh scratch.slurm
 #bash slurmHISAT2.sh $PWD/TK $PWD/hg38v110/genome_tran
 DATADIR=$1
@@ -11,4 +11,4 @@ WRITEDIR=$RUNCMD.$CURRENTEPOCTIME.results
 mkdir $DATADIR/$WRITEDIR
 for i in $DATADIR/*1.fq.gz ; do echo $i; i2=${i/%1.fq.gz/2.fq.gz} ; echo $i2; j=$(basename $i); echo $j; k=${j%%1.fq*}; echo  $k ; sed "s|seqRNA|$k.$RUNCMD.$CPU|g" $PARAMFILE > $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 ; 	sed "s|FASTFILE1|$i|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 ;  sed "s|FASTFILE2|$i2|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 ;  sed "s|FASTFILE|$DATADIR/$WRITEDIR/$k|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp4 ; sed "s|HISATGENOMEDIR|$MAPFILE|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp4 > $DATADIR/$WRITEDIR/$k.$PARAMFILE ; rm $DATADIR/$WRITEDIR/$PARAMFILE.tmp*  ; cat $DATADIR/$WRITEDIR/$k.$PARAMFILE ; echo $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; sbatch $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; done
 echo $DATADIR/$WRITEDIR
-
+#cat   /cluster/home/ash022/scripts/TK/hisat2.1690116069.results/*.scratch.slurm | grep "hisat2 -p" | awk '{print $5$7}'
--- slurmSTAR.sh
+++ slurmSTAR.sh
@@ -1,6 +1,6 @@
-#git checkout d7f942ab93fed3168416a60ee87adad277f71d94 slurmHISAT2.sh scratch.slurm
-#dos2unix slurmHISAT2.sh scratch.slurm#perl -pi -e's/\015\012/\012/g' slurmHISAT2.sh
-#bash slurmHISAT2.sh $PWD/AYU /cluster/projects/nn9036k/rnaSeqChk/grch38_tran/genome_tran
+#git checkout 069e21a60b8255c8e0c493413565ace1b92e84e3 scratch.slurm
+#dos2unix scratch.slurm
+#bash slurmSTAR.sh $PWD/TK hg38v110star/hg38_index_len150
 DATADIR=$1
 MAPFILE=$2
 RUNCMD=star
@@ -11,4 +11,4 @@ WRITEDIR=$RUNCMD.$CURRENTEPOCTIME.results
 mkdir $DATADIR/$WRITEDIR
 for i in $DATADIR/*1.fq.gz ; do echo $i; i2=${i/%1.fq.gz/2.fq.gz} ; echo $i2; j=$(basename $i); echo $j; k=${j%%1.fq*}; echo  $k ; sed "s|seqRNA|$k.$RUNCMD.$CPU|g" $PARAMFILE > $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 ; 	sed "s|FASTFILE1|$i|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 ;  sed "s|FASTFILE2|$i2|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 ;  sed "s|FASTFILE|$DATADIR/$WRITEDIR/$k|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp4 ; sed "s|STARGENOMEDIR|$MAPFILE|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp4 > $DATADIR/$WRITEDIR/$k.$PARAMFILE ; rm $DATADIR/$WRITEDIR/$PARAMFILE.tmp*  ; cat $DATADIR/$WRITEDIR/$k.$PARAMFILE ; echo $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; sbatch $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; done
 echo $DATADIR/$WRITEDIR
-
+#cat  /cluster/home/ash022/scripts/TK/star.1690114446.results/*slurm |grep "readFilesIn"
