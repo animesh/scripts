b4076c724842b68ffb908f1f43cbcd33c7770653
Animesh Sharma <animesh.sharma@ntnu.no>
2023-09-14 11:20:46 +0200

    correlation with pH values precision at 3 decimal place

--- diffExprTestCor.r
+++ diffExprTestCor.r
@@ -1,5 +1,5 @@
 #install.packages(c("readxl","writexl","svglite","ggplot2"),repos="http://cran.us.r-project.org",lib=.libPaths())
-#F:\R-4.3.1\bin\Rscript.exe diffExprTestCor.r "L:\promec\TIMSTOF\LARS\2023\230310 Sonali\combined\txtNoDN\proteinGroups.txt" "L:\promec\TIMSTOF\LARS\2023\230310 Sonali\combined\txtNoDN\SurvivalUpdates_other parameters.xlsx" "LFQ.intensity." "Group" "Remove" "pH"
+#F:\R-4.3.1\bin\Rscript.exe diffExprTestCor.r "L:\promec\TIMSTOF\LARS\2023\230310 Sonali\combined\txtNoDN\proteinGroups.txt" "L:\promec\TIMSTOF\LARS\2023\230310 Sonali\combined\txtNoDN\SurvivalUpdates_other parameters_new_14092023.xlsx" "LFQ.intensity." "Group" "Remove" "pH"
 print("USAGE:<path to>Rscript diffExprTestCor.r <complete path to directory containing proteinGroups.txt> AND <SurvivalUpdates.xlsx file>  \"intensity columns to consider\" \"Group information of samples\" \"Remove samples if any\" \"correlation column\"")
 args = commandArgs(trailingOnly=TRUE)
 print(paste("supplied argument(s):", length(args)))
@@ -14,7 +14,7 @@ print(paste("Thresholds used - ", thr ,"#count-valid-samples," ,selThr,"#pValue-
 inpF <- args[1]
 #inpF <-"L:/promec/TIMSTOF/LARS/2023/230310 Sonali//combined/txtNoDN/proteinGroups.txt"
 inpL <- args[2]
-#inpL <-"L:/promec/TIMSTOF/LARS/2023/230310 Sonali//combined/txtNoDN/SurvivalUpdates_other parameters.xlsx"
+#inpL <-"L:/promec/TIMSTOF/LARS/2023/230310 Sonali//combined/txtNoDN/SurvivalUpdates_other parameters_new_14092023.xlsx"
 selection<-args[3]
 #selection<-"LFQ.intensity."
 lGroup <- args[4]
