d0c2421846c29bbb3facddd46b16511f4537649b
Animesh Sharma <ash022@login-1.saga>
2023-10-19 14:23:52 +0200

    hisat2 tk9

--- slurmHISAT2.sh
+++ slurmHISAT2.sh
@@ -1,6 +1,8 @@
-#git checkout 92ab61e6b132b843a1fc5c2b3578566205a52a13 scratch.slurm
+#git checkout a8968108825874dc8d658697ad4dd8d5190bc149 slurmHISAT2.sh scratch.slurm
+#git checkout d7f942ab93fed3168416a60ee87adad277f71d94 slurmHISAT2.sh scratch.slurm
 #dos2unix slurmHISAT2.sh scratch.slurm
-#bash slurmHISAT2.sh $PWD/TK $PWD/CHM13v2-T2T/genome_tran
+#perl -pi -e's/\015\012/\012/g' slurmHISAT2.sh scratch.slurm
+#bash slurmHISAT2.sh $PWD/TK9 /cluster/projects/nn9036k/rnaSeqChk/grch38_tran/genome_tran
 DATADIR=$1
 MAPFILE=$2
 RUNCMD=hisat2
@@ -9,6 +11,6 @@ CPU=40
 CURRENTEPOCTIME=`date +%s`
 WRITEDIR=$RUNCMD.$CURRENTEPOCTIME.results
 mkdir $DATADIR/$WRITEDIR
-for i in $DATADIR/*1.fq.gz ; do echo $i; i2=${i/%1.fq.gz/2.fq.gz} ; echo $i2; j=$(basename $i); echo $j; k=${j%%1.fq*}; echo  $k ; sed "s|seqRNA|$k.$RUNCMD.$CPU|g" $PARAMFILE > $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 ; 	sed "s|FASTFILE1|$i|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 ;  sed "s|FASTFILE2|$i2|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 ;  sed "s|FASTFILE|$DATADIR/$WRITEDIR/$k|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp4 ; sed "s|HISATGENOMEDIR|$MAPFILE|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp4 > $DATADIR/$WRITEDIR/$k.$PARAMFILE ; rm $DATADIR/$WRITEDIR/$PARAMFILE.tmp*  ; cat $DATADIR/$WRITEDIR/$k.$PARAMFILE ; echo $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; sbatch $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; done
+for i in $DATADIR/*1.fastq* ; do echo $i; i2=${i/%1.fastq/2.fastq} ; echo $i2; j=$(basename $i); echo $j; k=${j%%1.fastq*}; echo  $k ; sed "s|seqRNA|$k.$RUNCMD.$CPU|g" $PARAMFILE > $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 ; 	sed "s|FASTFILE1|$i|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 ;  sed "s|FASTFILE2|$i2|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 ;  sed "s|FASTFILE|$DATADIR/$WRITEDIR/$k|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp4 ; sed "s|HISATGENOMEDIR|$MAPFILE|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp4 > $DATADIR/$WRITEDIR/$k.$PARAMFILE ; rm $DATADIR/$WRITEDIR/$PARAMFILE.tmp*  ; cat $DATADIR/$WRITEDIR/$k.$PARAMFILE ; echo $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; sbatch $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; done
 echo $DATADIR/$WRITEDIR
 
