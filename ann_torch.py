fa0ce647bfee4b796a8ab235906e1b49da1ed47b
Animesh Sharma <animesh.sharma@ntnu.no>
2023-10-23 20:09:15 +0200

    subread feature-count with tk9 and its trimmed hisat alignment

--- ann_torch.py
+++ ann_torch.py
@@ -80,3 +80,23 @@ while iter<0:
     #w2-=lr*np.outer(grad,h)
     print(w2)
 print("Time taken:", datetime.now() - startTime)
+
+#https://alphasignalai.beehiiv.com/p/gpulevel-inference-cpu?utm_source=alphasignalai.beehiiv.com&utm_medium=newsletter&utm_campaign=gpu-level-inference-on-your-cpu
+from torch.utils.tensorboard import SummaryWriter
+import torchvision
+
+# Init writer and model
+writer = SummaryWriter('runs/demo')
+model = torchvision.models.resnet50()
+dummy_data, _ = load_dataset()
+
+# Add model graph
+writer.add_graph(model, dummy_data)
+
+# Fake training loop for demo
+for epoch in range(5):
+    loss = epoch * 0.1  # Simulated loss
+    writer.add_scalar('train_loss', loss, epoch)
+
+# Close writer
+writer.close()
