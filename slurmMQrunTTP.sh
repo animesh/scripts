a1802bd41482bbe2194e11e329f9a1c18e63a522
Animesh Sharma <ash022@login-1.saga>
2023-12-03 11:43:07 +0100

    MaxDIA over saga for K8R10 DDA peptides

--- slurmMQrunTTP.sh
+++ slurmMQrunTTP.sh
@@ -1,11 +1,7 @@
-#git checkout 6635da4904bd62acf5f811ad7813524b96e5e348 slurmMQrunTTP.sh scratch.slurm mqparTTPdda.xml  
+#git checkout 5219840f97be1d57690b7fc36ed2144df4691d5c slurmMQrunTTP.sh scratch.slurm mqparTTPdda.xml  
 #dos2unix slurmMQrunTTP.sh scratch.slurm mqparTTPdda.xml
-#bash slurmMQrunTTP.sh /cluster/projects/nn9036k/MaxQuant_v_2.4.10.0/bin/MaxQuantCmd.exe /nird/projects/NS9036K//NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2023/231128_plasma_KF /cluster/projects/nn9036k/FastaDB mqparTTPdda.xml scratch.slurm
-#for i in  mqparTTPdda.xml.1701596769.results/*/*.slurm ; do echo $i ; dos2unix $i slurmMQrunTTP.sh ; sbatch $i ; done
-#mkdir -p 231128_plasma_KF/DIA
-#rsync -Parv login.nird-lmd.sigma2.no:/nird/projects/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2023/231128_plasma_KF/*dia*.d 231128_plasma_KF/DIA/.
-#bash slurmMQrunTTP.sh /cluster/projects/nn9036k/MaxQuant_v_2.4.10.0/bin/MaxQuantCmd.exe /cluster/projects/nn9036k/scripts/231128_plasma_KF/DIA /cluster/projects/nn9036k/FastaDB mqparTTPdia.xml scratch.slurm
-#for i in  mqparTTPdia.xml.1701594417.results/*/*.slurm ; do echo $i ; dos2unix $i slurmMQrunTTP.sh ; sbatch $i ; done
+#bash slurmMQrunTTP.sh /cluster/projects/nn9036k/MaxQuant_v_2.4.10.0/bin/MaxQuantCmd.exe /nird/projects/NS9036K//NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2023/231123_dia_dda /cluster/projects/nn9036k/FastaDB mqparTTPdda.xml scratch.slurm
+#for i in  mqparTTPdda.xml.1701596940.results/*/*.slurm ; do echo $i ; dos2unix $i slurmMQrunTTP.sh ; sbatch $i ; done
 #wget "https://maxquant.org/p/maxquant/MaxQuant_v_2.4.10.0.zip?md5=bwPYOvsI5oXolBP_wXUyzg&expires=1700127616" -O MQv24100.zip
 #unzip MQv24100.zip
 #wget "https://datashare.biochem.mpg.de/s/qe1IqcKbz2j2Ruf/download?path=%2FDiscoveryLibraries&files=homo_sapiens.zip" -O HS.DIA.zip
@@ -13,10 +9,6 @@
 #mv *.fasta FastaDB/.
 #mv *.txt FastaDB/.
 #mv MaxQuant_v_1.4.10.0 MaxQuant_v_2.4.10.0
-#cd scripts/
-#mkdir plasma
-#rsync -Parv login.nird-lmd.sigma2.no:PD/TIMSTOF/LARS/2023/231030_hela_peptides/DIA/*.d plasma/
-#cat scratch.slurm
 MAXQUANTCMD=$1
 DATADIR=$2
 FASTADIR=$3
