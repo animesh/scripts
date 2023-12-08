0f886b6ea60a2787194ffe2203875b284865414c
Animesh Sharma <ash022@login-1.saga>
2023-12-08 16:18:03 +0100

    ln -s TK sorted bam files and run count features with subread

--- slurmSR.sh
+++ slurmSR.sh
@@ -1,8 +1,68 @@
 #git checkout 8c9b98c1ef58fa67355743da3bf78516467fd85f scratch.slurm slurmSR.sh
 #dos2unix  scratch.slurm slurmSR.sh
-#cat slurmSR.sh
-#ls -ltrh /cluster/projects/nn9036k/scripts/TK/mergeHISAT/ >> slurmSR.sh
-#bash slurmSR.sh hg38v110/Homo_sapiens.GRCh38.110.gtf /cluster/projects/nn9036k/scripts/TK/mergeHISAT 
+#bash slurmSR.sh $PWD/hg38v110/Homo_sapiens.GRCh38.110.gtf /cluster/projects/nn9036k/scripts/TK/SB
+#rename 'sort' 'h8001s8338sort' $PWD/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_*sort.bam
+#rename 'sort' 'h6943s4795sort' $PWD/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_*sort.bam
+#mkdir TK/SB
+#ln -s /cluster/projects/nn9036k/scripts/TK9trim/hisat2.1697881193.results/samtools.1697919237.results/ln -s /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/*.sort.bam $PWD/TK/SB/
+#ln -s $PWD/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_*sort.bam $PWD/TK/SB/
+#ln -s $PWD/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_*sort.bam $PWD/TK/SB/
+#ln -s $PWD/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9*sort.bam $PWD/TK/SB/
+#ls -ltrh TK/SB
+#TK18_R2_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK18_R2_.sort.bam
+#TK18_R1_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK18_R1_.sort.bam
+#TK16_R3_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK16_R3_.sort.bam
+#TK16_R2_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK16_R2_.sort.bam
+#TK16_R1_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK16_R1_.sort.bam
+#TK14_3_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK14_3_.sort.bam
+#TK14_2_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK14_2_.sort.bam
+#TK14_1_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK14_1_.sort.bam
+#TK13_3_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK13_3_.sort.bam
+#TK13_2_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK13_2_.sort.bam
+#TK13_1_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK13_1_.sort.bam
+#TK12_R3_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK12_R3_.sort.bam
+#TK12_R2_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK12_R2_.sort.bam
+#TK12_R1_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK12_R1_.sort.bam
+#TK10_51_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK10_51_.sort.bam
+#TK10_50_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK10_50_.sort.bam
+#TK10_49_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK10_49_.sort.bam
+#TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L003_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L003_.h8001s8338sort.bam
+#TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L002_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L002_.h8001s8338sort.bam
+#TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L006_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L006_.h8001s8338sort.bam
+#TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L005_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L005_.h8001s8338sort.bam
+#TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L003_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L003_.h8001s8338sort.bam
+#TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L002_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L002_.h8001s8338sort.bam
+#TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L006_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L006_.h8001s8338sort.bam
+#TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L005_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L005_.h8001s8338sort.bam
+#TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L003_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L003_.h8001s8338sort.bam
+#TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L002_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L002_.h8001s8338sort.bam
+#TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L006_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L006_.h8001s8338sort.bam
+#TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L005_.h8001s8338sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results/samtools.1701964661.results/TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L005_.h8001s8338sort.bam
+#TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L003_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L003_.h6943s4795sort.bam
+#TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L002_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L002_.h6943s4795sort.bam
+#TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L006_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L006_.h6943s4795sort.bam
+#TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L005_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L005_.h6943s4795sort.bam
+#TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L003_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L003_.h6943s4795sort.bam
+#TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L002_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L002_.h6943s4795sort.bam
+#TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L006_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L006_.h6943s4795sort.bam
+#TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L005_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L005_.h6943s4795sort.bam
+#TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L003_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L003_.h6943s4795sort.bam
+#TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L002_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L002_.h6943s4795sort.bam
+#TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L006_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L006_.h6943s4795sort.bam
+#TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L005_.h6943s4795sort.bam -> /cluster/home/ash022/scripts/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results/samtools.1701964795.results/TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L005_.h6943s4795sort.bam
+#TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L006__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L006__.sort.bam
+#TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L005__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L005__.sort.bam
+#TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L003__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L003__.sort.bam
+#TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L002__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_3_22GH5GLT3_GGCGCCATTG-ACCGCGCAAT_L002__.sort.bam
+#TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L006__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L006__.sort.bam
+#TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L005__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L005__.sort.bam
+#TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L003__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L003__.sort.bam
+#TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L002__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_2_22GH5GLT3_GATATTGTGT-ACCACACGGT_L002__.sort.bam
+#TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L006__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L006__.sort.bam
+#TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L005__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L005__.sort.bam
+#TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L003__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L003__.sort.bam
+#TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L002__.sort.bam -> /cluster/home/ash022/scripts/TK9/hisat2.1701602282.results/samtools.1702035152.results/TK9_1_22GH5GLT3_AGAGAACCTA-GGTTATGCTA_L002__.sort.bam
+#ls -ltrh /cluster/projects/nn9036k/scripts/TK/SB/ >> slurmSR.sh
 RUNCMD=subread
 MAPFILE=$1
 mfj=$(basename $MAPFILE); 
@@ -37,33 +97,3 @@ cat $DATADIR/$WRITEDIR/countP.$PARAMFILE
 echo $DATADIR/$WRITEDIR/countP.$PARAMFILE
 sbatch $DATADIR/$WRITEDIR/countP.$PARAMFILE
 
-#lrwxrwxr-x 1 ash022 nn9036k 145 Oct 21 10:33 TK9_1L6_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9/hisat2.1697716972.results/samtools.1697718610.results/TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L006_R.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 145 Oct 21 10:33 TK9_1L5_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9/hisat2.1697716972.results/samtools.1697718610.results/TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L005_R.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 145 Oct 21 10:33 TK9_2L6_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9/hisat2.1697716972.results/samtools.1697718610.results/TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L006_R.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 145 Oct 21 10:33 TK9_2L5_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9/hisat2.1697716972.results/samtools.1697718610.results/TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L005_R.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 145 Oct 21 10:33 TK9_3L5_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9/hisat2.1697716972.results/samtools.1697718610.results/TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L005_R.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 145 Oct 21 10:33 TK9_3L6_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9/hisat2.1697716972.results/samtools.1697718610.results/TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L006_R.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK10_51_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK10_51_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK10_50_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK10_50_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK10_49_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK10_49_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK16_R3_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK16_R3_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK16_R2_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK16_R2_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK16_R1_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK16_R1_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 107 Oct 21 13:06 TK14_3_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK14_3_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 107 Oct 21 13:06 TK14_2_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK14_2_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 107 Oct 21 13:06 TK14_1_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK14_1_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 107 Oct 21 13:06 TK13_3_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK13_3_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 107 Oct 21 13:06 TK13_2_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK13_2_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 107 Oct 21 13:06 TK13_1_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK13_1_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK12_R3_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK12_R3_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK12_R2_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK12_R2_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK12_R1_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK12_R1_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK18_R3_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK18_R3_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK18_R2_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK18_R2_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 108 Oct 21 13:06 TK18_R1_.sort.bam -> /cluster/projects/nn9036k/scripts/TK/hisat2.1690116069.results/samtools.1690117388.results/TK18_R1_.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 149 Oct 21 22:29 TK9_1Ltrim5_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9trim/hisat2.1697881193.results/samtools.1697919237.results/TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L005_R.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 149 Oct 21 22:29 TK9_3Ltrim6_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9trim/hisat2.1697881193.results/samtools.1697919237.results/TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L006_R.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 149 Oct 21 22:29 TK9_3Ltrim5_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9trim/hisat2.1697881193.results/samtools.1697919237.results/TK9_3_22FFLLLT3_CGTACAGGAA-TAGGTTCTCT_L005_R.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 149 Oct 21 22:29 TK9_2Ltrim6_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9trim/hisat2.1697881193.results/samtools.1697919237.results/TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L006_R.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 149 Oct 21 22:29 TK9_2Ltrim5_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9trim/hisat2.1697881193.results/samtools.1697919237.results/TK9_2_22FFLLLT3_GATATTGTGT-ACCACACGGT_L005_R.sort.bam
-#lrwxrwxr-x 1 ash022 nn9036k 149 Oct 21 22:29 TK9_1Ltrim6_R.sort.bam -> /cluster/projects/nn9036k/scripts/TK9trim/hisat2.1697881193.results/samtools.1697919237.results/TK9_1_22FFLLLT3_AGAGAACCTA-GGTTATGCTA_L006_R.sort.bam
