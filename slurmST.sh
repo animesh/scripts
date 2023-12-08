15047f442dacfe4ba656447f16cc937b245c06b9
Animesh Sharma <ash022@login-1.saga>
2023-12-08 12:26:57 +0100

    convert to bam, sort and index tk9 sam files

--- slurmST.sh
+++ slurmST.sh
@@ -1,6 +1,8 @@
-#git checkout 4d8b6e7e3c6d924c56bf97e5e9cb30bf46e1ea69 scratch.slurm
-#dos2unix scratch.slurm 
-#bash slurmST.sh $PWD/TK9/hisat2.1697716972.results
+#git checkout 53b672d5f9da4ac8bef8076cd54f91a1b8916fe0 scratch.slurm slurmST.sh
+#dos2unix scratch.slurm slurmST.sh
+#bash slurmST.sh $PWD/TK9/trimmomatic.1701618021.results/hisat2.1701897177.results
+#bash slurmST.sh $PWD/TK9/trimmomatic.1701608001.results/hisat2.1701618338.results
+#bash slurmST.sh $PWD/TK9/trimmomatic.1701608001.results/hisat2.1701896943.results
 DATADIR=$1
 RUNCMD=samtools
 PARAMFILE=scratch.slurm
@@ -9,5 +11,4 @@ CURRENTEPOCTIME=`date +%s`
 WRITEDIR=$RUNCMD.$CURRENTEPOCTIME.results
 mkdir $DATADIR/$WRITEDIR
 for i in $DATADIR/*.?am ; do echo $i; j=$(basename $i); k=${j%%.?am*}; echo  $k ; sed "s|seqRNA|$k.$RUNCMD.$CPU|g" $PARAMFILE > $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 ; 	sed "s|SAMFILE|$i|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp1 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 ; sed "s|BAMFILE|$DATADIR/$WRITEDIR/$k.bam|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp2 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 ; sed "s|SORTBAMFILE|$DATADIR/$WRITEDIR/$k.sort.bam|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp3 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp4 ; sed "s|INDEXSORT|$DATADIR/$WRITEDIR/$k.sort.bam|"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp4 > $DATADIR/$WRITEDIR/$PARAMFILE.tmp5 ;  sed "s|NCPU|$CPU|g"  $DATADIR/$WRITEDIR/$PARAMFILE.tmp5 > $DATADIR/$WRITEDIR/$k.$PARAMFILE ;   rm $DATADIR/$WRITEDIR/$PARAMFILE.tmp*  ; cat $DATADIR/$WRITEDIR/$k.$PARAMFILE ; echo $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; sbatch $DATADIR/$WRITEDIR/$k.$PARAMFILE  ; done
-
 
