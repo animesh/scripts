c6d48a8ae1ed5c703abee92555c9b44c91a7d0be
Animesh Sharma <animeshs@ntnu.no>
2024-03-09 11:26:03 +0100

    combine DIA beads data using Ions detectec with PASER

--- proteinGroupsCombineTTP.py
+++ proteinGroupsCombineTTP.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
-#!pip3 install pathlib --user
+#python proteinGroupsCombineTTP.py L:\promec\TIMSTOF\LARS\2024\240202_beads\9947a216a0e94f5c889f6a3a24dbb88b\processing-run
+#!pip3 install pandas matplotlib pathlib --user
 import sys
 from pathlib import Path
-if len(sys.argv)!=2: sys.exit("\n\nREQUIRED: pandas, pathlib, tcl==8.6.9; tested with Python 3.7.9 \n\nUSAGE: python proteinGroupsCombineTTP.py <path to folder containing protein.tsv file(s) like \"L:\promec\Animesh\Samah\mqpar.xml.1623227664.results\" >\n\nExample\n\npython proteinGroupsCombineTTP.py L:\promec\Animesh\Samah\mqpar.xml.1623227664.results")
-#python proteinGroupsCombineTTP.py C:/Users/animeshs/Desktop/FP2
+if len(sys.argv)!=2: sys.exit("\n\nREQUIRED: pandas, pathlib, tcl==8.6.9; tested with Python 3.7.9 \n\nUSAGE: python proteinGroupsCombineTTP.py <path to folder containing protein.tsv file(s) like \"L:\promec\Animesh\Samah\mqpar.xml.1623227664.results\" >\n\nExample\n\npython proteinGroupsCombineTTP.py L:\\promec\\TIMSTOF\\LARS\\2024\\240202_beads")
 pathFiles = Path(sys.argv[1])
 #pathFiles=Path("L:/promec/TIMSTOF/LARS/2024/240202_beads/9947a216a0e94f5c889f6a3a24dbb88b/processing-run")
 fileName='tims-diann.protein.tsv'
@@ -24,18 +23,14 @@ for f in trainList:
         proteinHits.rename({'protein_group_name':'ID'},inplace=True,axis='columns')
         proteinHits.rename({'number_precursors':'Ions'},inplace=True,axis='columns')
         proteinHits=proteinHits.ID.str.split(';', expand=True).set_index(proteinHits.Ions).stack().reset_index(level=0, name='ID')
+        proteinHits=proteinHits.groupby('ID').sum()
         proteinHits['Name']=fName.index[0]
         df=pd.concat([df,proteinHits],sort=False)
 print(df.columns)
 print(df.head())
-df.index=df['ID']
-df.ID=None
-df=df.pivot(index='ID', columns='Name', values='Ions')
+df=df.pivot(columns='Name', values='Ions')
 df['Ions']=df.sum(axis=1)
 df=df.sort_values("Ions", ascending=False)  
-#df.filter(like='Reverse', axis=0)
-dfS=df[df.index.str.contains('Reverse')==False]
-#!pip3 install matplotlib
 plotcsv=pathFiles/("Ions.histogram.svg")
 df.plot(kind='hist',alpha=0.5,bins=100).figure.savefig(plotcsv,dpi=100,bbox_inches = "tight")
 print(df.head())
@@ -43,7 +38,7 @@ print(df.columns)
 #writeDPpng=pathFiles/(fileName+"Score.png")
 #df.plot(kind='hist').figure.savefig(writeDPpng.absolute(),dpi=100,bbox_inches = "tight")
 #print("Histogram of Score in",writeDPpng)
-writeScores=pathFiles/("PSMs.sum.csv")
+writeScores=pathFiles/("Ions.sum.csv")
 df.to_csv(writeScores)#.with_suffix('.combo.csv'))
-print("Ionsin\n",writeScores,"\n",plotcsv)
+print("Ions in\n",writeScores,"\n",plotcsv)
 #dfID=df.assign(ID=df.ID.str.split(';')).explode('ID')
