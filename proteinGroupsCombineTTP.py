97f417bc73d0ecf3cae7b2d4186c91240831aa1f
Animesh Sharma <animeshs@ntnu.no>
2024-03-10 10:28:04 +0100

    combine DDA Kelt data using PSMs detectec with PASER

--- proteinGroupsCombineTTP.py
+++ proteinGroupsCombineTTP.py
@@ -1,11 +1,12 @@
-#python proteinGroupsCombineTTP.py L:\promec\TIMSTOF\LARS\2024\240202_beads\9947a216a0e94f5c889f6a3a24dbb88b\processing-run
+#python proteinGroupsCombineTTP.py L:\promec\TIMSTOF\LARS\2024\240221_Tom_Kelt\4caaf540a6cc92c6dd28e5f87aa2406f\processing-run
 #!pip3 install pandas matplotlib pathlib --user
 import sys
 from pathlib import Path
 if len(sys.argv)!=2: sys.exit("\n\nREQUIRED: pandas, pathlib, tcl==8.6.9; tested with Python 3.7.9 \n\nUSAGE: python proteinGroupsCombineTTP.py <path to folder containing protein.tsv file(s) like \"L:\promec\Animesh\Samah\mqpar.xml.1623227664.results\" >\n\nExample\n\npython proteinGroupsCombineTTP.py L:\\promec\\TIMSTOF\\LARS\\2024\\240202_beads")
 pathFiles = Path(sys.argv[1])
-#pathFiles=Path("L:/promec/TIMSTOF/LARS/2024/240202_beads/9947a216a0e94f5c889f6a3a24dbb88b/processing-run")
-fileName='tims-diann.protein.tsv'
+#pathFiles=Path("L:/promec/TIMSTOF/LARS/2024/240221_Tom_Kelt/4caaf540a6cc92c6dd28e5f87aa2406f/processing-run")
+fileName='dtaselect.protein.tsv'
+summaryFile='summary-results.results.tsv'
 trainList=list(pathFiles.rglob(fileName))
 print(trainList)
 print(len(trainList),"files found in",pathFiles)
@@ -18,27 +19,27 @@ df=pd.DataFrame()
 for f in trainList:
     if Path(f).stat().st_size > 0:
         proteinHits=pd.read_csv(f,low_memory=False,sep='\t')
-        fName=proteinHits.groupby('sample_name').size()
-        print(fName)
+        fName=pd.read_csv(f.parents[0]/summaryFile,sep='\t')
+        print(fName['sample_name'])
         proteinHits.rename({'protein_group_name':'ID'},inplace=True,axis='columns')
-        proteinHits.rename({'number_precursors':'Ions'},inplace=True,axis='columns')
-        proteinHits=proteinHits.ID.str.split(';', expand=True).set_index(proteinHits.Ions).stack().reset_index(level=0, name='ID')
+        proteinHits.rename({'number_psms':'PSMs'},inplace=True,axis='columns')
+        proteinHits=proteinHits.ID.str.split(';', expand=True).set_index(proteinHits.PSMs).stack().reset_index(level=0, name='ID')
         proteinHits=proteinHits.groupby('ID').sum()
-        proteinHits['Name']=fName.index[0]
+        proteinHits['Name']=fName['sample_name'][0]
         df=pd.concat([df,proteinHits],sort=False)
 print(df.columns)
 print(df.head())
-df=df.pivot(columns='Name', values='Ions')
-df['Ions']=df.sum(axis=1)
-df=df.sort_values("Ions", ascending=False)  
-plotcsv=pathFiles/("Ions.histogram.svg")
+df=df.pivot(columns='Name', values='PSMs')
+df['PSMs']=df.sum(axis=1)
+df=df.sort_values("PSMs", ascending=False)  
+plotcsv=pathFiles/("PSMs.histogram.svg")
 df.plot(kind='hist',alpha=0.5,bins=100).figure.savefig(plotcsv,dpi=100,bbox_inches = "tight")
 print(df.head())
 print(df.columns)
 #writeDPpng=pathFiles/(fileName+"Score.png")
 #df.plot(kind='hist').figure.savefig(writeDPpng.absolute(),dpi=100,bbox_inches = "tight")
 #print("Histogram of Score in",writeDPpng)
-writeScores=pathFiles/("Ions.sum.csv")
+writeScores=pathFiles/("PSMs.sum.csv")
 df.to_csv(writeScores)#.with_suffix('.combo.csv'))
-print("Ions in\n",writeScores,"\n",plotcsv)
+print("PSMs in\n",writeScores,"\n",plotcsv)
 #dfID=df.assign(ID=df.ID.str.split(';')).explode('ID')
