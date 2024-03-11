10fe2efe37c271d82a51c85bb15c00c02e16176b
Animesh Sharma <animeshs@ntnu.no>
2024-03-11 13:33:24 +0100

    combine peptides and annotate with protein-groups, combine PSMs using artifact

--- peptideGroupsCombineTTP.py
+++ peptideGroupsCombineTTP.py
@@ -1,50 +1,51 @@
-#!/usr/bin/env python
-#!pip3 install pathlib --user
+#python peptideGroupsCombineTTP.py L:\promec\TIMSTOF\LARS\2024\240221_Tom_Kelt\4caaf540a6cc92c6dd28e5f87aa2406f\processing-run
+#!pip3 install pandas matplotlib pathlib --user
 import sys
 from pathlib import Path
-if len(sys.argv)!=2: sys.exit("\n\nREQUIRED: pandas, pathlib, tcl==8.6.9; tested with Python 3.7.9 \n\nUSAGE: python peptideGroupsCombineTTP.py <path to folder containing peptide_label_quant.tsv file(s) like \"L:\promec\Animesh\Samah\mqpar.xml.1623227664.results\" >\n\nExample\n\npython proteinGroupsCombineTTP.py L:\promec\Animesh\Samah\mqpar.xml.1623227664.results")
-#python proteinGroupsCombineTTP.py C:/Users/animeshs/Desktop/FP2
+if len(sys.argv)!=2: sys.exit("\n\nREQUIRED: pandas, pathlib, tcl==8.6.9; tested with Python 3.7.9 \n\nUSAGE: python proteinGroupsCombineTTP.py <path to folder containing protein.tsv file(s) like \"L:\promec\Animesh\Samah\mqpar.xml.1623227664.results\" >\n\nExample\n\npython proteinGroupsCombineTTP.py L:\\promec\\TIMSTOF\\LARS\\2024\\240202_beads")
 pathFiles = Path(sys.argv[1])
-#pathFiles=Path("L:/promec/TIMSTOF/LARS/2021/SEPTEMBER/SILAC/FP6F")
-fileName='peptide_label_quant.tsv'
+#pathFiles=Path("L:/promec/TIMSTOF/LARS/2024/240221_Tom_Kelt/4caaf540a6cc92c6dd28e5f87aa2406f/processing-run")
+fileName='dtaselect.peptide.tsv'
+proteinGroups='dtaselect.protein.tsv'
+summaryFile='summary-results.results.tsv'
 trainList=list(pathFiles.rglob(fileName))
-#!pip3 install pandas --user
+print(trainList)
+print(len(trainList),"files found in",pathFiles)
+#!pip3 install pandas
 import pandas as pd
 #df = pd.concat(map(pd.read_table, trainList))
 #df.to_csv(pathFiles.with_suffix('.combinedT.txt'),sep="\t")#,rownames=FALSE)
-#f=trainList[0]#Path("C:/Users/animeshs/Desktop/FP2/210902_sudhl5_tot_3_Slot1-30_1_181/peptide_label_quant.tsv")
+#f=trainList[0]
 df=pd.DataFrame()
 for f in trainList:
     if Path(f).stat().st_size > 0:
-        peptideHits=pd.read_csv(f,low_memory=False,sep='\t')
-        print(f.parts[-2])
-        #peptideHits.rename({'Protein':'ID'},inplace=True,axis='columns')
-        #peptideHits.rename({'Median Log2 Ratios HL':'MedianLog2SILAC'},inplace=True,axis='columns')
-        peptideHits['ID']=peptideHits['Protein']+';'+peptideHits['Modified Peptide']#+(peptideHits['Charges']).astype(str)
-        peptideHits.rename({'Log2 Ratio HL':'MedianLog2SILAC'},inplace=True,axis='columns')
-        peptideHits=peptideHits.ID.str.split(' ', expand=True).set_index(peptideHits.MedianLog2SILAC).stack().reset_index(level=0, name='ID')
-        peptideHits['Name']=f.parts[-2]
-        df=pd.concat([df,peptideHits],sort=False)
+        fName=pd.read_csv(f.parents[0]/summaryFile,sep='\t')
+        print(fName['sample_name'])
+        proteinHits=pd.read_csv(f.parents[0]/proteinGroups,sep='\t')
+        proteinHits.rename({'protein_group_name':'ID'},inplace=True,axis='columns')
+        proteinHits.rename({'protein_group_id':'protein_group_parent_id'},inplace=True,axis='columns')
+        proteinHits=proteinHits.ID.str.split(';', expand=True).set_index(proteinHits.protein_group_parent_id).stack().reset_index(level=0, name='protein_group_name')
+        proteinHits=proteinHits.groupby('protein_group_parent_id').sum()
+        pepHits=pd.read_csv(f,low_memory=False,sep='\t')
+        pepHits=pd.merge(pepHits,proteinHits,on='protein_group_parent_id')
+        pepHits['ID']=pepHits['protein_group_name']+':'+pepHits['stripped_peptide']
+        pepHits = pepHits[['ID', 'x_corr_score']]
+        pepHits=pepHits.groupby('ID').sum()
+        pepHits['Name']=fName['sample_name'][0]
+        df=pd.concat([df,pepHits],sort=False)
 print(df.columns)
 print(df.head())
-#df=df.pivot(index='ID', columns='Name', values='MedianLog2SILAC')
-df=df.pivot_table(index='ID', columns='Name', values='MedianLog2SILAC', aggfunc='median')
-#dfO=df
-#df.to_csv(pathFiles.with_suffix('.combined.txt'),sep="\")#,rownames=FALSE)
-plotcsv=pathFiles/(fileName+".MedianLog2SILAC.histogram.svg")
+df=df.pivot(columns='Name', values='x_corr_score')
+df['x_corr_score']=df.sum(axis=1)
+df=df.sort_values("x_corr_score", ascending=False)  
+plotcsv=pathFiles/("x_corr_score.histogram.svg")
 df.plot(kind='hist',alpha=0.5,bins=100).figure.savefig(plotcsv,dpi=100,bbox_inches = "tight")
 print(df.head())
 print(df.columns)
-df=df.fillna(0)
-df.iloc[:,0:1].plot(kind='hist')
 #writeDPpng=pathFiles/(fileName+"Score.png")
 #df.plot(kind='hist').figure.savefig(writeDPpng.absolute(),dpi=100,bbox_inches = "tight")
 #print("Histogram of Score in",writeDPpng)
-#selecting for phosphorylated-peptides
-#df=df.filter(regex='79', axis="index")
-df['Median']=df.median(axis=1)
-df=df.sort_values('Median',ascending=False)
-writeScores=pathFiles/(fileName+".MedianLog2SILAC.ratio.csv")
+writeScores=pathFiles/("x_corr_score.sum.csv")
 df.to_csv(writeScores)#.with_suffix('.combo.csv'))
-print("MedianLog2SILAC in\n",writeScores,"\n",plotcsv)
+print("x_corr_score in\n",writeScores,"\n",plotcsv)
 #dfID=df.assign(ID=df.ID.str.split(';')).explode('ID')
--- proteinGroupsCombineTTP.py
+++ proteinGroupsCombineTTP.py
@@ -1,10 +1,10 @@
-#python proteinGroupsCombineTTP.py L:\promec\TIMSTOF\LARS\2024\240221_Tom_Kelt\4caaf540a6cc92c6dd28e5f87aa2406f\processing-run
+#python proteinGroupsCombineTTP.py L:\promec\TIMSTOF\LARS\2024\240608_stami_urine\8d9579c57e0ee53e0efd8333b3ecf89e\processing-run
 #!pip3 install pandas matplotlib pathlib --user
 import sys
 from pathlib import Path
 if len(sys.argv)!=2: sys.exit("\n\nREQUIRED: pandas, pathlib, tcl==8.6.9; tested with Python 3.7.9 \n\nUSAGE: python proteinGroupsCombineTTP.py <path to folder containing protein.tsv file(s) like \"L:\promec\Animesh\Samah\mqpar.xml.1623227664.results\" >\n\nExample\n\npython proteinGroupsCombineTTP.py L:\\promec\\TIMSTOF\\LARS\\2024\\240202_beads")
 pathFiles = Path(sys.argv[1])
-#pathFiles=Path("L:/promec/TIMSTOF/LARS/2024/240221_Tom_Kelt/4caaf540a6cc92c6dd28e5f87aa2406f/processing-run")
+#pathFiles=Path("L:/promec/TIMSTOF/LARS/2024/240608_stami_urine/8d9579c57e0ee53e0efd8333b3ecf89e/processing-run")
 fileName='dtaselect.protein.tsv'
 summaryFile='summary-results.results.tsv'
 trainList=list(pathFiles.rglob(fileName))
@@ -43,3 +43,14 @@ writeScores=pathFiles/("PSMs.sum.csv")
 df.to_csv(writeScores)#.with_suffix('.combo.csv'))
 print("PSMs in\n",writeScores,"\n",plotcsv)
 #dfID=df.assign(ID=df.ID.str.split(';')).explode('ID')
+#compare
+pdHits=pd.read_csv("L:\\promec\\HF\\Lars\\2024\\STAMI_urine\\PDv2p5\\ID\\240306_STAMI_urine_11_1_PSMs.txt",low_memory=False,sep='\t')
+print(pdHits.columns)
+pdHits.rename({'Protein Accessions':'ID'},inplace=True,axis='columns')
+pdHits.rename({'Concatenated Rank':'PSMs'},inplace=True,axis='columns')
+pdHitsCR1=pdHits[pdHits['PSMs']==1]
+pdHitsCR1=pdHitsCR1.ID.str.split(';', expand=True).set_index(pdHitsCR1.PSMs).stack().reset_index(level=0, name='ID')
+pdHitsCR1p=pdHitsCR1.pivot(columns='Spectrum File', values='PSMs')
+df['PSMs']=df.sum(axis=1)
+pdHitsCR1=pdHitsCR1.groupby('ID').sum()
+
