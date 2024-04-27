da4f30ee5b8dd358a732dafd9061fffe41f81d0f
ani <sharma.animesh@gmail.com>
2024-04-27 10:12:59 +0000

    read csv test-reports and hist/volcano-plot

--- combineTxtFilesToXlsx.r
+++ combineTxtFilesToXlsx.r
@@ -1,29 +1,34 @@
-#F:\R-4.3.1\bin\Rscript.exe combineTxtFilesToXlsx.r "L:\promec\USERS\Alessandro\230130_Alessandro_35_samples\m16lfqClassic - Copy" proteinGroups.txtLFQ.intensity.18 BeadRemGroups.txttTestBH.csv
-#F:\R-4.3.1\bin\Rscript.exe diffExprTestT.r "L:\promec\USERS\Alessandro\230130_Alessandro_35_samples\m16lfqClassic - Copy\proteinGroups.txt" "L:\promec\USERS\Alessandro\230130_Alessandro_35_samples\m16lfqClassic - Copy\Groups.txt" "Bead" "Rem"
+#Rscript combineTxtFilesToXlsx.r /mnt/promec-ns9036k/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2024/240207_Deo/combined/txt proteinGroups.txtLFQ.intensity.16 0.110.05BioRemGroupsG.txttTestBH.csv
 args = commandArgs(trailingOnly=TRUE)
 print(paste("supplied argument(s):", length(args)))
 print(args)
 inpD <- args[1]
-#inpD<-"L:\\promec\\USERS\\Alessandro\\230130_Alessandro_35_samples\\m16lfqClassic - Copy"
+#inpD<-"/mnt/promec-ns9036k/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2024/240207_Deo/combined/txt/"
 filePrefix <- args[2]
-#filePrefix<-"proteinGroups.txtLFQ.intensity.18"
+#filePrefix<-"proteinGroups.txtLFQ.intensity.16"
 fileSuffix <- args[3]
-#fileSuffix<-"BeadRemGroups.txttTestBH.csv"
+#fileSuffix<-"0.110.05BioRemGroupsG.txttTestBH.csv"
 inpFL<-list.files(pattern=filePrefix,path=inpD,full.names=F,recursive=F)
 inpFL<-inpFL[grepl(fileSuffix,inpFL)]
 print(inpFL)
 dfMZ1<-0
-sheets<-list()
-library(writexl)
+#sheets<-list()
+#library(writexl)
+outPDF<-paste0(inpD,filePrefix,fileSuffix,"combined.pdf")
+outRep<-paste0(inpD,filePrefix,fileSuffix,"combined.xlsx")
+pdf(outPDF)
 for(inpF in inpFL){
     #inpF<-inpFL[1]
+    data<-read.csv(paste(inpD,inpF,sep="/"))
+    inpF<-gsub(fileSuffix,"",inpF)
+    inpF<-gsub(filePrefix,"",inpF)
     print(inpF)
-    data<-read.csv(paste(inpD,inpF,sep="\\"))
-    sheets<-append(sheets,list(data))
+    hist(as.numeric(data[,"Log2MedianChange"]),main=inpF,breaks=100)
+    plot(as.numeric(data[,"Log2MedianChange"]),as.numeric(data[,"PValueMinusLog10"]),main=inpF)
+    #sheets<-append(sheets,list(data))
     MZ1<-data$RowGeneUniProtScorePeps
     dfMZ1<-union(dfMZ1,MZ1)
     colnames(data)<-paste0(colnames(data),inpF)
-    #hist(log2(as.numeric(data[,4])))
     data$RowGeneUniProtScorePeps<-MZ1
     assign(inpF,data)
 }
@@ -35,6 +40,8 @@ inpFL<-gsub(fileSuffix,"",inpFL)
 inpFL<-gsub(filePrefix,"",inpFL)
 print(inpFL)
 #names(sheets)<-inpFL[1]
-names(sheets)<-inpFL
-write_xlsx(sheets, paste(inpD,paste0(filePrefix,fileSuffix,".combined.xlsx"),sep="\\"))
-length(sheets)
+#names(sheets)<-inpFL
+#write_xlsx(sheets, paste(inpD,paste0(filePrefix,fileSuffix,".combined.xlsx"),sep="\\"))
+#length(sheets)
+print(outPDF)
+print(outRep)
