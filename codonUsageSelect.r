49178ab773fccbf631eed2e3d452fb7a7fce8f9a
Animesh Sharma <animeshs@ntnu.no>
2024-05-30 13:10:56 +0200

    Compare intensity-columns of proteinGroups and map the gene-list to codon-usage

--- codonUsageSelect.r
+++ codonUsageSelect.r
@@ -1,4 +1,6 @@
-#Rscript codonUsageSelect.r "L:\promec\TIMSTOF\LARS\2024\240319_Nicola\combined\txt\2024.05.16_analysis nico.xlsx" "Lists codon usage" 2 "L:\promec\TIMSTOF\LARS\2024\240319_Nicola\combined\txt\CCDS.20221027.txt" "L:\promec\TIMSTOF\LARS\2024\240319_Nicola\combined\txt\CCDS.20221027.annot.csv"
+#git checkout ebbdd3bf84fdd40e6abd7c353fc101fadcedc87c diffExprTestT.r
+#Rscript.exe diffExprTestT.r L:\promec\TIMSTOF\LARS\2024\240319_Nicola\combined\txt\proteinGroups.txt L:\promec\TIMSTOF\LARS\2024\240319_Nicola\combined\txt\GroupsRem.txt Bio Rem Intensity. WT 0.1 1 0.05
+#Rscript codonUsageSelect.r "L:\promec\TIMSTOF\LARS\2024\240319_Nicola\combined\txt\proteinGroups.txtIntensity.0.110.05BioRemGroupsRem.txtWTtTestBH.combined.xlsx" "Sheet1" 2 "L:\promec\TIMSTOF\LARS\2024\240319_Nicola\combined\txt\CCDS.20221027.txt" "L:\promec\TIMSTOF\LARS\2024\240319_Nicola\combined\txt\CCDS.20221027.annot.csv"
 #setup####
 #install.packages("ggplot2")
 #install.packages("svglite")
@@ -6,9 +8,9 @@
 args = commandArgs(trailingOnly=TRUE)
 print(args)
 inpF <- args[1]
-#inpF <-"L:/promec/TIMSTOF/LARS/2024/240319_Nicola/combined/txt/2024.05.16_analysis nico.xlsx"
+#inpF <-"L:/promec/TIMSTOF/LARS/2024/240319_Nicola/combined/txt/proteinGroups.txtIntensity.0.110.05BioRemGroupsRem.txtWTtTestBH.combined.xlsx"
 inpS <- args[2]
-#inpS <- "Lists codon usage"
+#inpS <- "Sheet1"
 inpC <- args[3]
 inpC <- as.numeric(inpC)
 #inpC <- 2
@@ -19,10 +21,6 @@ inpCU<-args[5]
 #data####
 data<-readxl::read_xlsx(inpF,sheet = inpS)
 data<-data.frame(data)
-if(inpC>1){
-  colnames(data)<-paste0(colnames(data),data[inpC-1,])
-  data<-data[-(inpC-1),]
-}
 dim(data)
 #annotation####
 #wget https://ftp.ncbi.nlm.nih.gov/pub/CCDS/current_human/CCDS.20221027.txt
@@ -34,22 +32,13 @@ dataAnno<-read.table(inpAnno,sep="\t",header=F)
 dataCU<-read.csv(inpCU,header=T)
 #merge####
 dataCUanno<-merge(dataCU,dataAnno,by.x="ID",by.y="V5",all=T)
-#upInAll<-dataCUanno[data$`FC0.5UP in all` %in% dataCUanno$V3,]
-#dataUpInAll<-merge(data,objData,by="RowGeneUniProtScorePeps",all=T)
-#data<-data[!duplicated(data$RowGeneUniProtScorePeps),]
-#dataS<-data[,grep(selection,colnames(data))]
-#dataS<-data.frame(sapply(dataS,as.numeric))
 #list####
-for(i in colnames(data)){
-  #i<-"FC0.5UP in all"
-  print(i)
-  dataSelCol<-data.frame(ID=data[(data[i]!="0")&!is.na(data[i]),i])
-  print(dim(dataSelCol))
-  dataSelColCUanno<-merge(dataCUanno,dataSelCol,by.x="V3",by.y="ID",all.y=T)
-  dataSelColCUanno<-dataSelColCUanno[!is.na(dataSelColCUanno$StopCodons),]
-  print(dim(dataSelColCUanno))
-  #write####
-  writexl::write_xlsx(dataSelColCUanno,paste0(inpF,".sheet.",inpS,".list.",i,"hdr",inpC,".annotation.",basename(inpAnno),".count.",basename(inpCU),".xlsx"))
-  print(paste("processed sheet",inpS,".list.",i,"hdr",inpC))
-  print(paste0(inpF,".sheet.",inpS,".list.",i,"hdr",inpC,".annotation.",basename(inpAnno),".count.",basename(inpCU),".xlsx"))
-}
+dataSelCol<-data.frame(ID=data[(data[inpC]!="0")&!is.na(data[inpC]),inpC])
+print(dim(dataSelCol))
+dataSelColCUanno<-merge(dataCUanno,dataSelCol,by.x="V3",by.y="ID",all.y=T)
+dataSelColCUanno<-dataSelColCUanno[!is.na(dataSelColCUanno$StopCodons),]
+print(dim(dataSelColCUanno))
+#write####
+writexl::write_xlsx(dataSelColCUanno,paste0(inpF,".sheet.",inpS,".list.","hdr",inpC,".annotation.",basename(inpAnno),".count.",basename(inpCU),".xlsx"))
+print(paste("processed sheet",inpS,".list.","hdr",inpC))
+print(paste0(inpF,".sheet.",inpS,".list.","hdr",inpC,".annotation.",basename(inpAnno),".count.",basename(inpCU),".xlsx"))
