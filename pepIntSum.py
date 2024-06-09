300ab89afcd23d743f8f10fc7fc14d6ce6ad9a6c
Animesh Sharma <animesh@fuzzylife.org>
2024-06-09 20:54:31 +0200

    calculate log2differences and group peptides to protein using median of log2differences, protein-QC

--- pepIntSum.py
+++ pepIntSum.py
@@ -1,88 +1,75 @@
-#python.exe pepIntSum.py  C:\Users\animeshs\OneDrive\Desktop\dynamicrangebenchmark
-#USAGE: python pepIntSum.py <path to folder containing peptides.txt AND proteinGroups.txt files for the experiment>
-#dependencies: pandas and pathlib
-#install: pip install pandas pathlib
-#data: https://ftp.pride.ebi.ac.uk/pride/data/archive/2014/09/PXD000279/dynamicrangebenchmark.zip
-import sys
-from pathlib import Path
-pathFiles = Path(sys.argv[1])
-#pathFiles = Path("L:\\promec\\TIMSTOF\\LARS\\2024\\Kine_Samset_Hoem\\DDA\\240103_KineSamsetHoem\\txt243lin\\txt\\")
-import pandas as pd
-df=pd.read_csv(pathFiles/"peptides.txt",low_memory=False,sep='\t')
-print(df.columns)
-print(df.head())
-#import matplotlib.pyplot as plt
+#python pepIntSum.py  dynamicrangebenchmark/peptides.txt
+# %% setup
+#install dependencies: pip install matplotlib pandas 
+import sys,matplotlib.pyplot as plt,numpy as np,pandas as pd
+# %% data
+pathFiles = sys.argv[1]
+#eg. data: https://ftp.pride.ebi.ac.uk/pride/data/archive/2014/09/PXD000279/dynamicrangebenchmark.zip
+#pathFiles = "dynamicrangebenchmark/peptides.txt"
+dfS=pd.read_csv(pathFiles,low_memory=False,sep='\t')
+print(dfS.columns)
+print(dfS.head())
+#plt.close('all')
+#import matplotlib
+#matplotlib.use('Agg')
 #plt.plot(df['Intensity'])
-dfS=df.copy()
+#plt.show()
 #dfS=df[df['PEP']<0.01]
 #print(dfS['Mod. peptide IDs'].value_counts())
 #plt.plot(dfS['Intensity'])
 #dfS.rename({'Leading razor protein':'ID'},inplace=True,axis='columns')
-dfS.rename({'Proteins':'ID'},inplace=True,axis='columns')
+# %% separate IDs by semicolon
+dfS.rename({'Leading razor protein':'ID'},inplace=True,axis='columns')
 print(dfS[dfS==0].count())
-import numpy as np
 #dfS.replace(0, np.nan, inplace=True)
 #print(dfS[dfS==0].count())
 dfS['IDs']=dfS.ID.str.split(';')
 dfSE=dfS.explode('IDs')
+dfSE.index=dfSE["Sequence"]
+# %% groupy IDs using sum of intensities
 dfSEG=dfSE.groupby(dfSE['IDs']).aggregate('sum')
-dfSEG.to_csv(pathFiles/'peptides.combinedIntensity.csv')#,sep="\")#,rownames=FALSE)
-dfSelProt3=dfSEG[dfSEG.index.isin(['B5X2G1','A0A1S3R3M8','A1ED59'])]
-dfSelProt3.to_csv(pathFiles/'peptidesSel3.sumIntensity.csv')#,sep="\")#,rownames=FALSE)
-dfSelProt3Int=dfSelProt3.filter(regex='Intensity ',axis=1)
-dfSelProt3IntLog2=np.log2(dfSelProt3Int+1)
-dfSelProt3IntLog2.columns=dfSelProt3IntLog2.columns.str.removeprefix('Intensity ')
-dfSelProt3IntLog2.columns=dfSelProt3IntLog2.columns.str.split('_').str[0]
-dfSelProt3IntLog2.T.plot.line().figure.savefig(pathFiles/'peptidesSel3.sumIntensityLog2.png',dpi=100,bbox_inches = "tight")
-import matplotlib.pyplot as plt
-plt.show()
-dfSelProt3IntLog2.to_csv(pathFiles/'peptidesSel3.sumIntensityLog2.csv')#,sep="\")#,rownames=FALSE)
-#dfP=dfS.pivot_table(index='ID', columns='Sequence', values='Intensity', aggfunc='sum')
-#dfP.to_csv(pathFiles.with_suffix('.combinedIntensity.csv'))#,sep="\")#,rownames=FALSE)
-dfProtG=pd.read_csv(pathFiles/"proteinGroups.txt",low_memory=False,sep='\t')
-dfProtG.rename({'Majority protein IDs':'ID'},inplace=True,axis='columns')
-#dfProtG.rename({'Protein IDs':'ID'},inplace=True,axis='columns')
-dfProtG['IDs']=dfProtG.ID.str.split(';')
-dfProtGE=dfProtG.explode('IDs')
-dfM=dfProtGE.merge(dfSEG,left_on='IDs', right_on='IDs',how='outer',indicator=True)
-#dfM=dfProtG.merge(dfSEG,left_on='ID', right_on='IDs',how='outer',indicator=True)
-dfM.to_csv(pathFiles/'proteins.mergedIntensity.csv')#,sep="\")#,rownames=FALSprint(dfM.columns)
-print(dfM[dfM['_merge']=="right_only"])
-print(dfM[dfM['_merge']=="left_only"])
-print(dfM[dfM['IDs'].str.contains('REV__')])
-#dfC=dfM[dfM['_merge']=="left_only"]
-dfC=dfM[dfM['_merge']=="both"].filter(regex='Intensity',axis=1)
-dfC["ID"]=dfM['IDs']
-dfC["IDs"]=dfM['ID']
-dfC["diffInt"]=(dfC['Intensity_y']-dfC['Intensity_x'])/dfC['Intensity_x']
-print(dfC.columns)
-print("\nDff Intensity Summary\n",dfC["diffInt"].describe())
-#dfC["diffInt"].hist()
-dfC.to_csv(pathFiles/'proteins.merged.selIntensity.csv')#,sep="\")#,rownames=FALSprint(dfM.column#plt.plot(diffInt,"o")
-#plt.savefig(pathFiles.with_suffix('.combinedIntensity.png'),dpi=100,bbox_inches = "tight")
-#plt.show()
-dfSEint=dfSE.copy()
-dfSEint.replace(np.nan,1,inplace=True)
-dfSEint=dfSEint.filter(regex='Intensity ',axis=1)
+dfSEG.to_csv(pathFiles+'.combinedIntensity.csv')#,sep="\")#,rownames=FALSE)
+# %% check for a protein
+dfA5A614=dfSE[dfSE['IDs'].isin(["A5A614"])]
+dfA5A614.index=dfA5A614.Sequence
+dfA5A614=dfA5A614.filter(regex='Intensity ',axis=1)
+dfA5A614.columns=dfA5A614.columns.str.removeprefix('Intensity ')
+#dfA5A614.columns=dfA5A614.columns.str.split('_').str[0]
+#dfA5PFJ8=dfSE[dfSE['IDs'].isin(["A5PFJ8"])]
+#dfSE[dfSE['IDs'].isin(["A5PFJ8"])]
+dfA5A614.T.plot.line().figure.savefig(pathFiles+'peptidesSel.sumIntensity.png',dpi=100,bbox_inches = "tight")
+dfA5A614.to_csv(pathFiles+'peptidesSel.sumIntensity.csv')#,sep="\")#,rownames=FALSE)
+# %% extract intensity
+dfSE.replace(0,np.nan,inplace=True)
+dfSEint=dfSE.filter(regex='Intensity ',axis=1)
+dfSEint.columns=dfSEint.columns.str.removeprefix('Intensity ')
 print(dfSEint.describe())
+# %% tranform with log2
 dfSEintLog2=np.log2(dfSEint)
 print(dfSEintLog2.describe())
+# %% pairwise substract log2 sample values per peptide
 dfSEintRepS=dfSEintLog2[np.repeat(dfSEintLog2.columns.values,dfSEintLog2.shape[1])]
+#dfSEintRepS.reset_index(inplace=True)
+dfSEintRepS.columns
 dfSEintRepB=pd.concat([dfSEintLog2]*(dfSEintLog2.shape[1]),axis=1)
-dfSEintRepSB=np.subtract(dfSEintRepS,dfSEintRepB)
-dfSEintRepSB.columns=dfSEintRepS.columns+';'+dfSEintRepB.columns
+#dfSEintRepB.reset_index(inplace=True)
+dfSEintRepB.columns
+dfSEintRepS.columns=dfSEintRepS.columns+';'+dfSEintRepB.columns
+dfSEintRepB.columns=dfSEintRepS.columns
+dfSEintRepSB=dfSEintRepS-dfSEintRepB
 dfSEintRepSB.replace(0,np.nan,inplace=True)
 dfSEintRepSB.dropna(axis = 1, how = 'all',inplace=True)
 print(dfSEintRepSB.describe())
-dfSEintRepSB["peptide"]=dfSE["Sequence"]
-dfSEintRepSB["ID"]=dfSE["IDs"]
-dfSEintRepSB.to_csv(pathFiles/'peptides.sampleBySampleIntensityMedian.csv')
+dfSEintRepSB["Protein"]=dfSE["IDs"]
+dfSEintRepSB=dfSEintRepSB.sort_values('Protein')
+dfSEintRepSB.to_csv(pathFiles+'sampleBySampleLog2IntensityDiff.csv')
 print(dfSEintRepSB.columns)
-print("\Intensity Difference Summary\n",dfSEintRepSB.describe())
-dfSEintRepSBPG=dfSEintRepSB.groupby(dfSEintRepSB['ID']).aggregate('median')
+print("Intensity Difference Summary\n",dfSEintRepSB.describe())
+# %% group peptides to protein using median of log2differences 
+dfSEintRepSBPG=dfSEintRepSB.groupby(dfSEintRepSB['Protein']).aggregate('median')
 #print(dfSEintRepSBPG.describe())
 #dfSEintRepSBPG.hist()
 #dfSEintRepSBPG.replace(np.nan,0,inplace=True)
-dfSEintRepSBPG.to_csv(pathFiles/'proteins.merged.sampleBySampleIntensityMedian.csv')
+dfSEintRepSBPG.to_csv(pathFiles+'proteins.merged.sampleBySampleIntensityMedian.csv')
 #print(dfSEintRepSBPG.columns)
-print("\Median Intensity Difference Summary\n",dfSEintRepSBPG.describe())
+print("Median Intensity Difference Summary\n",dfSEintRepSBPG.describe())
--- proteinGroupsQC.r
+++ proteinGroupsQC.r
@@ -1,5 +1,7 @@
-#Rscript proteinGroupsQC.r "L:/promec/TIMSTOF/LARS/2023/230310 ChunMei/combined/txt/proteinGroups.txt"
+#Rscript proteinGroupsQC.r "L:/promec/TIMSTOF/LARS/2023/230310 ChunMei/combined/txt/proteinGroups.txt" "L:/promec/Animesh/Mathilde/Groups.xlsx"
 #setup####
+#remotes::install_github('wolski/prolfquapp', dependencies = TRUE, force = FALSE)
+#https://github.com/fgcz/prolfqua/issues/77
 args = commandArgs(trailingOnly=TRUE)
 print(paste("supplied argument(s):", length(args)))
 print(args)
@@ -19,3 +21,32 @@ levels(dataCovClip50Bin6)<-paste(c("0-10","10-20","20-30","30-40","40-50",">50")
 pie(table(dataCovClip50Bin6),main="Sequence coverage")
 dev.off()
 #hist(dataCovClip50,xlim=c(0,50),main="Sequence coverage",xlab="Sequence coverage",ylab="Frequency")
+#label####
+inputAnnotation <-"L:/promec/Animesh/Mathilde/Groups.xlsx"
+labelD<-basename(inputAnnotation)
+annotation <- readxl::read_xlsx(inputAnnotation)
+head(annotation$raw.file)
+startdata <- prolfqua::tidyMQ_ProteinGroups(inputMQfile)
+head(startdata$raw.file)
+outP=paste(inputMQfile,labelD,"prolfqua","pdf",sep = ".")
+#pdf(outP)
+startdata <- dplyr::inner_join(annotation, startdata, by = "raw.file")
+#data[grep("A0A023T778",data$Protein.IDs),grep("Intensity",colnames(data))]
+#A0A023T778<-data[grep("A0A023T778",data$Protein.IDs),]#76_slot2.24_1_3925 intensity 49608 LFQ 57589
+#startdata <- dplyr::filter(startdata, nr.peptides > 1)
+atable <- prolfqua::AnalysisTableAnnotation$new()
+atable$fileName = "raw.file"
+head(startdata$proteinID)
+atable$hierarchy[["protein_Id"]] <- c("proteinID")
+atable$factors[["mouse."]] = "sample"
+atable$set_response("mq.protein.intensity")
+config <- prolfqua::AnalysisConfiguration$new(atable)
+adata <- prolfqua::setup_analysis(startdata, config)
+lfqdata <- prolfqua::LFQData$new(adata, config)
+lfqdata$remove_small_intensities()
+dataLFQ<-lfqdata[["to_wide"]]()$data
+head(dataLFQ)
+dataLFQd<-data.frame(dataLFQ[,3:61])
+dataLFQd<-log2(dataLFQd)
+dataLFQd<-sapply(dataLFQd,as.numeric)
+hist(dataLFQd)
