77a35f8e4f5f92d3b96770056b7cbf58d6a2d3e9
Animesh Sharma <animeshs@ntnu.no>
2024-07-05 15:57:22 +0200

    comparing offline-online DDA MBR-LFQ PASER results

--- proteinGroupsCompareTTP.py
+++ proteinGroupsCompareTTP.py
@@ -1,39 +1,39 @@
 #!pip3 install pandas matplotlib pathlib --user
-#python proteinGroupsCompareTTP.py ID  L:\promec\TIMSTOF\LARS\2024\240626_Mira\2de755bb378949183b1b6b89a359e507\processing-run\PSMs.sum.csv L:\promec\TIMSTOF\LARS\2024\240626_Mira\b3093c79d999b9393e121db1af7c0438\processing-run\PSMs.sum.csv 
+#python proteinGroupsCompareTTP.py "Protein accession"  L:\promec\TIMSTOF\LARS\2024\240626_Mira\Mira.tsv L:\promec\TIMSTOF\LARS\2024\240626_Mira\Mira2.tsv
 # %% setup
 import sys
 from pathlib import Path
 import pandas as pd
 # %% read
-#pathFiles=Path("L:/promec/TIMSTOF/LARS/2024/240626_Mira/2de755bb378949183b1b6b89a359e507/processing-run/PSMs.sum.csv","L:/promec/TIMSTOF/LARS/2024/240626_Mira/b3093c79d999b9393e121db1af7c0438/processing-run/PSMs.sum.csv")
-#pathFiles=Path()
-cID=sys.argv[1]
-#cID="ID"
-pathFile1 = Path(sys.argv[2])  
-#pathFile1 = Path("L:/promec/TIMSTOF/LARS/2024/240626_Mira/2de755bb378949183b1b6b89a359e507/processing-run/PSMs.sum.csv")  
-pathFile2 = Path(sys.argv[3])  
-#pathFile2 = Path("L:/promec/TIMSTOF/LARS/2024/240626_Mira/b3093c79d999b9393e121db1af7c0438/processing-run/PSMs.sum.csv")  
+#cID=sys.argv[1]
+cID="Protein accession"
+#pathFile1 = Path(sys.argv[2])  
+pathFile1 = Path("L:/promec/TIMSTOF/LARS/2024/240626_Mira/Mira.tsv")  
+#pathFile2 = Path(sys.argv[3])  
+pathFile2 = Path("L:/promec/TIMSTOF/LARS/2024/240626_Mira/Mira2.tsv")  
 #compare
-pdHits1=pd.read_csv(pathFile1,low_memory=False)
+pdHits1=pd.read_csv(pathFile1,low_memory=False,sep='\t')
 print(pdHits1.columns)
-pdHits2=pd.read_csv(pathFile2,low_memory=False)
+pdHits2=pd.read_csv(pathFile2,low_memory=False,sep='\t')
 print(pdHits2.columns)
-# %% compare
+# %% combine
 pdHits1.rename({cID:'ID'},inplace=True,axis='columns')
 pdHits2.rename({cID:'ID'},inplace=True,axis='columns')
 pdHits=pd.merge(pdHits1,pdHits2,on='ID',how='outer',suffixes=('_pathFile1', '_pathFile2'))
 pdHits=pdHits.fillna(0)
 print(pdHits.describe())
 print(pdHits.columns)
+# %% compare
 pdHitsDiff=pd.DataFrame()
 for c in pdHits.columns:
     if c.endswith('_pathFile1'):
         c2=c.replace('_pathFile1','_pathFile2')
         if c2 in pdHits.columns:
-            print(c,c2)
             c0=c.replace('_pathFile1','')
-            pdHitsDiff[c0]=pdHits[c]-pdHits[c2]
-            print(pdHitsDiff[c0].describe())
+            print(c0,c,c2)
+            if pdHits[c].dtype == 'float64':
+                pdHitsDiff[c0]=pdHits[c]-pdHits[c2]
+                print(pdHitsDiff[c0].describe())
 print(pdHitsDiff.columns)
 print(pdHitsDiff.shape)
 pdHitsDiff.index=pdHits.ID
@@ -45,11 +45,11 @@ pdHitsDiff.dropna(axis=1, how='all', inplace=True)
 pdHitsDiff.dropna(axis=0, how='all', inplace=True)
 print(pdHitsDiff.describe())
 print(pdHitsDiff.shape)
+print(pdHitsDiff.columns)
 pdHitsDiff.to_csv(pathFile1.with_name(pathFile1.stem+".diff."+pathFile2.stem+".csv"))
 # %% plot
-print(pdHitsDiff.columns)
-pdHitsDiff.drop("PSMs",inplace=True,axis=1)
+pdHitsDiff=pdHitsDiff[pdHitsDiff.columns[~pdHitsDiff.columns.str.endswith(' avg')]]
 plotcsv=pathFile1.with_name(pathFile1.stem+".diff."+pathFile2.stem+".histogram.svg")
-pdHitsDiff.plot(kind='hist',alpha=0.5,bins=100).figure.savefig(plotcsv,dpi=100,bbox_inches = "tight")
+pdHitsDiff.plot(kind='hist',alpha=0.5,bins=100,legend=False).figure.savefig(plotcsv,dpi=320,bbox_inches = "tight")
 print(plotcsv)
 # %%
