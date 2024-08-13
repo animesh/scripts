e3e91f773bdcd23d761b0efe1562662b6550b4ce
Animesh Sharma <ash022@login-1.saga>
2024-08-13 13:53:48 +0200

    batch DIA timsTOF-pro data over low-mem saga nodes

--- slurmMQrunTTP.sh
+++ slurmMQrunTTP.sh
@@ -1,8 +1,8 @@
 #git checkout 92ca83be3bc60f118b7b5ab7072aa5dd542fc11c mqparTTPdda.xml scratch.slurm slurmMQrunTTP.sh
+#dos2unix mqparTTPdda.xml scratch.slurm slurmMQrunTTP.sh
+#bash slurmMQrunTTP.sh /cluster/projects/nn9036k/MaxQuant_v2.6.3.0/bin/MaxQuantCmd.dll /cluster/projects/nn9036k/Brent /cluster/projects/nn9036k/FastaDB/uniprotkb_proteome_UP000005640_2024_07_22.fasta mqparTTPdia.xml scratch.slurm
 #grep "multiplicity" *.xml
 #grep "Atg10;Lys8" mqparTTPdia.xml
-#dos2unix mqparTTPdda.xml scratch.slurm slurmMQrunTTP.sh
-#bash slurmMQrunTTP.sh /cluster/projects/nn9036k/MaxQuant_v_2.4.10.0/bin/MaxQuantCmd.exe /cluster/projects/nn9036k/scripts/231123_dia_dda/DDA /cluster/projects/nn9036k/FastaDB mqparTTPdda.xml scratch.slurm
 #wget "https://maxquant.org/p/maxquant/MaxQuant_v_2.4.10.0.zip?md5=bwPYOvsI5oXolBP_wXUyzg&expires=1700127616" -O MQv24100.zip
 #unzip MQv24100.zip
 #wget "https://datashare.biochem.mpg.de/s/qe1IqcKbz2j2Ruf/download?path=%2FDiscoveryLibraries&files=homo_sapiens.zip" -O HS.DIA.zip
