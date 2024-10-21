64fa02b67f07edfdb8fab43bcf933cc4dcba76ee
Animesh Sharma <animeshs@ntnu.no>
2024-10-21 15:41:08 +0200

    count phospho STY peptides and overall-ration

--- msmsCount.py
+++ msmsCount.py
@@ -1,22 +1,27 @@
+#python msmsCount.py L:\promec\TIMSTOF\LARS\2024\241002_zrimac
 import sys
 from pathlib import Path
 fileName='msms.txt'
 colName="Sequence"
 if len(sys.argv)!=2:    sys.exit("USAGE: python dePep.py <path to folder containing \""+fileName+"\" file(s) like \"L:/combined/txt\" with column \""+colName+"\">")
 pathFiles = Path(sys.argv[1])
-#pathFiles = Path('L:/promec/TIMSTOF/LARS/2022/februar/AndersS/combined/txt/')
+#pathFiles = Path('L:/promec/TIMSTOF/LARS/2024/241002_zrimac/')
 trainList=list(pathFiles.rglob(fileName))
-
 import pandas as pd
 df=pd.DataFrame()
-f=trainList[0]
+#f=trainList[0]
 for f in trainList:
-    peptideHits=pd.read_csv(f,low_memory=False,sep='\t')
     print(f)
-    peptideHits['Name']=f
+    peptideHits=pd.read_csv(f,low_memory=False,sep='\t')
+    peptideHitsPhoSTY=peptideHits.pivot_table(index='Sequence', columns='Raw file',values='Phospho (STY)',aggfunc='sum')
+    peptideHitsScans=peptideHits.pivot_table(index='Sequence', columns='Raw file',values='Scan event number',aggfunc='sum')
+    peptideHitsPhoSTYratio=(-1)*peptideHitsPhoSTY/peptideHitsScans
+    peptideHitsPhoSTYratioHist = peptideHitsPhoSTYratio.plot.hist()
+    peptideHitsPhoSTYratioHist.figure.savefig(f.with_suffix(".phoSTYratio.png"))
+    peptideHitsPhoSTYratio.to_csv(f.with_suffix(".phoSTYratio.csv"))
+    peptideHits['Name']=peptideHits['Raw file']+str(f)
     df=pd.concat([df,peptideHits],sort=False)
 print(df.head())
-
 print(colName,"found in column",df.columns.get_loc(colName))
 dfMSMS=df.loc[:, df.columns.str.startswith(colName)]
 dfMSMS=dfMSMS[dfMSMS[colName].notnull()]
