878c2f269da5d5897184cb8ac012ccd4e1b8ce81
Animesh Sharma <ash022@login-1.saga>
2024-10-27 08:45:35 +0100

    check phospho-peptides ratio in DIA-NN results

--- msmsCount.py
+++ msmsCount.py
@@ -1,37 +1,25 @@
-#python msmsCount.py L:\promec\TIMSTOF\LARS\2024\241002_zrimac
+#python msmsCount.py /cluster/projects/nn9036k/FastaDB/phoslibMC1V3mz100to1700c2to3human.predicted.speclibreport.parquet
 import sys
 from pathlib import Path
-fileName='msms.txt'
-colName="Sequence"
-if len(sys.argv)!=2:    sys.exit("USAGE: python dePep.py <path to folder containing \""+fileName+"\" file(s) like \"L:/combined/txt\" with column \""+colName+"\">")
-pathFiles = Path(sys.argv[1])
-#pathFiles = Path('L:/promec/TIMSTOF/LARS/2024/241002_zrimac/')
-trainList=list(pathFiles.rglob(fileName))
+if len(sys.argv)!=2:    sys.exit("USAGE: python dePep.py <path to folder containing parquet fileName like \"/cluster/projects/nn9036k/FastaDB/phoslibMC1V3mz100to1700c2to3human.predicted.speclibreport.parquet\" >")
+f = Path(sys.argv[1])
+#f = Path('/cluster/projects/nn9036k/FastaDB/phoslibMC1V3mz100to1700c2to3human.predicted.speclibreport.parquet')
 import pandas as pd
 df=pd.DataFrame()
-#f=trainList[0]
-for f in trainList:
-    print(f)
-    peptideHits=pd.read_csv(f,low_memory=False,sep='\t')
-    peptideHitsPhoSTY=peptideHits.pivot_table(index='Sequence', columns='Raw file',values='Phospho (STY)',aggfunc='sum')
-    peptideHitsScans=peptideHits.pivot_table(index='Sequence', columns='Raw file',values='Scan event number',aggfunc='sum')
-    peptideHitsPhoSTYratio=(-1)*peptideHitsPhoSTY/peptideHitsScans
-    peptideHitsPhoSTYratioHist = peptideHitsPhoSTYratio.plot.hist()
-    peptideHitsPhoSTYratioHist.figure.savefig(f.with_suffix(".phoSTYratio.png"))
-    peptideHitsPhoSTYratio.to_csv(f.with_suffix(".phoSTYratio.csv"))
-    peptideHits['Name']=peptideHits['Raw file']+str(f)
-    df=pd.concat([df,peptideHits],sort=False)
-print(df.head())
-print(colName,"found in column",df.columns.get_loc(colName))
-dfMSMS=df.loc[:, df.columns.str.startswith(colName)]
-dfMSMS=dfMSMS[dfMSMS[colName].notnull()]
-dfMSMScnt=dfMSMS[colName].value_counts()
-import numpy as np
-np.log2(dfMSMScnt).hist()
-print("writing output to ... ")
-writeMSMSpng=pathFiles/(fileName+".count.png")
-dfMSMScnt[dfMSMScnt>10].plot(kind='pie').figure.savefig(writeMSMSpng,dpi=100,bbox_inches = "tight")
-print(writeMSMSpng)
-writeMSMScsv=pathFiles/(fileName+".count.csv")
-dfMSMScnt.to_csv(writeMSMScsv)
-print(writeMSMScsv)
+print(f)
+import pyarrow.parquet as pq
+peptideHits=pq.read_table(f).to_pandas()
+print(peptideHits.columns)
+peptideHits.to_csv(f.with_suffix(".parse.csv"))
+#peptideHits=peptideHits[~peptideHits['Protein.Ids'].str.contains('cRAP', case=False, na=False)]
+peptideHits['Phospho (STY)'] = peptideHits['Precursor.Id'].str.contains('UniMod:21', case=False, na=False)
+peptideHits['Phospho (STY)'].describe()
+peptideHits['Scan event number'] = True
+peptideHitsPhoSTY=peptideHits.pivot_table(index='Stripped.Sequence', columns='Run',values='Phospho (STY)',aggfunc='sum')
+peptideHitsScans=peptideHits.pivot_table(index='Stripped.Sequence', columns='Run',values='Scan event number',aggfunc='sum')
+peptideHitsPhoSTYratio=peptideHitsPhoSTY/peptideHitsScans
+peptideHitsPhoSTYratioHist = peptideHitsPhoSTYratio.plot.hist()
+peptideHitsPhoSTYratio.to_csv(f.with_suffix(".phoSTYratio.csv"))
+print(f.with_suffix(".phoSTYratio.csv"))
+peptideHitsPhoSTYratioHist.figure.savefig(f.with_suffix(".phoSTYratio.png"))
+print(f.with_suffix(".phoSTYratio.png"))
