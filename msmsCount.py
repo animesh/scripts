1b32cc4eb866988a7e0f6208c3b57ef9ad919da7
Animesh Sharma <animeshs@ntnu.no>
2024-10-28 10:09:59 +0100

    results for the NEW ones at L:\promec\TIMSTOF\LARS\2024\241002_zrimac\phoSTYnew\dia where ratio looks like using latest DIA-NN

--- msmsCount.py
+++ msmsCount.py
@@ -1,9 +1,10 @@
-#python msmsCount.py /cluster/projects/nn9036k/FastaDB/phoslibMC1V3mz100to1700c2to3human.predicted.speclibreport.parquet
+#python msmsCount.py L:\promec\TIMSTOF\LARS\2024\241002_zrimac\phoSTYnew\dia\phoslibMC1V3mz100to1700c2to3human.predicted.speclibreportNY.parquet
+##rsync -Pirm --include='*.parquet' --include='*/' --exclude='*' ash022@login.saga.sigma2.no:scripts/ /mnt/l/promec/TIMSTOF/LARS/2024/241002_zrimac/ 
 import sys
 from pathlib import Path
 if len(sys.argv)!=2:    sys.exit("USAGE: python dePep.py <path to folder containing parquet fileName like \"/cluster/projects/nn9036k/FastaDB/phoslibMC1V3mz100to1700c2to3human.predicted.speclibreport.parquet\" >")
 f = Path(sys.argv[1])
-#f = Path('/cluster/projects/nn9036k/FastaDB/phoslibMC1V3mz100to1700c2to3human.predicted.speclibreport.parquet')
+#f = Path('L:/promec/TIMSTOF/LARS/2024/241002_zrimac/phoSTYnew/dia/phoslibMC1V3mz100to1700c2to3human.predicted.speclibreportNY.parquet')
 import pandas as pd
 df=pd.DataFrame()
 print(f)
@@ -18,7 +19,7 @@ peptideHits['Scan event number'] = True
 peptideHitsPhoSTY=peptideHits.pivot_table(index='Stripped.Sequence', columns='Run',values='Phospho (STY)',aggfunc='sum')
 peptideHitsScans=peptideHits.pivot_table(index='Stripped.Sequence', columns='Run',values='Scan event number',aggfunc='sum')
 peptideHitsPhoSTYratio=peptideHitsPhoSTY/peptideHitsScans
-peptideHitsPhoSTYratioHist = peptideHitsPhoSTYratio.plot.hist()
+peptideHitsPhoSTYratioHist = peptideHitsPhoSTYratio.plot.hist(bins=10, alpha=0.3)
 peptideHitsPhoSTYratio.to_csv(f.with_suffix(".phoSTYratio.csv"))
 print(f.with_suffix(".phoSTYratio.csv"))
 peptideHitsPhoSTYratioHist.figure.savefig(f.with_suffix(".phoSTYratio.png"))
