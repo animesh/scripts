e0689837a470168aac3bdd75018a10012fa8aefe
Animesh Sharma <animeshs@ntnu.no>
2024-11-14 17:07:10 +0100

    generate QC plot for whole Human PDAC organoids

--- evidenceQC.r
+++ evidenceQC.r
@@ -1,31 +1,27 @@
-#Rscript evidenceQC.r "L:/promec/TIMSTOF/LARS/2023/230414 mathilde/evidence.txt" "230414_Mathilde_3_Slot2-48_1_4330"
+#Rscript evidenceQC.r "L:/promec/TIMSTOF/LARS/2024/240404_Mathilde/combined/txt/evidence.txt"
 #setup####
 args = commandArgs(trailingOnly=TRUE)
 print(paste("supplied argument(s):", length(args)))
 print(args)
 inpF <- args[1]
-#inpF<-"L:/promec/TIMSTOF/LARS/2023/230414 mathilde/evidence.txt"
-selection <- args[2]
-#selection<-"230414_Mathilde_3_Slot2-48_1_4330"
-#data####
+#inpF<-"L:/promec/TIMSTOF/LARS/2024/240404_Mathilde/combined/txt/evidence.txt"
 data<-read.table(inpF,header = T,sep = "\t",quote = "")
-data<-data[(data$Raw.file  %in% strsplit(selection," ")[[1]]),]
-jpeg(paste0(inpF,selection,"Uncalibrated...Calibrated.m.z..ppm.jpg"))
+jpeg(paste0(inpF,"Uncalibrated...Calibrated.m.z..ppm.jpg"))
 hist(data$Uncalibrated...Calibrated.m.z..ppm.,breaks=10,density = 200,main = "Uncalibrated...Calibrated.m.z..ppm.",xlab = "PPM",col = "skyblue",)
 #hist(data$Mass.error..ppm.)
 dev.off()
-jpeg(paste0(inpF,selection,"Uncalibrated...Calibrated.m.z..ppm.density.jpg"))
-d<-density(data$Uncalibrated...Calibrated.m.z..Da.,na.rm = T)
+jpeg(paste0(inpF,"Uncalibrated...Calibrated.m.z..ppm.density.jpg"))
+d<-density(data$Uncalibrated...Calibrated.m.z..Da.)
 plot(d,main = "Uncalibrated...Calibrated.m.z..Da.",xlab = "Dalton")
 polygon(d, col="skyblue", border="black")
 #hist(data$Mass.error..Da.)
 dev.off()
 dataSel<-data#[1:50000,]
-jpeg(paste0(inpF,selection,"Charge.jpg"))
+jpeg(paste0(inpF,"Charge.jpg"))
 #hist(dataSel$Charge)
 plot(dataSel$Length,dataSel$m.z,pch=19,col=factor(dataSel$Charge),main="Length vs m.z",xlab="Length",ylab="m.z")
 legend("bottomright",legend=levels(factor(dataSel$Charge)),col=1:4,pch=19,title="Charge")
 dev.off()
-jpeg(paste0(inpF,selection,"Missed.cleavages.jpg"))
+jpeg(paste0(inpF,"Missed.cleavages.jpg"))
 pie(table(dataSel$Missed.cleavages),main="Missed cleavages")
 dev.off()
