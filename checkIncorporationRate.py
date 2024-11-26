e762b78773b308a712c240f0edc096919a5d70c6
Animesh Sharma <animeshs@ntnu.no>
2024-11-26 07:01:27 +0100

    check IR

--- checkIncorporationRate.py
+++ checkIncorporationRate.py
@@ -1,10 +1,10 @@
-#python checkIncorporationRate.py L:\promec\TIMSTOF\LARS\2024\241116_SILAC\combined\txt peptides.txt
+#python checkIncorporationRate.py
 print("peptide.txt output file to calculate the incorporation rate. Distinguish between lysine- and argininecontaining peptides. For each of these subsets determine the incorporation rate as 1–1/average ratio, using the non-normalized ratios. The density plot of the density distribution should be narrow, and for both lysine and arginine it should be above 0.95, Reference: Use of stable isotope labeling by amino acids in cell culture as a spike-in standard in quantitative proteomics https://www.nature.com/articles/nprot.2010.192")
 import sys
 from pathlib import Path
 
 if len(sys.argv) != 3:
-    dirName = Path("L:/promec/TIMSTOF/LARS/2024/241116_SILAC/combined/txt/")
+    dirName = Path("L:/promec/HF/LARS/2024/241121_silaCTEST/combined/txt/")
     #fileName='proteinGroups.txt'
     fileName='peptides.txt'
     print("\n\nUSAGE: python checkIncorporationRate.py <path to folder containing",fileName,"file(s)>\n\nUsing DEFAULT directory\"", dirName, "\"looking for\"", fileName, "\"file(s)\n\n")
@@ -16,7 +16,7 @@ else:
 trainList=list(dirName.rglob(fileName))
 print("Using file(s)\n",trainList)
 
-columnName='^(Ratio H/L) [0-9](.*)'#'*([0-9])$'
+columnName='^(Ratio H/L) [AIJRU](.*)'#'*([0-9])$'
 columnNameH='^(Intensity H)(.*)|(.*)(Light)$'#'*([0-9])$'
 columnNameL='^(Intensity L)(.*)|(.*)(Heavy)$'#'*([0-9])$'
 #f=trainList[0]
