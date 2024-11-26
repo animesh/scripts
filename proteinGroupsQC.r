6422947cbc4759f52ba821812203edd687941e85
Animesh Sharma <animeshs@ntnu.no>
2024-11-26 15:40:35 +0100

    CV histogram from

--- proteinGroupsQC.r
+++ proteinGroupsQC.r
@@ -1,18 +1,16 @@
-#Rscript proteinGroupsQC.r "L:/promec/TIMSTOF/LARS/2023/230414 mathilde/proteinGroups.txt" "230414_Mathilde_1_Slot2-46_1_4326 230414_Mathilde_2_Slot2-47_1_4328"
+#Rscript proteinGroupsQC.r "L:/promec/TIMSTOF/LARS/2022/APRIL/Mathilde/proteinGroups.txt" "Intensity."
 #setup####
 args = commandArgs(trailingOnly=TRUE)
 print(paste("supplied argument(s):", length(args)))
 inpF <- args[1]
-#inpF<-"L:/promec/TIMSTOF/LARS/2023/230414 mathilde/proteinGroups.txt"
+#inpF<-"L:/promec/TIMSTOF/LARS/2022/APRIL/Mathilde/proteinGroups.txt"
 selection <- args[2]
-#selection<-"230414_Mathilde_1_Slot2-46_1_4326 230414_Mathilde_2_Slot2-47_1_4328"
+#selection<-"Intensity."
 print(args)
 #data####
 data<-read.table(inpF,header = T,sep = "\t",quote = "")
-dataCovClip50<-data[,match(gsub("-",".",gsub("230414_Mathilde_","",paste0("Sequence.coverage.",strsplit(selection," ")[[1]],"...."))),colnames(data))]
-data$Sequence.coverage....<-apply(dataCovClip50,1,function(x) max(x,na.rm=T))
 #https://www.nature.com/articles/s41597-024-03355-4#Sec8
-jpeg(paste0(inpF,selection,"Sequence.coverage.jpg"))
+jpeg(paste0(inpF,"Sequence.coverage.jpg"))
 range(data$Sequence.coverage....)
 #hist(data$Sequence.coverage....)
 dataCovClip50<-scales::squish(data$Sequence.coverage....,c(0,50))
@@ -24,11 +22,11 @@ levels(dataCovClip50Bin6)<-paste(c("0-10","10-20","20-30","30-40","40-50",">50")
 pie(table(dataCovClip50Bin6),main="Sequence coverage")
 dev.off()
 #intensity####
-intdata<-data[,match(gsub("-",".",gsub("230414_Mathilde_","",paste0("Intensity.",strsplit(selection," ")[[1]]))),colnames(data))]
+intdata<-data[,grep(selection,colnames(data))]
 log2Int<-as.matrix(log2(intdata))
 dim(log2Int)
 log2Int[log2Int==-Inf]=NA
-colnames(log2Int)<-gsub("Intensity.","",colnames(log2Int))
+colnames(log2Int)<-gsub(selection,"",colnames(log2Int))
 summary(log2Int)
 #corHCint####
 colnames(log2Int)
@@ -43,3 +41,10 @@ palette <- c(colorRampPalette(colors = c("yellow", "orange"))(n = length(bk1)-1)
 svgPHC<-pheatmap::pheatmap(log2IntimpCorr,color=palette,fontsize_row=6,fontsize_col=6,cluster_cols=T,cluster_rows=T)#,clustering_distance_rows= "euclidean",clustering_distance_cols="euclidean")
 ggplot2::ggsave(paste0(inpF,selection,"log2IntimpCorr.heatmap.jpg"),plot=svgPHC,width=10,height=8)
 print(paste0(inpF,selection,"log2IntimpCorr.heatmap.jpg"))
+#CV####
+intdata[intdata==0]=NA
+cvInt<-(apply(intdata,1,function(x) sd(x,na.rm=T)/mean(x,na.rm=T)))*100
+jpeg(paste0(inpF,selection,".CVhist.jpg"))
+hist(cvInt,main = "Protein Group Intensity",xlab = "%CV",ylab="Frequency",breaks=length(cvInt))
+dev.off()
+print(paste0(inpF,selection,".CVhist.jpg"))
