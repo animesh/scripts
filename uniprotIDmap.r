b634f2a1e80a7cd116c39facca7abb1105bd231f
Animesh Sharma <animeshs@ntnu.no>
2024-12-14 13:43:39 +0100

    count annotations provided by Uniprot for Bacterial taxonomy and overall count for top Biological Process categories

--- uniprotIDmap.r
+++ uniprotIDmap.r
@@ -1,6 +1,6 @@
-#C:\Users\animeshs>R-4.4.0\bin\Rscript.exe "drive\My Drive\scripts\uniprotIDmap.r"  L:\promec\Animesh\Motif\uniprot_sprot.motif.found.txt L:\promec\Animesh\Motif\GO.tsv
+#C:\Users\animeshs>R-4.4.0\bin\Rscript.exe "drive\My Drive\scripts\uniprotIDmap.r"  L:\promec\Animesh\Motif\uniprot_sprot.motif.found.txt L:\promec\Animesh\Motif\GObac.tsv
 #setup####
-#download.file("https://rest.uniprot.org/uniprotkb/stream?download=true&fields=accession%2Creviewed%2Cid%2Cprotein_name%2Cgene_names%2Corganism_name%2Clength%2Cgo_p%2Cgo_c%2Cgo_f%2Cxref_ensembl_full&format=tsv&query=%28%28database%3AGO%29+AND+%28reviewed%3Atrue%29%29","L:/promec/Animesh/Motif/GO.tsv")
+#download.file("https://rest.uniprot.org/uniprotkb/stream?download=true&fields=accession%2Creviewed%2Cid%2Cprotein_name%2Cgene_names%2Corganism_name%2Clength%2Cgo_p%2Cgo_c%2Cgo_f%2Cxref_ensembl_full&format=tsv&query=%28%28reviewed%3Atrue%29+AND+%28taxonomy_id%3A2%29%29","L:/promec/Animesh/Motif/GObac.tsv")
 args = commandArgs(trailingOnly=TRUE)
 print(paste("supplied argument(s):", length(args)))
 inpF <- args[1]
@@ -9,7 +9,7 @@ inpF <- args[1]
 annotationGO <- args[2]
 #wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
 #gunzip uniprot_sprot.fasta.gz
-#annotationGO <- "L:/promec/Animesh/Motif/GO.tsv"
+#annotationGO <- "L:/promec/Animesh/Motif/GObac.tsv"
 print(args)
 #data####
 data<-read.csv(inpF,sep="\t",header=T)
@@ -21,13 +21,18 @@ print(colnames(data))
 upData<-read.csv(annotationGO,sep="\t",header=T)
 print(dim(upData))
 print(names(upData))
+annoGOBP<-data.frame(table(unlist(strsplit(upData[,"Gene.Ontology..biological.process."], "; "))))
+#hist(annoGOBP$Freq)
+print(summary(annoGOBP$Freq))
+#dataGOBP<-dataGOBP[sort(dataGOBP$Freq,decreasing = F),]
+write.csv(annoGOBP,paste0(annotationGO,"GOID.count.csv"))
 #hist(log2(as.numeric(upData$Length)))
 dataMap<-merge(data,upData,by.y="Entry",by.x="Uniprot")#,all=TRUE)
-write.csv(dataMap,paste0(inpF,"GOIDs.csv"))
+write.csv(dataMap,paste0(inpF,basename(annotationGO),"GOIDs.csv"))
 #dataGOBP<-data.frame(table(unlist(strsplit(dataMap[,"Gene.Ontology..biological.process."], "\\[GO:|;|\\]"  , perl = TRUE))))
 dataGOBP<-data.frame(table(unlist(strsplit(dataMap[,"Gene.Ontology..biological.process."], "; "))))
 #dataGOBP<-dataGOBP[dataGOBP$Var1!="",]
 #hist(dataGOBP$Freq)
 print(summary(dataGOBP$Freq))
 #dataGOBP<-dataGOBP[sort(dataGOBP$Freq,decreasing = F),]
-write.csv(dataGOBP,paste0(inpF,"GOID.count.csv"))
+write.csv(dataGOBP,paste0(inpF,basename(annotationGO),"GOID.count.csv"))
