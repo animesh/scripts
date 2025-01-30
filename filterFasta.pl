7054d813b3252ee3685931045482b3c7e62a17cf
ani <animesh.sharma@ntnu.no>
2025-01-30 11:56:13 +0100

    remove non-ascii characters in fasta

--- filterFasta.pl
+++ filterFasta.pl
@@ -10,7 +10,7 @@ while(my $l1=<F1>){
 	chomp $l1;
 	$l1=~s/\r//g;
 	$l1=~s/\s+//g;
-	if($l1=~/^>/){$l1=~s/>//g;$seqc=$l1;}
+	if($l1=~/^>/){$l1=~s/>//g;$l1=~s/[^[:ascii:]]//g;$seqc=$l1;}
 	else{$seqm{$seqc}.=uc($l1);}
 }
 close F1;
@@ -25,7 +25,7 @@ foreach my $seqs (keys %seqm){
 
 
 __END__
-(base) ash022@DMED7596:~/animeshs/scripts$ perl filterFasta.pl IRD_UtvidaSekvens_20241127.aa.us.fasta 7 > IRD_UtvidaSekvens_20241127.aa.us.L7.fasta
-perl filterFasta.pl IRD_klon.6F.unstar.fasta 7 > IRD_klon.6F.unstar.L7.fasta
+(base) ash022@DMED7596:~/animeshs/scripts$ perl filterFasta.pl IRD_UtvidaSekvens_20241127.aa.us.fasta 7 > IRD_UtvidaSekvens_20241127.aa.us.L7.ascii.fasta
+perl filterFasta.pl IRD_klon.6F.unstar.fasta 7 > IRD_klon.6F.unstar.L7.ascii.fasta
 animeshs@dmed6942:~$ cd promec/promec/FastaDB/    
-cat UP000005640_9606_unique_gene.fasta IRD_klon.6F.unstar.L7.fasta IRD_UtvidaSekvens_20241127.aa.us.L7.fasta > human.IRD.US.fasta      
+cat UP000005640_9606_unique_gene.fasta IRD_klon.6F.unstar.L7.ascii.fasta IRD_UtvidaSekvens_20241127.aa.us.L7.ascii.fasta > human.IRD.US.ascii.fasta   
