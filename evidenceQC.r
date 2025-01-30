f1696a4a53ca3e5faacc0abd6b83797c23c89d4d
Animesh Sharma <animeshs@ntnu.no>
2025-01-30 14:57:13 +0100

    protein and evidence QC plot Murine PDAC data L:\promec\USERS\Mei\2016-05_PancreaticCancer\QE with dpi 300/40x40 inch and save as PDF

--- evidenceQC.r
+++ evidenceQC.r
@@ -1,31 +1,30 @@
-#Rscript evidenceQC.r "L:/promec/TIMSTOF/LARS/2024/240404_Mathilde/combined/txt/evidence.txt" "240404_Mathilde_TN_PDAC_P1_Slot2-4_1_7011 240404_Mathilde_TN_PDAC_P2_Slot2-5_1_7013 240404_Mathilde_TN_PDAC_P4_Slot2-7_1_7017"
+#Rscript evidenceQC.r "L:/promec/USERS/Mei/2016-05_PancreaticCancer/QE/evidence.txt" "Tumor_"
 #setup####
 args = commandArgs(trailingOnly=TRUE)
 print(paste("supplied argument(s):", length(args)))
 print(args)
 inpF <- args[1]
-#inpF<-"L:/promec/TIMSTOF/LARS/2024/240404_Mathilde/combined/txt/evidence.txt"
+#inpF<-"L:/promec/USERS/Mei/2016-05_PancreaticCancer/QE/evidence.txt"
 selection <- args[2]
-#selection<-"240404_Mathilde_TN_PDAC_P1_Slot2-4_1_7011 240404_Mathilde_TN_PDAC_P2_Slot2-5_1_7013 240404_Mathilde_TN_PDAC_P4_Slot2-7_1_7017"
-#data####
+#selection<-"Tumor_"
 data<-read.table(inpF,header = T,sep = "\t",quote = "")
-data<-data[(data$Raw.file  %in% strsplit(selection," ")[[1]]),]
-jpeg(paste0(inpF,selection,"Uncalibrated...Calibrated.m.z..ppm.jpg"))
+data<-data[grep(selection,data$Raw.file),]
+pdf(paste0(inpF,selection,"Uncalibrated...Calibrated.m.z..ppm.pdf"),width=40,height=40)
 hist(data$Uncalibrated...Calibrated.m.z..ppm.,breaks=10,density = 200,main = "Uncalibrated...Calibrated.m.z..ppm.",xlab = "PPM",col = "skyblue",)
 #hist(data$Mass.error..ppm.)
 dev.off()
-jpeg(paste0(inpF,selection,"Uncalibrated...Calibrated.m.z..ppm.density.jpg"))
+pdf(paste0(inpF,selection,"Uncalibrated...Calibrated.m.z..ppm.density.pdf"),width=40,height=40)
 d<-density(data$Uncalibrated...Calibrated.m.z..Da.,na.rm = T)
 plot(d,main = "Uncalibrated...Calibrated.m.z..Da.",xlab = "Dalton")
 polygon(d, col="skyblue", border="black")
 #hist(data$Mass.error..Da.)
 dev.off()
 dataSel<-data#[1:50000,]
-jpeg(paste0(inpF,selection,"Charge.jpg"))
+pdf(paste0(inpF,selection,"Charge.pdf"),width=40,height=40)
 #hist(dataSel$Charge)
 plot(dataSel$Length,dataSel$m.z,pch=19,col=factor(dataSel$Charge),main="Length vs m.z",xlab="Length",ylab="m.z")
 legend("bottomright",legend=levels(factor(dataSel$Charge)),col=1:4,pch=19,title="Charge")
 dev.off()
-jpeg(paste0(inpF,selection,"Missed.cleavages.jpg"))
+pdf(paste0(inpF,selection,"Missed.cleavages.pdf"),width=40,height=40)
 pie(table(dataSel$Missed.cleavages),main="Missed cleavages")
 dev.off()
--- proteinGroupsQC.r
+++ proteinGroupsQC.r
@@ -1,18 +1,18 @@
-#Rscript proteinGroupsQC.r "L:/promec/TIMSTOF/LARS/2024/241210_HELA_DDA_DIA/combined/txtDDA/proteinGroups.txt" "evosep_dda_b_S1.A2_1_9243 evosep_dda_c_S1.A3_1_9244 evosep_dda_c_S1.A4_1_9245"
+#Rscript proteinGroupsQC.r "L:/promec/USERS/Mei/2016-05_PancreaticCancer/QE/proteinGroups.txt" "Cell_"
 #setup####
 args = commandArgs(trailingOnly=TRUE)
 print(paste("supplied argument(s):", length(args)))
 inpF <- args[1]
-#inpF<-"L:/promec/TIMSTOF/LARS/2024/241210_HELA_DDA_DIA/combined/txtDDA/proteinGroups.txt"
+#inpF<-"L:/promec/USERS/Mei/2016-05_PancreaticCancer/QE/proteinGroups.txt"
 selection <- args[2]
-#selection<-"DDA_A_Slot1.54_1_9233 DDA_B_Slot1.54_1_9234 DDA_C_Slot1.54_1_9235"
+#selection<-"Cell_"
 print(args)
 #data####
 data<-read.table(inpF,header = T,sep = "\t",quote = "")
-dataCovClip50<-data[,match(gsub("-",".",gsub("241210_","",paste0("Sequence.coverage.",strsplit(selection," ")[[1]],"...."))),colnames(data))]
+dataCovClip50<-data[,grep(paste0("Sequence.coverage.",selection),colnames(data))]
 data$Sequence.coverage....<-apply(dataCovClip50,1,function(x) max(x,na.rm=T))
 #https://www.nature.com/articles/s41597-024-03355-4#Sec8
-jpeg(paste0(inpF,selection,"Sequence.coverage.jpg"))
+jpeg(paste0(inpF,"Sequence.coverage.Tumor.jpg"))
 range(data$Sequence.coverage....)
 #hist(data$Sequence.coverage....)
 dataCovClip50<-scales::squish(data$Sequence.coverage....,c(0,50))
@@ -24,7 +24,7 @@ levels(dataCovClip50Bin6)<-paste(c("0-10","10-20","20-30","30-40","40-50",">50")
 pie(table(dataCovClip50Bin6),main="Sequence coverage")
 dev.off()
 #intensity####
-intdata<-data[,match(gsub("-",".",gsub("241210_","",paste0("Intensity.",strsplit(selection," ")[[1]]))),colnames(data))]
+intdata<-data[,grep(paste0("Intensity.",selection),colnames(data))]
 log2Int<-as.matrix(log2(intdata))
 dim(log2Int)
 log2Int[log2Int==-Inf]=NA
@@ -40,13 +40,13 @@ bk1 <- c(seq(-1,-0.01,by=0.01))
 bk2 <- c(seq(0.01,1,by=0.01))
 bk <- c(bk1,bk2)  #combine the break limits for purpose of graphing
 palette <- c(colorRampPalette(colors = c("yellow", "orange"))(n = length(bk1)-1),"orange", "orange",c(colorRampPalette(colors = c("orange","red"))(n = length(bk2)-1)))
-svgPHC<-pheatmap::pheatmap(log2IntimpCorr,color=palette,fontsize_row=6,fontsize_col=6,cluster_cols=T,cluster_rows=T)#,clustering_distance_rows= "euclidean",clustering_distance_cols="euclidean")
-ggplot2::ggsave(paste0(inpF,selection,"log2IntimpCorr.heatmap.jpg"),plot=svgPHC,width=10,height=8)
-print(paste0(inpF,selection,"log2IntimpCorr.heatmap.jpg"))
+svgPHC<-pheatmap::pheatmap(log2IntimpCorr,color=palette,fontsize_row=20,fontsize_col=20,cluster_cols=T,cluster_rows=T)#,clustering_distance_rows= "euclidean",clustering_distance_cols="euclidean")
+ggplot2::ggsave(paste0(inpF,selection,"log2IntimpCorr.heatmap.pdf"),plot=svgPHC, device="pdf",dpi=300, units = "in",width=40,height=40)
+print(paste0(inpF,selection,"log2IntimpCorr.heatmap.pdf"))
 #CV####
 intdata[intdata==0]=NA
 cvInt<-(apply(intdata,1,function(x) sd(x,na.rm=T)/mean(x,na.rm=T)))*100
-jpeg(paste0(inpF,selection,".CVhist.jpg"))
+pdf(paste0(inpF,selection,".CVhist.pdf"),width=40,height=40)
 hist(cvInt,main = "Protein Group Intensity",xlab = "%CV",ylab="Frequency",breaks=length(cvInt))
 dev.off()
-print(paste0(inpF,selection,".CVhist.jpg"))
+print(paste0(inpF,selection,".CVhist.pdf"))
