77c1f60e5724962fa82e890334689fe52456a284
Animesh Sharma <animeshs@ntnu.no>
2025-04-04 10:57:47 +0200

    run latest DIA-NN with MBR, MaxQuant in parallel on windows and combine protein groups report

--- mqrun.bat
+++ mqrun.bat
@@ -1,26 +1,35 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 :: change the following paths according to the MaxQuant Installation, directory containing experiment raw files, fasta file and representative parameter file for that version respectively
-set MAXQUANTCMD="C:\Program Files\MaxQuant_v2.6.5.0\bin\MaxQuantCmd.exe"
-set DATADIR=F:\promec\TIMSTOF\LARS\2025\250107_Hela_Coli\DIA
-set FASTAFILE=F:\promec\FastaDB\UP000005640_9606_UP000000625_83333_unique_gene.fasta
+set MAXQUANTCMD="C:\Program Files\MaxQuant_v2.6.7.0\bin\MaxQuantCmd.exe"
+set DATADIR="F:\promec\TIMSTOF\LARS\2025\250402_dda_Hela"
+set FASTAFILE=F:\promec\FastaDB\UP000005640_9606_unique_gene.fasta
 set PARAMFILE=mqpar.xml
+set TMPDIR=D:\TMPDIR
+set DT=%date:~-4,4%%date:~-10,2%%date:~7,2%
+set TS=%time:~-11,2%%time:~3,2%%time:~6,2%
+mkdir %TMPDIR%\%PARAMFILE%_%DT%_%TS%
 :: leave following empty to include ALL files
 set SEARCHTEXT=TestDir
 set SEARCHTEXT2=SequencesFasta
 :: taskkill /f /im maxquant*
 :: taskkill /f /im dotnet*
-for /d %%i in (%DATADIR%\*.d) do  (
-  if exist %%i.xml del %%i.xml
+for /d %%i in (%DATADIR%\250327_HELA*DDA*.d) do  (
+  dir %%i
+  mkdir %TMPDIR%\%PARAMFILE%_%DT%_%TS%\%%~ni.results
+  mkdir %TMPDIR%\%PARAMFILE%_%DT%_%TS%\%%~ni.results\data.d
+  if exist %%i.results\%PARAMFILE% del %%i.results\%PARAMFILE%
 	for /f "tokens=1,* delims=" %%A in ( '"type %PARAMFILE%"') do (
 	SET string=%%A
-	SET modified=!string:%SEARCHTEXT%=%%i!
+	SET modified=!string:%SEARCHTEXT%=%TMPDIR%\%PARAMFILE%_%DT%_%TS%\%%~ni.results\data.d!
 	SET modified2=!modified:%SEARCHTEXT2%=%FASTAFILE%!
-  echo !modified2! >> %%i.xml
+  echo !modified2! >> %TMPDIR%\%PARAMFILE%_%DT%_%TS%\%%~ni.results\%PARAMFILE%
   )
-  dir %%i.xml
-  %MAXQUANTCMD% %%i.xml
-  echo D| xcopy  /E /Y /Q %DATADIR%\combined\txt %%i.xml.result
+  echo D| xcopy  /E /Y /Q %%i %TMPDIR%\%PARAMFILE%_%DT%_%TS%\%%~ni.results\data.d
+  dir %TMPDIR%\%PARAMFILE%_%DT%_%TS%\%%~ni.results
+  start "MQ.%DT%.%TS%.%%i" %MAXQUANTCMD% %TMPDIR%\%PARAMFILE%_%DT%_%TS%\%%~ni.results\%PARAMFILE%
 )
 :: https://stackoverflow.com/a/16079895
 :: https://stackoverflow.com/a/13805466
+:: https://stackoverflow.com/a/55519158
+:: https://stackoverflow.com/a/30231479
--- proteinGroupsCombine.py
+++ proteinGroupsCombine.py
@@ -1,14 +1,18 @@
-# mqrun.bat
-# python proteinGroupsCombine.py L:\promec\TIMSTOF\LARS\2025\250107_Hela_Coli\DDA
+# python proteinGroupsCombine.py D:\TMPDIR\mqpar.xml_20250403_135936 4 12
+# D:\TMPDIR\mqpar.xml_20250403_135936 is output of mqrun.bat
 # %%setup
 #python -m pip install pandas seaborn pathlib supervenn
 import sys
 from pathlib import Path
 # %% read
-if len(sys.argv) != 2: sys.exit("\n\nREQUIRED: pandas, seaborn, supervenn, pathlib\nUSAGE: python peptideGroupsCombine.py <path to folder containing proteinGroups.txt file(s)> <species filter> <species include>")
+if len(sys.argv) != 4: sys.exit("\n\nREQUIRED: pandas, seaborn, supervenn, pathlib\nUSAGE: python proteinGroupsCombine.py <path to folder containing proteinGroups.txt file(s)> <ID position from last> <start position within ID string>")
 pathFiles = Path(sys.argv[1])
-#pathFiles=Path("L:\\promec\\TIMSTOF\\LARS\\2025\\250107_Hela_Coli\\DDA")
+#pathFiles=Path("D:\\TMPDIR\\mqpar.xml_20250403_135936")
 fileName='proteinGroups.txt'
+fNID=int(sys.argv[2])
+#fNID=4
+fnIDpart=int(sys.argv[3])
+#fnIDpart=12
 trainList=list(pathFiles.rglob(fileName))
 print(trainList)
 print(len(trainList),"files found in",pathFiles)
@@ -31,13 +35,13 @@ for f in trainList:
             #print(proteinHits.columns)
             proteinHits.rename({'Protein IDs':'ID'},inplace=True,axis='columns')
             proteinHits=proteinHits[proteinHits['Reverse']!="+"]
-            proteinHits=proteinHits[proteinHits['Potential contaminant']!="+"]
+            #proteinHits=proteinHits[proteinHits['Potential contaminant']!="+"]
             #proteinHits=proteinHits[proteinHits['Only identified by site']!="+"]
-            proteinHits=proteinHits[~proteinHits['ID'].str.contains("_HUMAN",na=False)]
+            #proteinHits=proteinHits[~proteinHits['ID'].str.contains("_HUMAN",na=False)]
             proteinHitsC=proteinHits.ID.str.split(';', expand=True).set_index(proteinHits.Score).stack().reset_index(level=0, name='ID')
             proteinHitsI=proteinHits.ID.str.split(';', expand=True).set_index(proteinHits.Intensity).stack().reset_index(level=0, name='ID')
-            proteinHitsI['Name']=str(f.parts[-2])[12:27]
-            proteinHitsC['Name']=str(f.parts[-2])[12:22]
+            proteinHitsI['Name']=str(f.parts[(-1)*fNID])[fnIDpart:]
+            proteinHitsC['Name']=str(f.parts[(-1)*fNID])[fnIDpart:]
             dfI=pd.concat([dfI,proteinHitsI],sort=False)
             dfC=pd.concat([dfC,proteinHitsC],sort=False)
 print(dfI.columns)
@@ -53,26 +57,13 @@ dfIp.dropna(axis=1, how='all', inplace=True)
 dfIpDD=dfIp.drop_duplicates()    
 print(dfIpDD.notna().all(axis=1).sum())
 print(dfIpDD.count(axis=0))
+dfIpDD.to_csv(pathFiles/(fileName+'.dfIpDD.csv'))
 import numpy as np
 dfIpDDlog2=np.log2(dfIpDD+1)
 dfIpDDlog2.to_csv(pathFiles/(fileName+'.dfIpDDlog2.csv'))
-print(dfIpDDlog2.corr(method='pearson'))
-dfIpDDlog2.T.plot(kind='line',alpha=0.6,legend=False,rot=90,fontsize=10).figure.savefig(pathFiles/(fileName+"dfIpDDlog2.line.svg"),dpi=100,bbox_inches = "tight")
-print(pathFiles/(fileName+"dfIpDDlog2.line.svg"))
-#%%scatter
-import seaborn as sns
-ppS=sns.PairGrid(dfIpDDlog2)
-coefV=dfIpDDlog2.corr()
-ppS.fig.suptitle(coefV)
-ppS.map_diag(sns.histplot)
-ppS.map_lower(sns.kdeplot)
-ppS.map_upper(sns.regplot)
-ppS.savefig(pathFiles/(fileName+"dfIpDDlog2.scatter.svg"), dpi=100, bbox_inches="tight")
 #%%score
-dfC=dfC[~dfC['ID'].duplicated()]
 dfCp=dfC.pivot(index='ID', columns='Name', values='Score')
-dfCp['SumC']=dfCp.astype(str).sum(axis=1)
-dfCp=dfCp[dfCp['SumC']!=0]
+dfCp['SumC']=dfCp.sum(axis=1)
 dfCp=dfCp.sort_values('SumC',ascending=False)
 dfCp.to_csv(pathFiles/(fileName+"Combo.score.csv"))#.with_suffix('.combo.csv'))
 dfCpDD=dfCp.drop_duplicates()    
@@ -82,23 +73,3 @@ print(dfCpDD.count(axis=0))
 print("procesed "+str(len(trainList))+" "+fileName+" in dir "+str(pathFiles))
 print("results")
 print(str(pathFiles)+"\\"+fileName+"Combo.score.csv")
-# %% venn
-from supervenn import supervenn, make_sets_from_chunk_sizes
-import matplotlib.pyplot as plt
-plt.figure(figsize=(10, 10))
-dfPGIvenn = dfIpDDlog2.notna()
-dfPGIvenn['size'] = 1#dfPGIvenn.sum(axis=1)
-sets, labels = make_sets_from_chunk_sizes(dfPGIvenn)
-supervenn(sets, labels)
-#plt.show()
-plt.savefig(pathFiles/(fileName+"dfPGI.venn.svg"), dpi=100, bbox_inches="tight")
-plt.close()
-plt.figure(figsize=(10, 10))
-dfPepGSvenn = dfCpDD.notna()
-dfPepGSvenn['size'] = 1#dfPGIvenn.sum(axis=1)
-sets, labels = make_sets_from_chunk_sizes(dfPepGSvenn)
-supervenn(sets, labels)
-#plt.show()
-plt.savefig(pathFiles/(fileName+"dfPGS.venn.svg"), dpi=100, bbox_inches="tight")
-plt.close()
-
