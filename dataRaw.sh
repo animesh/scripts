da22c3887dd3d25aeba6a4037e34664e4dbce0ea
animeshs <animesh.sharma@ntnu.no>
2025-04-18 11:48:49 +0200

    rsync to sftp cron job

--- dataRaw.sh
+++ dataRaw.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-$HOME/gopath/gopath/bin/drive pull -ignore-conflict -ignore-name-clashes -no-prompt -verbose $HOME/Data/HF/ 1>> $HOME/dataRaw.1.log 2>> $HOME/dataRaw.2.log >> $HOME/dataRaw.log
-$HOME/gopath/gopath/bin/drive pull -ignore-conflict -ignore-name-clashes -no-prompt -verbose $HOME/Data/QE  1>> $HOME/dataRaw.1.log 2>> $HOME/dataRaw.2.log >> $HOME/dataRaw.log
-$HOME/gopath/gopath/bin/drive pull -ignore-conflict -ignore-name-clashes -no-prompt -verbose $HOME/Data/Elite  1>> $HOME/dataRaw.1.log 2>> $HOME/dataRaw.2.log >> $HOME/dataRaw.log
-find $HOME/Data/ -iname "*.raw" | wc 1>> $HOME/dataRaw.1.log 2>> $HOME/dataRaw.2.log >> $HOME/dataRaw.log
+sftp -P 12  ash022@login0.nird-lmd.sigma2.no << EOF
+cd TIMSTOF/
+put -R promec/promec/TIMSTOF/Raw
+bye
+EOF
+sftp -P 12  ash022@login0.nird-lmd.sigma2.no << EOF
+cd Qexactive/
+put -R promec/promec/Qexactive/Raw
+bye
+EOF
 
