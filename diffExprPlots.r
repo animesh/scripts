649ba34e6d75f053dfea3213a941e12a5adb1e0a
Animesh Sharma <animeshs@ntnu.no>
2025-04-23 21:43:21 +0200

    combine the data from two excel files and calculate the PCs for each sample

--- diffExprPlots.r
+++ diffExprPlots.r
@@ -1,17 +1,25 @@
-#Rscript diffExprPlots.r "C:/Users/animeshs/OneDrive - NTNU/Kristine/proteinGroups.txtLFQ.intensity.11224hT2Arg024hC400Arg0.050.50.05tTestBH.xlsx"
-#Rscript diffExprPlots.r "C:/Users/animeshs/OneDrive - NTNU/Kristine/proteinGroups.txtLFQ.intensity.11248hT2Arg048hC400Arg0.050.50.05tTestBH.xlsx"
+#Rscript diffExprPlots.r "C:/Users/animeshs/OneDrive - NTNU/Kristine/proteinGroups.txtLFQ.intensity.11224hT2Arg024hC400Arg0.050.50.05tTestBH.xlsx" "C:/Users/animeshs/OneDrive - NTNU/Kristine/proteinGroups.txtLFQ.intensity.11248hT2Arg048hC400Arg0.050.50.05tTestBH.xlsx"
 #setup####
 #install.packages("readxl")
 #install.packages("svglite")
 #install.packages("ggplot2")
 args = commandArgs(trailingOnly=TRUE)
-inpF <- args[1]
-#inpF<-"C:/Users/animeshs/OneDrive - NTNU/Kristine/proteinGroups.txtLFQ.intensity.11224hT2Arg024hC400Arg0.050.50.05tTestBH.xlsx"
+inpF1 <- args[1]
+#inpF1<-"C:/Users/animeshs/OneDrive - NTNU/Kristine/proteinGroups.txtLFQ.intensity.11224hT2Arg024hC400Arg0.050.50.05tTestBH.xlsx"
+inpF2 <- args[2]
+#inpF2<-"C:/Users/animeshs/OneDrive - NTNU/Kristine/proteinGroups.txtLFQ.intensity.11248hT2Arg048hC400Arg0.050.50.05tTestBH.xlsx"
 selection<-"Slot"
-data<-readxl::read_xlsx(inpF,sheet = 1)
+#data####
+data1<-readxl::read_xlsx(inpF1,sheet = 1)
+dim(data1)
+colnames(data1)
+data2<-readxl::read_xlsx(inpF2,sheet = 1)
+dim(data2)
+colnames(data2)
+data<-merge(x = data1,y = data2,by = 'RowGeneUniProtScorePeps', all = T)
 data<-data.frame(data)
 dim(data)
-#data####
+#select####
 dataS<-data[,grep(selection,colnames(data))]
 dim(dataS)
 log2LFQrem<-sapply(dataS,as.numeric)
@@ -27,21 +35,21 @@ log2LFQt<-t(log2LFQrem[complete.cases(log2LFQrem),])
 row.names(log2LFQt)<-colnames(log2LFQrem)
 plot(prcomp(log2LFQt))
 log2LFQtPCA<-prcomp(log2LFQt)
-write.csv(x = data.frame(log2LFQtPCA$x),file = paste0(inpF,"log2LFQtPCA.csv"))
+write.csv(x = data.frame(log2LFQtPCA$x),file = paste0("combo.log2LFQtPCA.csv"))
 dflog2LFQtPCA<-data.frame(log2LFQtPCA$x)
 rownames(dflog2LFQtPCA)<-gsub("\\.","-",rownames(dflog2LFQtPCA))
 rownames(dflog2LFQtPCA)
 #label####
-label<-data.frame(paste(sapply(strsplit(rownames(dflog2LFQtPCA),"_"), "[", 1)))
+label<-data.frame(paste(sapply(strsplit(rownames(dflog2LFQtPCA),"[0-9]"), "[", 2)))
 rownames(label)<-rownames(dflog2LFQtPCA)
 dflog2LFQtPCAlab<-merge(x = dflog2LFQtPCA,y = label,by = 'row.names', all = F)
-write.csv(x = dflog2LFQtPCAlab,file = paste0(inpF,"dflog2LFQtPCAlab.csv"))
+write.csv(x = dflog2LFQtPCAlab,file = paste0("combo.dflog2LFQtPCAlab.csv"))
 log2LFQtPCAsumm<-summary(log2LFQtPCA)
-write.csv(x = (log2LFQtPCAsumm$importance),file = paste0(inpF,"log2LFQtPCAsummimportance.csv"))
-svglite::svglite(paste0(inpF,"log2LFQtPCA12.svg"),width = 12, height = 10)
-op <- par(cex = 0.9)
-plot(log2LFQtPCA$x[,1], log2LFQtPCA$x[,2], pch = 16, col = factor(rownames(log2LFQt)),xlab = paste0("PC1 (", round(100*log2LFQtPCAsumm$importance[2,1],1), "%)"), ylab = paste0("PC2 (", round(100*log2LFQtPCAsumm$importance[2,2],1), "%)"),main=paste("PCA 1 and 2 with complete-case","\nTotal protein groups", dim(log2LFQt)[2],"across samples",dim(log2LFQt)[1]))
-legend("topleft", col = factor(rownames(log2LFQt)), legend = factor(rownames(log2LFQt)), pch = 16)
+write.csv(x = (log2LFQtPCAsumm$importance),file = paste0("combo.log2LFQtPCAsummimportance.csv"))
+svglite::svglite(paste0("combo.log2LFQtPCA12.svg"),width = 10, height = 8)
+op <- par(cex = 0.8,mar = c(10, 4, 3, 3), xpd = TRUE)
+plot(dflog2LFQtPCAlab[,"PC1"], dflog2LFQtPCAlab[,"PC2"], pch = 16, col = factor(dflog2LFQtPCAlab[,"paste.sapply.strsplit.rownames.dflog2LFQtPCA.....0.9.........."]),xlab = paste0("PC1 (", round(100*log2LFQtPCAsumm$importance[2,1],1), "%)"), ylab = paste0("PC2 (", round(100*log2LFQtPCAsumm$importance[2,2],1), "%)"),main=paste("PCA 1 and 2 with complete-case","\nTotal protein groups", dim(log2LFQt)[2],"across samples",dim(log2LFQt)[1]))
+legend("topright", inset = c(0.7, 1.1),col = factor(dflog2LFQtPCAlab[,"paste.sapply.strsplit.rownames.dflog2LFQtPCA.....0.9.........."]), legend = factor(dflog2LFQtPCAlab[,"paste.sapply.strsplit.rownames.dflog2LFQtPCA.....0.9.........."]), pch = 16)
 dev.off()
 #PCAimp####
 scale=2
@@ -63,16 +71,15 @@ log2LFQt<-t(dataHighNormLog2)
 row.names(log2LFQt)<-colnames(log2LFQrem)
 plot(prcomp(log2LFQt))
 log2LFQtPCA<-prcomp(log2LFQt)
-write.csv(x = data.frame(log2LFQtPCA$x),file = paste0(inpF,"log2LFQtPCAimp.csv"))
+write.csv(x = data.frame(log2LFQtPCA$x),file = paste0("combo.log2LFQtPCAimp.csv"))
 dflog2LFQtPCA<-data.frame(log2LFQtPCA$x)
 rownames(dflog2LFQtPCA)<-gsub("\\.","-",rownames(dflog2LFQtPCA))
 rownames(dflog2LFQtPCA)
 dflog2LFQtPCAlab<-merge(x = dflog2LFQtPCA,y = label,by = 'row.names', all = F)
-write.csv(x = dflog2LFQtPCAlab,file = paste0(inpF,"dflog2LFQtPCAimplab.csv"))
+write.csv(x = dflog2LFQtPCAlab,file = paste0("combo.dflog2LFQtPCAimplab.csv"))
 log2LFQtPCAsumm<-summary(log2LFQtPCA)
-write.csv(x = (log2LFQtPCAsumm$importance),file = paste0(inpF,"log2LFQtPCAimpsummimportance.csv"))
-svglite::svglite(paste0(inpF,"log2LFQtPCA12imp.svg"),width = 12, height = 10)
-op <- par(cex = 0.8)
+write.csv(x = (log2LFQtPCAsumm$importance),file = paste0("combo.log2LFQtPCAimpsummimportance.csv"))
+svglite::svglite(paste0("combo.log2LFQtPCA12imp.svg"),width = 12, height = 10)
 plot(log2LFQtPCA$x[,1], log2LFQtPCA$x[,2], pch = 16, col = factor(rownames(log2LFQt)),xlab = paste0("PC1 (", round(100*log2LFQtPCAsumm$importance[2,1],1), "%)"), ylab = paste0("PC2 (", round(100*log2LFQtPCAsumm$importance[2,2],1), "%)"),main=paste("PCA 1/2 with 0 containing proteinGroups imputed scale",scale,"\nProtein groups", dim(log2LFQt)[2],"across samples",dim(log2LFQt)[1]))
 legend("topleft", col = factor(rownames(log2LFQt)), legend = factor(rownames(log2LFQt)), pch = 16)
 dev.off()
--- runDIANN.bat
+++ runDIANN.bat
@@ -1,9 +1,100 @@
+@echo off
+:: fix from https://claude.ai/share/17edbd27-098f-432b-b48e-ccb421318621
 :: install https://github.com/vdemichev/DiaNN/releases/download/2.0/DIA-NN-2.1.0-Academia.msi
 :: download and gunzip https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000005640/UP000005640_9606.fasta.gz
 :: move UP000005640_9606.fasta F:\promec\FastaDB\UP000005640_9606_unique_gene.fasta
+
 SET workDir=%cd%
-set NCPU=64
+SET NCPU=64
+SET CAMPROTPATH=camprotR_240512_cRAP_20190401_full_tags.fasta
+
 cd "C:\Program Files\DIA-NN\2.1.0\"
-diann.exe --lib "" --threads 64 --verbose 1 --out "F:\promec\FastaDB\UP000005640_9606_unique_gene.met.acet.fixprop.report.parquet" --qvalue 0.01 --matrices  --out-lib "F:\promec\FastaDB\UP000005640_9606_unique_gene.met.acet.fixprop.report-lib.parquet" --gen-spec-lib --predictor --reannotate --fasta camprotR_240512_cRAP_20190401_full_tags.fasta --cont-quant-exclude cRAP- --fasta "F:\promec\FastaDB\UP000005640_9606_unique_gene.fasta" --fasta-search --min-fr-mz 100 --max-fr-mz 1700 --met-excision --min-pep-len 7 --max-pep-len 35 --min-pr-mz 100 --max-pr-mz 1700 --min-pr-charge 2 --max-pr-charge 4 --cut K*,R* --missed-cleavages 2 --unimod4 ‐‐fixed‐mod UniMod:58,56.026215,n --var-mods 3 --var-mod UniMod:35,15.994915,M --var-mod UniMod:1,42.010565,K --var-mod UniMod:209,112.052430,K --var-mod UniMod:34,14.015650,K --var-mod UniMod:36,28.031300,K --var-mod UniMod:37,42.046950,K --mass-acc 15 --mass-acc-ms1 15 --individual-mass-acc --individual-windows --proteoforms  --no-cut-after-mod UniMod:58 --reanalyse --rt-profiling
-diann.exe --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_CTR_DIA_A_Slot1-23_1_10160.d" --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_CTR_DIA_B_Slot1-23_1_10164.d" --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_CTR_DIA_C_Slot1-23_1_10168.d" --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_SAHA_DIA_A_Slot1-24_1_10162.d" --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_SAHA_DIA_B_Slot1-24_1_10166.d" --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_SAHA_DIA_C_Slot1-24_1_10170.d" --lib "F:\promec\FastaDB\UP000005640_9606_unique_gene.met.acet.fixprop.report-lib.predicted.speclib" --threads 64 --verbose 1 --out "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\DIANN2p1\met.acet.report.parquet" --qvalue 0.01 --matrices  --out-lib "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\DIANN2p1\met.acet.fixprop.lib.parquet"  --fasta camprotR_240512_cRAP_20190401_full_tags.fasta --cont-quant-exclude cRAP- --fasta F:\promec\FastaDB\UP000005640_9606_unique_gene.fasta --min-fr-mz 100 --max-fr-mz 1700 --met-excision --min-pep-len 7 --max-pep-len 35 --min-fr-mz 100 --max-fr-mz 1700 --met-excision --min-pep-len 7 --max-pep-len 35 --min-pr-mz 100 --max-pr-mz 1700 --min-pr-charge 2 --max-pr-charge 4 --cut K*,R* --missed-cleavages 2 --unimod4 ‐‐fixed‐mod UniMod:58,56.026215,n --var-mods 3 --var-mod UniMod:35,15.994915,M --var-mod UniMod:1,42.010565,K --var-mod UniMod:209,112.052430,K --var-mod UniMod:34,14.015650,K --var-mod UniMod:36,28.031300,K --var-mod UniMod:37,42.046950,K --mass-acc 15 --mass-acc-ms1 15 --proteoforms  --no-cut-after-mod UniMod:58 --reanalyse --rt-profiling
+
+:: First command - Generate spectral library
+diann.exe --lib "" ^
+ --threads %NCPU% ^
+ --verbose 1 ^
+ --out "F:\promec\FastaDB\UP000005640_9606_unique_gene.met.acet.fixprop.report.parquet" ^
+ --qvalue 0.01 ^
+ --matrices ^
+ --out-lib "F:\promec\FastaDB\UP000005640_9606_unique_gene.met.acet.fixprop.report-lib.parquet" ^
+ --gen-spec-lib ^
+ --predictor ^
+ --reannotate ^
+ --fasta "%CAMPROTPATH%" ^
+ --cont-quant-exclude cRAP- ^
+ --fasta "F:\promec\FastaDB\UP000005640_9606_unique_gene.fasta" ^
+ --fasta-search ^
+ --min-fr-mz 100 ^
+ --max-fr-mz 1700 ^
+ --met-excision ^
+ --min-pep-len 7 ^
+ --max-pep-len 35 ^
+ --min-pr-mz 100 ^
+ --max-pr-mz 1700 ^
+ --min-pr-charge 2 ^
+ --max-pr-charge 4 ^
+ --cut K*,R* ^
+ --missed-cleavages 2 ^
+ --unimod4 --fixed-mod UniMod:58,56.026215,n ^
+ --var-mods 3 ^
+ --var-mod UniMod:35,15.994915,M ^
+ --var-mod UniMod:1,42.010565,K ^
+ --var-mod UniMod:209,112.052430,K ^
+ --var-mod UniMod:34,14.015650,K ^
+ --var-mod UniMod:36,28.031300,K ^
+ --var-mod UniMod:37,42.046950,K ^
+ --mass-acc 15 ^
+ --mass-acc-ms1 15 ^
+ --individual-mass-acc ^
+ --individual-windows ^
+ --proteoforms ^
+ --no-cut-after-mod UniMod:58 ^
+ --reanalyse ^
+ --rt-profiling
+
+:: Second command - Process samples
+diann.exe ^
+ --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_CTR_DIA_A_Slot1-23_1_10160.d" ^
+ --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_CTR_DIA_B_Slot1-23_1_10164.d" ^
+ --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_CTR_DIA_C_Slot1-23_1_10168.d" ^
+ --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_SAHA_DIA_A_Slot1-24_1_10162.d" ^
+ --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_SAHA_DIA_B_Slot1-24_1_10166.d" ^
+ --f "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\250411_HISTONE_SAHA_DIA_C_Slot1-24_1_10170.d" ^
+ --lib "F:\promec\FastaDB\UP000005640_9606_unique_gene.met.acet.fixprop.report-lib.predicted.speclib" ^
+ --threads %NCPU% ^
+ --verbose 1 ^
+ --out "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\DIANN2p1\met.acet.report.parquet" ^
+ --qvalue 0.01 ^
+ --matrices ^
+ --out-lib "F:\promec\TIMSTOF\LARS\2025\250411_HISTONE_SAHA\DIANN2p1\met.acet.fixprop.lib.parquet" ^
+ --fasta "%CAMPROTPATH%" ^
+ --cont-quant-exclude cRAP- ^
+ --fasta "F:\promec\FastaDB\UP000005640_9606_unique_gene.fasta" ^
+ --min-fr-mz 100 ^
+ --max-fr-mz 1700 ^
+ --met-excision ^
+ --min-pep-len 7 ^
+ --max-pep-len 35 ^
+ --min-pr-mz 100 ^
+ --max-pr-mz 1700 ^
+ --min-pr-charge 2 ^
+ --max-pr-charge 4 ^
+ --cut K*,R* ^
+ --missed-cleavages 2 ^
+ --unimod4 --fixed-mod UniMod:58,56.026215,n ^
+ --var-mods 3 ^
+ --var-mod UniMod:35,15.994915,M ^
+ --var-mod UniMod:1,42.010565,K ^
+ --var-mod UniMod:209,112.052430,K ^
+ --var-mod UniMod:34,14.015650,K ^
+ --var-mod UniMod:36,28.031300,K ^
+ --var-mod UniMod:37,42.046950,K ^
+ --mass-acc 15 ^
+ --mass-acc-ms1 15 ^
+ --proteoforms ^
+ --no-cut-after-mod UniMod:58 ^
+ --reanalyse ^
+ --rt-profiling
+
 cd %workDir%
