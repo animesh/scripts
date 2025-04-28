6fd0334fa4e5893280e76175d47a5fbf48930d2f
Animesh Sharma <ash022@login-1.saga>
2025-04-28 13:28:13 +0200

    find and tar/backup, basic DIA-NN search batch mode saga

--- backUp.sh
+++ backUp.sh
@@ -1,11 +1,15 @@
 mkdir tes
 tar cvf t.tar *.txt *.log
 tar -xf $PWD/t.tar -C $PWD/tes/ --wildcards --ignore-case "*.txt"
-for i in /nird/projects/NS9036K/NORSTORE_OSL_TAPE/PROMECBkUp/*.tar.gz ; do echo $i;  tar -xvzf $i -C /nird/projects/NS9036K/taraw/ --wildcards --ignore-case "*.raw" ; done
-find /nird/home/ash022/promec/NORSTORE_OSL_DISK/NS9036K/promec/promec/ -iname "*.raw" -exec cp "{}" DL/orbitrap/ \;
-ls DL/orbitrap/ | wc #19077 
-find /nird/home/ash022/promec/NORSTORE_OSL_DISK/NS9036K/promec/promec/USERS/ -iname "*.raw" -exec cp "{}" DL/orbitrap/ \;
-find /nird/home/ash022/promec/NORSTORE_OSL_DISK/NS9036K/promec/promec/Elite/ -iname "*.raw" -exec cp "{}" DL/orbitrap/ \;
-find /nird/home/ash022/promec/NORSTORE_OSL_DISK/NS9036K/promec/promec/Qexactive/ -iname "*.raw" -exec cp "{}" DL/orbitrap/ \;
-find /nird/home/ash022/promec/NORSTORE_OSL_DISK/NS9036K/promec/promec/HF/ -iname "*.raw" -exec cp "{}" DL/orbitrap/ \;
-
+cd tes
+ssh-copy-id 'ash022@login0.nird-lmd.sigma2.no'
+sftp 'ash022@login0.nird-lmd.sigma2.no'
+#Connected to login0.nird-lmd.sigma2.no.
+#sftp> get -a *.txt
+find PD/ -iname "*220218_CVID_11*.d"
+ls -1d PD/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/*.d | wc
+#     63      63    5022
+for i in  PD/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/*.d ; do echo $i ; tar cvf  $i.tar  $i ; done
+rm PD/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/combined/txt.tar*
+for i in  PD/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/combined/txt* ; do echo $i ; tar cvf  $i.tar  $i ; done
+eval $(ssh-agent);ssh-add;rclone copy --include "/*.tar" -Pv NS9036K:PD/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/ promec/promec/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/
--- slurmDIANN.sh
+++ slurmDIANN.sh
@@ -1,15 +1,15 @@
+#bash slurmDIANN.sh /cluster/projects/nn9036k/scripts/kamilla 
 #cd /cluster/home/ash022/scripts
+#cp /nird/projects/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/FastaDB/UP000005640_9606_unique_gene.fasta /cluster/projects/nn9036k/diann-2.1.0/
+#cp /cluster/projects/nn9036k/diann-2.0/camprotR_240512_cRAP_20190401_full_tags.fasta /cluster/projects/nn9036k/diann-2.1.0/
+#cp /nird/projects/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/FastaDB/UP000005640_9606_unique_gene_C24_MC1_P735_MZ1001700_Mod3.predicted.speclib /cluster/projects/nn9036k/FastaDB/.
+#mkdir kamilla
+#cp -rf PD/TIMSTOF/LARS/2025/250428_Kamilla/*.d kamilla/.
 #dos2unix slurmDIANN.sh scratch.slurm
-#for i in acet/25041*.d.1745142787.report.slurm ; do echo $i ; sed -i s/--threads\ 80/--threads\ 40/g $i;  done
-#bash slurmDIANN.sh /cluster/projects/nn9036k/scripts/acet 
-#squeue -u ash022
+#for i in kamilla/*.d*slurm ; do echo $i ; sed -i s/--threads\ 80/--threads\ 40/g $i;  done
 #wget https://github.com/vdemichev/DiaNN/releases/download/2.0/DIA-NN-2.1.0-Academia-Linux.zip
 #unzip DIA-NN-2.1.0-Academia-Linux.zip
 #cp -rf diann-2.1.0 /cluster/projects/nn9036k/
-#cp /nird/projects/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/FastaDB/UP000005640_9606_unique_gene.fasta /cluster/projects/nn9036k/diann-2.1.0/
-#cp /cluster/projects/nn9036k/diann-2.0/camprotR_240512_cRAP_20190401_full_tags.fasta /cluster/projects/nn9036k/diann-2.1.0/
-#mkdir acet
-#cp -rf /nird/projects/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2025/250411_HISTONE_SAHA/*DIA*.d acet/.
 #cd /cluster/projects/nn9036k/diann-2.1.0/
 #chmod +x diann.exe
 DATADIR=$1
