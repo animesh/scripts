5587c8182c13ee17385f94b0478573e9685ecd0a
Animesh Sharma <animeshs@ntnu.no>
2025-04-29 14:44:47 +0200

    last three controls sign-rank test with rest

--- diffExprWSRT.r
+++ diffExprWSRT.r
@@ -1,38 +1,36 @@
-fo='L:\\promec\\HF\\Lars\\2020\\AUGUST\\siri\\combined\\txt_PHOSTY\\proteinGroups.txt';#MaxQuant Output
+fo='L:\\promec\\Animesh\\Jana\\proteinGroups.txt';#MaxQuant Output
 testtype='WSRT';
 data=read.table(fo,stringsAsFactors=F,header=T,quote="",comment.char="",sep="\t");
-summary(data)
-IDX=c(1,7,8);#Uniprots, Gene Name, Fasta header
-sdx=108;#LFQ start column
-edx=119;#end
+print(dim(data))
+IDX=c(1,6,7);#Uniprots, Protein, Gene
+sdx=150;#LFQ start column
+edx=167;#end
 rep=3;#jump between repeats
 log2data=log2(as.matrix(data[,sdx:edx])+1);
-summary(log2data)
-log2ctr=log2data[,seq(1,ncol(log2data),by=ceiling((edx-sdx+1)/rep))];
-log2ctr=matrix(rep(as.numeric(t(log2ctr)),each=ceiling((edx-sdx+1)/rep)),nrow=nrow(log2ctr),byrow=TRUE)
-summary(log2ctr)
-log2data=log2data-log2ctr;
-log2data[log2data==0]=NaN;
-hist(log2data)
-summary(log2data)
+print(dim(log2data))
+log2ctr=log2data[,seq(ncol(log2data)-2,ncol(log2data))];
+log2ctrReps=do.call(cbind, replicate(ncol(log2data)/rep, log2ctr, simplify = FALSE))
+log2compare=log2data-log2ctrReps;
 idnm=data[,IDX];
 id=colnames(data[,sdx:edx]);
-pv=matrix(0L,nrow(data),ceiling((edx-sdx+1)/rep));
-summary(pv)
+pv=matrix(0L,nrow(data),ncol(log2data)/rep-1);
 ln=matrix(0L,nrow(data),1);
-summary(ln)
-hdr=matrix(0L,1,ncol(log2data)/rep)
-summary(hdr)
-for(j in 1:ncol(log2data)/rep){
-    hdr[j]=paste0('Col_',j,id[j],testtype);
+hdr=matrix(0L,1,ncol(log2data)/rep-1)
+for(j in 1:ncol(pv)){
+    hdr[j]=paste0('pValue_',j,id[j*rep],testtype);
     print(hdr[j]);
     for(i in 1:nrow(log2data)){
-      tmparr=log2data[i,seq(j,ncol(log2data),by=ncol(log2data)/rep)];
-      ln[i]=i;
-       if(sum(is.na(tmparr))<length(tmparr)){pv[i,j]=wilcox.test(tmparr)$p.value;}
-       else{pv[i,j]=1;}
+        tmparr=log2compare[i,seq(j*rep-2,j*rep)];
+        tmparr[tmparr==0]=NA;
+        ln[i]=i;
+        if(sum(!is.na(tmparr))>0){pv[i,j]=wilcox.test(tmparr)$p.value;}
+        else{pv[i,j]=NA;}
     }
 }
 colnames(pv)<-hdr;
-hist(pv)
-write.csv(as.data.frame(cbind(idnm,pv,log2data)),row.names=FALSE,paste0(fo,testtype,"log2.csv"))
+print(summary(pv))
+fw=paste0(fo,testtype,"log2.csv")
+write.csv(as.data.frame(cbind(idnm,pv,log2compare,log2data)),row.names=FALSE,fw)
+print(summary(warnings()))
+print(fw)
+
