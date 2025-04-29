8bde5a126435afbf96b16ed77afbb92b28f01e8a
Animesh Sharma <animeshs@ntnu.no>
2025-04-29 20:34:19 +0200

    submit data for https://www.jacionline.org/article/S0091-6749(22)01335-5/fulltext

--- backUp.sh
+++ backUp.sh
@@ -6,10 +6,10 @@ ssh-copy-id 'ash022@login0.nird-lmd.sigma2.no'
 sftp 'ash022@login0.nird-lmd.sigma2.no'
 #Connected to login0.nird-lmd.sigma2.no.
 #sftp> get -a *.txt
-find PD/ -iname "*220218_CVID_11*.d"
-ls -1d PD/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/*.d | wc
-#     63      63    5022
-for i in  PD/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/*.d ; do echo $i ; tar cvf  $i.tar  $i ; done
-rm PD/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/combined/txt.tar*
-for i in  PD/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/combined/txt* ; do echo $i ; tar cvf  $i.tar  $i ; done
-eval $(ssh-agent);ssh-add;rclone copy --include "/*.tar" -Pv NS9036K:PD/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/ promec/promec/Qexactive/Mirta/MariKarbo_CVID_2022/RawData/
+find PD/Qexactive/Mirta/ -iname "*20210309_CVID_1180*"
+ls -1 PD/Qexactive/Mirta/MariKaarbo/*.raw | wc
+#     37      37    1973
+tar cvf PD/Qexactive/Mirta/MariKaarbo/txt.tar PD/Qexactive/Mirta/MariKaarbo/combined/txt
+mkdir -p promec/promec/Qexactive/Mirta/MariKaarbo/
+eval $(ssh-agent);ssh-add;rclone copy --include "/*.raw" -Pv NS9036K:PD/Qexactive/Mirta/MariKaarbo/  promec/promec/Qexactive/Mirta/MariKaarbo/
+eval $(ssh-agent);ssh-add;rclone copy --include "/*.tar" -Pv NS9036K:PD/Qexactive/Mirta/MariKaarbo/  promec/promec/Qexactive/Mirta/MariKaarbo/
