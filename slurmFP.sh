f5bea0a7224491114b6e397091031de7b2dcc2f3
Animesh Sharma <ash022@login-1.saga>
2025-09-01 17:58:02 +0200

    fragpipe in batch mode over saga

--- slurmFP.sh
+++ slurmFP.sh
@@ -1,36 +1,12 @@
-#git checkout b9c1abe446bcb7bd7f94882b63f97546fcc9e0f9 scratch.slurm
-#scp -r ash022@login1.nird-lmd.sigma2.no:PD/Qexactive/LARS/2015/november/Jurkat_Sudhl5\\\ SAHA\\\ B2_3/*.raw .
-#bash slurmFP.sh $PWD/saha /cluster/projects/nn9036k/FastaDB/2022-04-21-decoys-isoforms-contam-UP000005640.fas scratch.slurm fp.workflow.txt
+#dos2unix slurmFP.sh
+#bash slurmFP.sh $PWD/240827_Bead_test/dda
 DATADIR=$1
-FASTA=$2
-SLURM=$3
-WORKFLOW=$4
 CURRENTEPOCTIME=`date +%s`
-WRITEDIR=$WORKFLOW.$CURRENTEPOCTIME.results
+WRITEDIR=slurmFP.$CURRENTEPOCTIME.results
+SLURM=scratch.slurm
 mkdir $WRITEDIR
-#perl -pe 's/\r$//' < mqrun.sh  > tmp
+#perl -pe 's/\r$//' < slurmFP.sh  > tmp
 dos2unix $SLURM
-cp -rf $DATADIR/*.raw $WRITEDIR/.
-ls -ltrh $WRITEDIR/*.raw > $LOG
-sed "s|FPFASTAFILE|$FASTA|" $WORKFLOW > $WRITEDIR/fp.workflow.txt
-for i in $PWD/$WRITEDIR/*.raw
-    do echo $i
-    j=$(basename $i)
-    k=${j%%.*}
-    d=$PWD/$WRITEDIR/$k
-    mkdir $d 
-    printf "$i\t$k\t\tDDA\n" | tee $d/$k.txt
-    LOG=$i.out.slurm.txt
-    #touch $LOG
-    cat $d/$k.txt
-    sed "s|FPSLURMNAME|$k|" $SLURM > $d/$k.slurm.tmp
-    sed "s|FPMANIFESTFILE|$d/$k\.txt|" $d/$k.slurm.tmp > $d/$k.slurm.tmp2
-    sed "s|FPWORKDIR|$d|" $d/$k.slurm.tmp2 > $d/$k.slurm.tmp
-    sed "s|FPSLURMLOG|$LOG|" $d/$k.slurm.tmp > $d/$k.slurm.tmp2
-    sed "s|FPWORKFLOW|$WRITEDIR/fp.workflow.txt|" $d/$k.slurm.tmp2 > $d/$k.slurm
-    ls -ltrh $d/$k.slurm
-    cat $d/$k.slurm
-    sbatch $d/$k.slurm
-done
-head -n 9 $WRITEDIR/fp.workflow.txt
+#cp -rf $DATADIR/*.raw $WRITEDIR/.
+for i in  $DATADIR/*.d; do echo $i; sed "s|RAWDATA|$i|" fp.manifest.txt > $i.$CURRENTEPOCTIME.manifest.txt ; sed "s|RAWDATA|$i.$CURRENTEPOCTIME|g" scratch.slurm > $i.$CURRENTEPOCTIME.slurm ; sbatch $i.$CURRENTEPOCTIME.slurm ; done
 
