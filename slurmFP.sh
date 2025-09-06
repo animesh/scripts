f97d58b33b583e4b15d9c66ada2104e3ff3539f3
Animesh Sharma <ash022@login-1.saga>
2025-09-06 19:31:51 +0200

    create same local directory, copy and batch run fragpipe

--- slurmFP.sh
+++ slurmFP.sh
@@ -1,12 +1,16 @@
 #dos2unix slurmFP.sh
-#bash slurmFP.sh $PWD/240827_Bead_test/dda
+#bash slurmFP.sh /nird/projects/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/Raw/ani
 DATADIR=$1
 CURRENTEPOCTIME=`date +%s`
-WRITEDIR=slurmFP.$CURRENTEPOCTIME.results
 SLURM=scratch.slurm
+WRITEDIR=`basename $DATADIR`
 mkdir $WRITEDIR
+ls -ltrh $DATADIR/*.d/analysis.tdf_bin
+rsync -Parv $DATADIR/*.d $WRITEDIR/ 
+#cp -rf $DATADIR/*.d $WRITEDIR/.
+ls -ltrh $DATADIR/*.d/analysis.tdf_bin > $WRITEDIR/files.txt
 #perl -pe 's/\r$//' < slurmFP.sh  > tmp
 dos2unix $SLURM
 #cp -rf $DATADIR/*.raw $WRITEDIR/.
-for i in  $DATADIR/*.d; do echo $i; sed "s|RAWDATA|$i|" fp.manifest.txt > $i.$CURRENTEPOCTIME.manifest.txt ; sed "s|RAWDATA|$i.$CURRENTEPOCTIME|g" scratch.slurm > $i.$CURRENTEPOCTIME.slurm ; sbatch $i.$CURRENTEPOCTIME.slurm ; done
+for i in  $PWD/$WRITEDIR/*.d; do echo $i; sed "s|RAWDATA|$i|" fp.manifest.txt > $i.$CURRENTEPOCTIME.manifest.txt ; sed "s|RAWDATA|$i.$CURRENTEPOCTIME|g" scratch.slurm > $i.$CURRENTEPOCTIME.slurm ; sbatch $i.$CURRENTEPOCTIME.slurm ; done
 
