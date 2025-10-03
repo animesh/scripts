4b137374536527f68b2e13abf2ac63b520effa04
Animesh Sharma <animeshs@ntnu.no>
2025-10-03 12:51:04 +0200

    try 50ppm for DIA

--- runDIANN.bat
+++ runDIANN.bat
@@ -1,51 +1,65 @@
-:: runDIANN.bat "F:\promec\TIMSTOF\LARS\2025\250929_Rabina_DIA" 18 --high-acc
-:: Usage: runDIANN.bat <dataDir> <NCPU> [mode]
+:: runDIANN.bat F:\promec\TIMSTOF\LARS\2025\250929_Rabina_DIA 4
+:: runDIANN.bat F:\promec\TIMSTOF\LARS\2025\250902_Alessandro 10 --high-acc
+:: runDIANN.bat F:\promec\TIMSTOF\LARS\2025\250902_Alessandro 12 --direct-quant
 :: DIA-NN 2.2.0 Academia  (Data-Independent Acquisition by Neural Networks) Compiled on May 29 2025 21:29:29 Current date and time: Tue Aug  5 09:36:11 2025 CPU: GenuineIntel Intel(R) Xeon(R) CPU E5-2683 v4 @ 2.10GHz SIMD instructions: AVX AVX2 FMA SSE4.1 SSE4.2 Logical CPU cores: 64 Thread number set to 32 Output will be filtered at 0.01 FDR Precursor/protein x samples expression level matrices will be saved along with the main report A spectral library will be generated
 :: wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000005640/UP000005640_9606.fasta.gz
 :: gunzip UP000005640_9606.fasta.gz
 :: copy UP000005640_9606.fasta F:\promec\FastaDB\UP000005640_9606_unique_gene.fasta
 
-
 @echo off
 setlocal enabledelayedexpansion
 
-@echo off
-setlocal enabledelayedexpansion
+SET dataDir=%1
+SET NCPU=%2
+SET mode=%3
+SET workDir=%cd%
 
-SET "dataDir=%~1"
-SET "NCPU=%~2"
-SET "mode=%~3"
-
-if "%dataDir%"=="" exit /b 1
-if "%NCPU%"=="" exit /b 1
-
-SET dirMode=default
+:: Create a clean directory name from mode parameter
+SET dirMode=
 if not "%mode%"=="" (
+    :: Remove -- prefix and - characters, keep only alphanumeric
     SET tempMode=%mode%
     SET tempMode=!tempMode:--=!
     SET tempMode=!tempMode:-=!
     SET tempMode=!tempMode: =!
     SET dirMode=!tempMode!
+) else (
+    SET dirMode=default
 )
 
-set /a dirCount=0
-for /d %%i in ("%dataDir%\*.d") do set /a dirCount+=1
+echo Using mode parameter: %mode%
+echo Using directory suffix: %dirMode%
+echo.
 
-if %dirCount%==0 exit /b 1
+for /d %%i in (%dataDir%\*.d) do (
+    echo Processing: %%i
 
-for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
-set "timestamp=%dt:~2,2%%dt:~4,2%%dt:~6,2%_%dt:~8,2%%dt:~10,2%%dt:~12,2%"
+    cd "C:\Program Files\DIA-NN\2.2.0\"
 
-set outputDir=%dataDir%\DIANNv2P2.%dirCount%.%timestamp%.%NCPU%.!dirMode!
-mkdir "%outputDir%" 2>nul
+    :: Create output directory using clean directory name
+    set outputDir=%%i.DIANNv2P2.%NCPU%.!dirMode!
+    echo Creating directory: !outputDir!
+    mkdir "!outputDir!" 2>nul
 
-set fileList=
-for /d %%i in ("%dataDir%\*.d") do set fileList=!fileList! --f "%%i"
+    :: Build DIA-NN command with mode parameter (if provided)
+    set diannCmd=diann.exe --f "%%i" --lib "F:\promec\FastaDB\UP000005640_9606_unique_gene_MC2V3.predicted.predicted.speclib" --threads %NCPU% --verbose 1 --out "!outputDir!\report.parquet" --qvalue 0.01 --matrices --out-lib "!outputDir!\report-lib.parquet" --gen-spec-lib --reannotate --fasta "camprotR_240512_cRAP_20190401_full_tags.fasta" --cont-quant-exclude cRAP- --fasta "F:\promec\FastaDB\UP000005640_9606_unique_gene.fasta" --met-excision --cut K*,R* --missed-cleavages 2 --unimod4 --var-mods 3 --var-mod UniMod:35,15.994915,M --var-mod UniMod:1,42.010565,*n --mass-acc 50.0 --mass-acc-ms1 50 --peptidoforms --rt-profiling
 
-cd "C:\Program Files\DIA-NN\2.2.0\"
+    :: Add mode parameter to DIA-NN command if it exists
+    if not "%mode%"=="" (
+        set diannCmd=!diannCmd! %mode%
+    )
 
-set diannCmd=diann.exe!fileList! --lib "F:\promec\FastaDB\UP000005640_9606_unique_gene_MC2V3.predicted.predicted.speclib" --threads %NCPU% --verbose 1 --out "%outputDir%\report.parquet" --qvalue 0.01 --matrices --out-lib "%outputDir%\replib.parquet" --gen-spec-lib --reannotate --fasta "camprotR_240512_cRAP_20190401_full_tags.fasta" --cont-quant-exclude cRAP- --fasta "F:\promec\FastaDB\UP000005640_9606_unique_gene.fasta" --met-excision --min-pep-len 7 --max-pep-len 30 --min-pr-mz 300 --max-pr-mz 1800 --min-pr-charge 1 --max-pr-charge 4 --cut K*,R* --missed-cleavages 2 --unimod4 --var-mods 3 --var-mod UniMod:35,15.994915,M --var-mod UniMod:1,42.010565,*n --mass-acc 15.0 --mass-acc-ms1 15.0 --peptidoforms --reanalyse --rt-profiling
+    echo.
+    echo Executing: !diannCmd!
+    echo.
 
-if not "%mode%"=="" set diannCmd=!diannCmd! %mode%
+    :: Run DIA-NN
+    start "DIANNv2P2.%NCPU%.!dirMode!.%%i" !diannCmd!
 
-!diannCmd!
+    :: List contents of output directory after completion
+    echo Contents of !outputDir!:
+    dir "!outputDir!"
+    echo.
+
+    cd "%workDir%"
+)
