dffff4543251b80a52e72c9bde9c85d70a00f613
animeshs <animesh.sharma@ntnu.no>
2025-10-07 16:53:15 +0200

    backup twice,hourly for Raw

--- dataRaw.sh
+++ dataRaw.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-sftp -P 12  ash022@login0.nird-lmd.sigma2.no << EOF
-cd TIMSTOF/
-put -R promec/promec/TIMSTOF/Raw
+sftp -P 12  ash022@login.nird.sigma2.no << EOF
+ls TIMSTOF/Raw/*/*
 bye
 EOF
-sftp -P 12  ash022@login0.nird-lmd.sigma2.no << EOF
-cd Qexactive/
-put -R promec/promec/Qexactive/Raw
+sftp -P 12  ash022@login.nird.sigma2.no << EOF
+ls Qexactive/Raw
 bye
 EOF
 
