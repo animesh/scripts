d9137ebd7aa0e3039babcc295f8aa853b3622683
Animesh Sharma <animeshs@ntnu.no>
2025-10-26 10:48:02 +0100

    combine RNA-seq/transcriptomics results using ENSEMBL ID, convert to ENTREZ and map to KEGG

---
+++ combineTxt.r
@@ -0,0 +1,56 @@
+# ..\R-4.5.0\bin\Rscript.exe .\combineTxt.r "L:\promec\TIMSTOF\LARS\2023\230217_Caroline\mRNA\Omego_T1_vs_Control_T1_mRNA_diff_expr_CPM1.txt" "L:\promec\TIMSTOF\LARS\2023\230217_Caroline\mRNA\Omego_T3_vs_Control_T3_mRNA_diff_expr_CPM1.txt" "L:\promec\TIMSTOF\LARS\2023\230217_Caroline\mRNA\Omego_T6_vs_Control_T6_mRNA_diff_expr_CPM1.txt" "L:\promec\TIMSTOF\LARS\2023\230217_Caroline\mRNA\Omego_T12_vs_Control_T12_mRNA_diff_expr_CPM1.txt" "logFC" "genes"
+args = commandArgs(trailingOnly=TRUE)
+print(paste("supplied argument(s):", length(args)))
+#args<-c("L:/promec/TIMSTOF/LARS/2023/230217_Caroline/mRNA/Omego_T1_vs_Control_T1_mRNA_diff_expr_CPM1.txt","L:/promec/TIMSTOF/LARS/2023/230217_Caroline/mRNA/Omego_T3_vs_Control_T3_mRNA_diff_expr_CPM1.txt","L:/promec/TIMSTOF/LARS/2023/230217_Caroline/mRNA/Omego_T6_vs_Control_T6_mRNA_diff_expr_CPM1.txt","L:/promec/TIMSTOF/LARS/2023/230217_Caroline/mRNA/Omego_T12_vs_Control_T12_mRNA_diff_expr_CPM1.txt","logFC","genes")
+print(args)
+rN<-args[length(args)]
+cN<-args[length(args)-1]
+inpFs<-basename(args)
+inpFs<-strsplit(inpFs,"\\.")
+inpFs<-unique(unlist(inpFs))
+inpFs<-paste(unlist(inpFs),collapse=".")
+#sheets<-list()
+outF<-paste(dirname(args[1]),inpFs,sep = "/")
+outPDF<-paste0(outF,"combo.pdf")
+outRep<-paste0(outF,"combo.xlsx")
+outRepCSV<-paste0(outF,"combo.csv")
+pdf(outPDF)
+dfMZ1<-NA
+for(inpF in args[-c((length(args)-1),(length(args)))]) {
+  #inpF<-args[1]
+  data<-read.csv(inpF,sep="\t",header =T)
+  print(inpF)
+  print(colnames(data))
+  hist(as.numeric(unlist(data[,cN])),main=inpF,breaks=100)
+  MZ1<-unlist(data[,rN])
+  dfMZ1<-union(dfMZ1,MZ1)
+  colName<-basename(inpF)
+  colnames(data)<-paste0(colnames(data),colName)
+  data[,rN]<-MZ1
+  assign(inpF,data)
+}
+length(dfMZ1)
+summary(warnings())
+summary(MZ1)
+data<-data.frame(dfMZ1)
+colnames(data)<-rN
+for (obj in args[-c((length(args)-1),(length(args)))]) {
+  #obj<-args[1]
+  print(obj)
+  objData<-get(obj)
+  colnames(objData)
+  data<-merge(data,objData,by=rN,all=T)
+}
+print(sum(rowSums(is.na(data))==ncol(data)))
+data=data[rowSums(is.na(data))!=ncol(data),]
+print(sum(rowSums(is.na(data))==ncol(data)))
+write.csv(data,outRepCSV,row.names = F)
+print(outRepCSV)
+writexl::write_xlsx(data,outRep)
+print(outRep)
+dataSel<-data[,grep(cN,colnames(data))]
+rownames(dataSel)<-data[,rN]
+print(dim(dataSel))
+writexl::write_xlsx(cbind(rownames(dataSel),dataSel),paste0(outF,"select.xlsx"))
+print("common IDs")
+print(sum(complete.cases(dataSel)))
--- mapKEGGpathview.r
+++ mapKEGGpathview.r
@@ -9,10 +9,10 @@ suppressPackageStartupMessages({
 library("pathview", quietly = TRUE)
 library("org.Hs.eg.db", quietly = TRUE)
 args <- commandArgs(trailingOnly = TRUE)
-inpF1 <- if (length(args) >= 1 && nzchar(args[1])) args[1] else "L:/promec/TIMSTOF/LARS/2023/230217_Caroline/combined/txt/proteinGroups.txtLFQ.intensity.110Omego1Cntr1h00.050.5InfBiotTestBH.xlsx.110Omego3Cntr3h00.110Omego6Cntr6h00.110Omego12Cntr12h00.Log2MedianChange.RowGeneUniProtScorePepsselect.xlsx"
+inpF1 <- if (length(args) >= 1 && nzchar(args[1])) args[1] else "L:/promec/TIMSTOF/LARS/2023/230217_Caroline/mRNA/Omego_T1_vs_Control_T1_mRNA_diff_expr_CPM1.txt.Omego_T3_vs_Control_T3_mRNA_diff_expr_CPM1.Omego_T6_vs_Control_T6_mRNA_diff_expr_CPM1.Omego_T12_vs_Control_T12_mRNA_diff_expr_CPM1.logFC.genesselect.xlsx"
 pathway <- if (length(args) >= 2 && nzchar(args[2])) strsplit(args[2], ",")[[1]] else c("hsa05135","hsa04140")
-idPos <- if (length(args) >= 3 && nzchar(args[3])) args[3] else 3
-scale <- if (length(args) >= 3 && nzchar(args[3])) args[3] else 5
+idPos <- if (length(args) >= 3 && nzchar(args[3])) args[3] else 1
+scale <- if (length(args) >= 4 && nzchar(args[4])) args[4] else 1
 # read table
 df <- readxl::read_xlsx(inpF1)
 # detect name column and numeric columns
@@ -33,9 +33,9 @@ allN <- trimws(allN[keep])
 mapRow <- rep(seq_along(rNs), lengths(rNs))[keep]
 matRep <- mat[mapRow, , drop = FALSE]
 rownames(matRep) <- allN
-allNE<-AnnotationDbi::select(org.Hs.eg.db, keys = allN, keytype = 'UNIPROT', columns = c('ENTREZID'))
+allNE<-AnnotationDbi::select(org.Hs.eg.db, keys = allN, keytype = 'ENSEMBL', columns = c('ENTREZID'))
 #allNE[is.na(allNE$ENTREZID), 'ENTREZID'] <- allNE[is.na(allNE$ENTREZID), 'UNIPROT']
-matRepID<-merge(matRep, allNE, by.x=0, by.y='UNIPROT', all=TRUE)
+matRepID<-merge(matRep, allNE, by.x=0, by.y='ENSEMBL', all=TRUE)
 write.csv(matRepID, file=paste0(tools::file_path_sans_ext(basename(inpF1)), ".IDmap.csv"), row.names=FALSE)
 matRepID <- matRepID[!is.na(matRepID$ENTREZID), , drop = FALSE]
 # combine rows with the same ENTREZID by taking the value with maximum absolute magnitude per column
@@ -47,16 +47,22 @@ aggMat <- do.call(rbind, lapply(entrezIDs, function(idxs) {
     v <- suppressWarnings(as.numeric(col))
     if (all(is.na(v))) return(NA_real_)
     v[which.max(abs(v))]
+    #v[which.min(abs(v))]
   })
 }))
 storage.mode(aggMat) <- "double"
 rownames(aggMat) <- names(entrezIDs)
-hda <- aggMat[,grep("Clip", colnames(aggMat), fixed=TRUE)]
+hda <- aggMat[,grep("logFC", colnames(aggMat), fixed=TRUE)]
+print(summary(hda))
 write.csv(hda, file=paste0(tools::file_path_sans_ext(basename(inpF1)), ".IDmap.absmax.csv"), row.names=TRUE)
+hda[is.infinite(hda)] <- NA
 #hda[is.na(hda)] <- 0
 #hda<-unique(hda)
-#hda <- scales::rescale(as.matrix(hda), c(-1*scale, scale))
-#pathview(hda, pathway.id = "04140", species = "hsa", gene.idtype = "ENTREZID")
+#hda<-mat
+#hda <- scales::rescale(as.matrix(hda), c(-1*5, 5))
+#hist(hda, breaks=100, main="Rescaled logFC values", xlab="Rescaled logFC")
+#pathview(hda, pathway.id = "04140", species = "hsa", gene.idtype = "ENSEMBL")
+#pathview(hda, pathway.id = "04140", species = "hsa", gene.idtype = "ENTREZ")
 outDir <- dirname(normalizePath(inpF1))
 preFix <- tools::file_path_sans_ext(basename(inpF1))
 message("Input: ", inpF1)
@@ -66,7 +72,8 @@ for (p in pathway) {
   message("Processing ", p)
   out_sfx <- paste("IDpos",idPos,"scale",scale, sep = ".")
   #message("Input values for ENTREZ 5291: ", paste(hda["5291",], collapse=", "))
-  pv <- try(pathview(gene.data = hda, pathway.id = p, species = "hsa", gene.idtype = "ENTREZID",low = list(gene = "blue"), mid = list(gene = "white"), high = list(gene = "orange"),both.dirs = list(gene = TRUE), na.col = "grey",limit = list(gene = max(abs(hda), na.rm = TRUE))), silent = TRUE)
+  pv <- try(pathview(gene.data = hda, pathway.id = p, species = "hsa", gene.idtype = "ENTREZ",low = list(gene = "blue"), mid = list(gene = "white"), high = list(gene = "orange"),both.dirs = list(gene = TRUE), na.col = "grey",limit = list(gene = max(abs(hda), na.rm = TRUE))), silent = TRUE)
+  #pv <- try(pathview(gene.data = hda, pathway.id = p, species = "hsa", gene.idtype = "ENSEMBL",low = list(gene = "blue"), mid = list(gene = "white"), high = list(gene = "orange"),both.dirs = list(gene = TRUE), na.col = "grey",limit = list(gene = max(abs(hda), na.rm = TRUE))), silent = TRUE)
   #if (!is.null(pv$plot.data.gene) && "5291" %in% (pv$plot.data.gene$kegg.names)) {message("Pathview output for 5291: ", paste(pv$plot.data.gene[pv$plot.data.gene$kegg.names=="5291",], collapse=", "))}
   if (inherits(pv, "try-error") || is.null(pv$plot.data.gene) || nrow(pv$plot.data.gene) == 0) {
     message("No mapping from UNIPROT for ", p, "; attempting ENTREZID fallback...")}
