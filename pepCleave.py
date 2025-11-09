dd0f3b189a3637f0ec67f06a1bd7ef50d56a2bff
Animesh Sharma <animeshs@ntnu.no>
2025-11-09 12:01:59 +0100

    combine omics data and map to kegg, extract values and write files to working dir

--- pepCleave.py
+++ pepCleave.py
@@ -1,47 +1,77 @@
-#python pepCleave.py ../../../uniprot-human-iso-feb23.fasta [KR] 7 35 0
-#https://pyteomics.readthedocs.io/en/latest/examples/example_fasta.html
-#python -m pip install -U pyteomics pandas
+#python pepCleave.py L:/promec/FastaDB/UP000005640_9606_unique_gene.fasta 10 30
+#sed 's/|/_/g' promec/promec/FastaDB/UP000005640_9606_unique_gene.fasta.len10to30.fasta | awk '/^>/ {print ">sp|S" ++i "|" substr($0, 2)} !/^>/ {print}' > promec/promec/FastaDB/UP000005640_9606_unique_gene.len10to30.hdrfix.fasta
+#grep -E "^[^>].*Z.*" promec/promec/FastaDB/UP000005640_9606_unique_gene.len10to30.hdrfix.fasta
+#Z:\DIA-NN\2.3.0>diann.exe --f "L:\promec\TIMSTOF\LARS\2025\251031_MAREN\251030_MAREN_DIALYSE_DIA_Slot1-37_1_11509.d" --lib "" --threads 24 --verbose 1 --out "Z:\DIA-NN\2.3.0\report.parquet" --qvalue 0.01 --matrices  --out-lib "Z:\DIA-NN\2.3.0\report-lib.parquet" --gen-spec-lib --fasta "L:\promec\FastaDB\UP000005640_9606_unique_gene.len10to30.hdrfix.fasta" --pre-search --pre-filter --met-excision --min-pep-len 10 --max-pep-len 30 --min-pr-mz 300 --max-pr-mz 1800 --min-pr-charge 1 --max-pr-charge 4 --min-fr-mz 200 --max-fr-mz 1800 --cut Z* --missed-cleavages 0 --unimod4 --reanalyse --rt-profiling
+#C:\Program Files\DIA-NN\2.2.0>diann.exe --lib "" --threads 32 --verbose 5 --out "F:\promec\FastaDB\humanL10to30peptides.parquet" --qvalue 0.01 --matrices  --out-lib "F:\promec\FastaDB\humanL10to30peptideslib.parquet" --gen-spec-lib --predictor --fasta "F:\promec\FastaDB\UP000005640_9606_unique_gene.len10to30.hdrfix.fasta" --fasta-search --min-fr-mz 200 --max-fr-mz 1800 --met-excision --min-pep-len 10 --max-pep-len 30 --min-pr-mz 300 --max-pr-mz 1800 --min-pr-charge 1 --max-pr-charge 4 --cut Z* --missed-cleavages 0 --unimod4 --mass-acc 15.0 --mass-acc-ms1 15.0 --reanalyse --rt-profiling
 from pyteomics import fasta, parser
-#parser.expasy_rules https://web.expasy.org/peptide_cutter/peptidecutter_enzymes.html
 import sys
-fastaF = sys.argv[1]
-protease = sys.argv[2]
-minLen= int(sys.argv[3])
-maxLen = int(sys.argv[4])
-missCleave = int(sys.argv[5])
-#fastaF = "../../../uniprot-human-iso-feb23.fasta"
-#protease = '[KR]'
-#minLen=7
-#maxLen=35
-#missCleave=0
-fastaFO=fastaF+protease+str(minLen)+str(maxLen)+str(missCleave)+".cleave.fasta"
-print('Cleaving ', fastaF ,'sequences with protease cleaving site',protease,'with miss-cleavages',missCleave,'filtering peptides at min / max-length of',minLen,'/',maxLen,'respectively and writing to... \n',fastaFO,'...\n')
-
-f= open(fastaFO,"w+")
-unique_peptides = set()
-for description, sequence in fasta.FASTA(fastaF):
-    new_peptides = parser.cleave(sequence, protease, missCleave,minLen)
-    new_peptides2 = [peptide for peptide in new_peptides if len(peptide) <= maxLen]
-    new_peptides3 = set(new_peptides2)
-    unique_peptides.update(new_peptides3)
-    [f.write(">T"+str(len(new_peptides3))+"F"+str(sequence.find(peptide))+"L"+str(len(peptide))+"O"+str(len(sequence))+"S"+description+"\n"+peptide+"\n") for peptide in new_peptides3]
-    #f.write(description+sequence+new_peptides5)
-    #print(new_peptides)
-print('Done, {0} unique peptide sequences obtained\n\n'.format(len(unique_peptides)))
-peptideCombined=''.join(unique_peptides)
-print("Total amino-acids",len(peptideCombined))
-print("Total amino-acids per sequence",len(peptideCombined)/len(unique_peptides))
-f.close()
-
 import pandas as pd
-aaCnt=parser.amino_acid_composition(peptideCombined)
-#aaCnt=parser.amino_acid_composition(max(unique_peptides))#unique_peptides.pop()
-compDF=pd.DataFrame([dict(aaCnt)])
-compDF=compDF.transpose()
-compDF=compDF.sort_values(0)
-compDF=compDF.sort_index()
-print(compDF)
-compDF.plot.bar().get_figure().savefig(fastaFO+'comp.png',dpi=100,bbox_inches = "tight")
-
 import pickle
-with open(fastaFO+'.pkl','wb') as f: pickle.dump(unique_peptides, f)#unique_peptides= pickle.load(f)
+if len(sys.argv) > 1:
+    fastaF = sys.argv[1]
+else:
+    fastaF = r"L:/promec/FastaDB/UP000005640_9606_unique_gene.fasta"
+if len(sys.argv) > 2:
+    minLen = int(sys.argv[2])
+else:
+    minLen = 10
+if len(sys.argv) > 3:
+    maxLen = int(sys.argv[3])
+else:
+    maxLen = 30
+
+# output file
+fastaFO = fastaF + ".len{0}to{1}.fasta".format(minLen, maxLen)
+print('Generating subsequences from', fastaF)
+print('minLen', minLen, 'maxLen', maxLen, '-> writing to', fastaFO)
+
+f = open(fastaFO, 'w+')
+unique_peptides = set()
+count_written = 0
+for description, sequence in fasta.FASTA(fastaF):
+    seq = sequence
+    seqlen = len(seq)
+    # iterate start positions
+    for i in range(0, seqlen - minLen + 1):
+        # iterate lengths
+        for L in range(minLen, maxLen + 1):
+            if i + L > seqlen:
+                break
+            peptide = seq[i:i+L]
+            if peptide in unique_peptides:
+                continue
+            unique_peptides.add(peptide)
+            header = ">{sid}|{start}-{end} len={llen} O={seqlen} {desc}".format(
+                sid=description.split()[0],
+                start=i+1,
+                end=i+L,
+                llen=L,
+                seqlen=seqlen,
+                desc=description
+            )
+            f.write(header + "\n")
+            f.write(peptide + "\n")
+            count_written += 1
+
+f.close()
+print('Done, wrote {0} unique subsequences'.format(count_written))
+
+# compute amino acid composition over concatenated unique peptides
+peptideCombined = ''.join(unique_peptides)
+if peptideCombined:
+    aaCnt = parser.amino_acid_composition(peptideCombined)
+    compDF = pd.DataFrame([dict(aaCnt)])
+    compDF = compDF.transpose()
+    compDF = compDF.sort_values(0)
+    compDF = compDF.sort_index()
+    print(compDF)
+    try:
+        compDF.plot.bar().get_figure().savefig(fastaFO + '.comp.png', dpi=100, bbox_inches='tight')
+    except Exception:
+        pass
+
+# save set
+with open(fastaFO + '.pkl', 'wb') as pf:
+    pickle.dump(unique_peptides, pf)
+
+print('Saved pickle to', fastaFO + '.pkl')
