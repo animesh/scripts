f54b4d489e69d9ca40da38b10c090cab977cce94
Animesh Sharma <animeshs@ntnu.no>
2025-12-09 18:33:14 +0100

    fix memory issue

--- combineCSV.r
+++ combineCSV.r
@@ -1,4 +1,4 @@
-# ..\R-4.5.0\bin\Rscript.exe .\combineCSV.r "F:\maren\extra\251203_3kda_A9_dda1_2_desalt.raw.centroid.MGF.csv" "F:\maren\extra\251203_3kda_A9_dda1_2.raw.centroid.MGF.csv" "F:\maren\extra\251203_3kda_A3_dda1_2_desalt.raw.centroid.MGF.csv" "F:\maren\extra\251203_3kda_A3_dda1_2.raw.centroid.MGF.csv" "F:\maren\extra\251203_1kda_A7_dda1_2_desalt.raw.centroid.MGF.csv" "F:\maren\extra\251203_1kda_A7_dda1_2.raw.centroid.MGF.csv" "F:\maren\extra\251203_1kda_A3_dda1_2_desalt.raw.centroid.MGF.csv" "F:\maren\extra\251203_1kda_A3_dda1_2.raw.centroid.MGF.csv" "20" "score" "peptide"
+# "c:\Program Files\R\R-4.5.1\bin\Rscript.exe" combineCSV.r "F:\promec\HF\Lars\2025\251204_Maren_Gemma\Extra\251203_3kda_A9_dda1_2_desalt.raw.centroid.MGF.csv" "F:\promec\HF\Lars\2025\251204_Maren_Gemma\Extra\251203_3kda_A9_dda1_2.raw.centroid.MGF.csv" "F:\promec\HF\Lars\2025\251204_Maren_Gemma\Extra\251203_3kda_A3_dda1_2_desalt.raw.centroid.MGF.csv" "F:\promec\HF\Lars\2025\251204_Maren_Gemma\Extra\251203_3kda_A3_dda1_2.raw.centroid.MGF.csv" "F:\promec\HF\Lars\2025\251204_Maren_Gemma\Extra\251203_1kda_A7_dda1_2_desalt.raw.centroid.MGF.csv" "F:\promec\HF\Lars\2025\251204_Maren_Gemma\Extra\251203_1kda_A7_dda1_2.raw.centroid.MGF.csv" "F:\promec\HF\Lars\2025\251204_Maren_Gemma\Extra\251203_1kda_A3_dda1_2_desalt.raw.centroid.MGF.csv" "F:\promec\HF\Lars\2025\251204_Maren_Gemma\Extra\251203_1kda_A3_dda1_2.raw.centroid.MGF.csv" "20" "score" "peptide"
 # for i in /mnt/f/maren/extra/*.raw ; do echo $i;  mono RawRead.exe $i ; done
 # for %i in ("F:\maren\*.MGF") do (java -jar "Z:\Download\SearchGUI-4.3.15-windows\SearchGUI-4.3.17\resources\Novor\novor.jar" "%i" -p novo.params -f)
 args = commandArgs(trailingOnly=TRUE)
@@ -13,48 +13,48 @@ inpFs<-strsplit(inpFs,"\\.")
 inpFs<-unique(unlist(inpFs))
 inpFs<-paste(unlist(inpFs),collapse=".")
 #sheets<-list()
-outF<-paste(dirname(args[1]),inpFs,sep = "/")
+outF<-paste(dirname(args[1]),sep = "/")
 outPDF<-paste0(outF,"combo.pdf")
-outRep<-paste0(outF,"combo.xlsx")
 outRepCSV<-paste0(outF,"combo.csv")
 pdf(outPDF)
 dfMZ1<-NA
 for(inpF in args[-c((length(args)-2):(length(args)))]) {
   #inpF<-args[1]
-  data<-read.csv(inpF,skip=sN,header=T)
   print(inpF)
+  data<-read.csv(inpF,skip=sN,header=T)
   print(colnames(data))
   hist(as.numeric(unlist(data[,cN])),main=inpF,breaks=100)
+  data[,rN]=toupper(data[,rN])
   data[,rN]=gsub(" ", "", data[,rN], fixed = TRUE)
-  MZ1<-unlist(data[,rN])
+  data[,rN]=gsub("[^[:alnum:]]+","_",data[,rN])
+  data<-aggregate(data[,cN],data[rN], max)
+  rownames(data)<-data[,rN]
+  data[,rN]<-NULL
+  colnames(data)<-basename(inpF)
+  MZ1<-unlist(rownames(data))
   dfMZ1<-union(dfMZ1,MZ1)
-  colName<-basename(inpF)
-  colnames(data)<-paste0(colnames(data),colName)
-  data[,rN]<-MZ1
   assign(inpF,data)
+  print(dim(data))
 }
 length(dfMZ1)
 summary(warnings())
 summary(MZ1)
 data<-data.frame(dfMZ1)
 colnames(data)<-rN
-for (obj in args[-c((length(args)-1),(length(args)))]) {
+for (obj in args[-c((length(args)-2):(length(args)))]) {
   #obj<-args[1]
   print(obj)
   objData<-get(obj)
-  colnames(objData)
-  data<-merge(data,objData,by=rN,all=T)
+  print(colnames(objData))
+  print(dim(objData))
+  data<-merge(data,objData,by.x=rN,by.y=0,all=T)
+  print(dim(data))
+  rm(objData)
 }
 print(sum(rowSums(is.na(data))==ncol(data)))
 data=data[rowSums(is.na(data))!=ncol(data),]
 print(sum(rowSums(is.na(data))==ncol(data)))
+data=data[order(rowSums(data[,-1],na.rm=T),decreasing=T),]
 write.csv(data,outRepCSV,row.names = F)
 print(outRepCSV)
-writexl::write_xlsx(data,outRep)
-print(outRep)
-dataSel<-data[,grep(cN,colnames(data))]
-rownames(dataSel)<-data[,rN]
-print(dim(dataSel))
-writexl::write_xlsx(cbind(rownames(dataSel),dataSel),paste0(outF,"select.xlsx"))
-print("common IDs")
-print(sum(complete.cases(dataSel)))
+print(sum(complete.cases(data)))
