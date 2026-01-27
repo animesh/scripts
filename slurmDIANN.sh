aafd3794590df99218315ca7867118f556f5de83
Animesh Sharma <ash022@login-1.saga>
2026-01-27 08:20:51 +0100

    Thalassiosira_pseudonana DIA-NN 2.2 batch

--- slurmDIANN.sh
+++ slurmDIANN.sh
@@ -1,9 +1,5 @@
-#bash slurmDIANN.sh /cluster/projects/nn9036k/scripts/maike
-#cd /cluster/home/ash022/scripts
-dos2unix scratch.slurm # slurmDIANN.sh
-#mkdir maike
-#cp -rf ./DL/Raw/LARS/251212_Maike/251212_*H*P*d ./maike/.
-#cp ./PD/FastaDB/UP000000589_10090.fasta ./maike/.
+#dos2unix scratch.slurm  slurmDIANN.sh
+#bash slurmDIANN.sh /cluster/projects/nn9036k/scripts/tore
 #for i in $PWD/maike/*.d ; do grep "TestFile.d" ; sed -i "s|TestFile\.d|$i|g" $i.slurm  ;done
 DATADIR=$1
 SEARCHTEXT=TestFile.d
@@ -14,9 +10,7 @@ for i in $DATADIR/*.d ; do echo $i ; sed "s|$SEARCHTEXT|$i|g" scratch.slurm > $i
 echo $WRITEFILE
 ls -ltrh $DATADIR/*$WRITEFILE.slurm 
 squeue -u ash022
-#rsync -Parv maike/*.report.* /nird/projects/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2025/251106_MAIKE_b/
-#rsync -Parv 0maike/*.report.* /nird/projects/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2025/251024_Maike/
-#tar cvf Data/251106_MAIKE.SILAC.DIA.NN.tar TIMSTOF/LARS/2025/251106_MAIKE_b/*report.report.par*
+#rsync -Parv tore/*.report.* /nird/projects/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2026/
 
 
 
