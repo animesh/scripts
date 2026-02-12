948c18d4e3d110f01dcc31b16259c61b145b3316
Animesh Sharma <ash022@login-1.saga>
2026-02-12 09:59:59 +0100

    batch phos DIA Thalassiosira_pseudonana with v2.2

--- slurmDIANN.sh
+++ slurmDIANN.sh
@@ -1,4 +1,8 @@
 #dos2unix scratch.slurm  slurmDIANN.sh
+#mkdir tore tore_phos
+#rsync -Parv  TIMSTOF/LARS/2026/260129_ToreBrembuPHOS/ToreB_8/260129_ToreBrembuPHOS_*.d tore_phos/
+#rsync -Parv  TIMSTOF/LARS/2026/260107_Tore/Thalassiosira_pseudonana_PASA-proteins_uniprot.MC1V3.predicted.speclib tore/
+#rsync -Parv  TIMSTOF/LARS/2026/260107_Tore/Thalassiosira_pseudonana_PASA-proteins_uniprot.fasta  tore/
 #bash slurmDIANN.sh /cluster/projects/nn9036k/scripts/tore_phos
 #for i in $PWD/maike/*.d ; do grep "TestFile.d" ; sed -i "s|TestFile\.d|$i|g" $i.slurm  ;done
 DATADIR=$1
@@ -10,7 +14,7 @@ for i in $DATADIR/*.d ; do echo $i ; sed "s|$SEARCHTEXT|$i|g" scratch.slurm > $i
 echo $WRITEFILE
 ls -ltrh $DATADIR/*$WRITEFILE.slurm 
 squeue -u ash022
-#rsync -Parv tore/*.report.* /nird/projects/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2026/
+#rsync -Parv tore_phos/*.report.* /nird/projects/NS9036K/NORSTORE_OSL_DISK/NS9036K/promec/promec/TIMSTOF/LARS/2026/260129_ToreBrembuPHOS/ToreB_8/
 
 
 
