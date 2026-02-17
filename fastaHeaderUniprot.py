e40f02e9e5662deef879c9db6dc1eacebeada8aa
Animesh Sharma <animeshs@ntnu.no>
2026-02-17 13:11:32 +0100

    format araport gene model/proteome

--- fastaHeaderUniprot.py
+++ fastaHeaderUniprot.py
@@ -1,34 +1,20 @@
-#python fastaHeaderUniprot.py < F:\promec\TIMSTOF\LARS\2026\260107_Tore\Thalassiosira_pseudonana_PASA-proteins.txt > F:\promec\TIMSTOF\LARS\2026\260107_Tore\Thalassiosira_pseudonana_PASA-proteins_uniprot.fasta
-
+#python fastaHeaderUniprot.py < L:\promec\TIMSTOF\LARS\2025\251103_STEVEN\Araport11_pep_20250411.fasta > L:\promec\TIMSTOF\LARS\2025\251103_STEVEN\Araport11_pep_20250411.uniprot.fasta
+#python fastaHeaderUniprot.py < L:\promec\TIMSTOF\LARS\2025\251103_STEVEN\Araport11_pep_20250411_representative_gene_model.fasta > L:\promec\TIMSTOF\LARS\2025\251103_STEVEN\Araport11_pep_20250411_representative_gene_model.uniprot.fasta
 import sys
 import re
-
 for line in sys.stdin:
     if line.startswith(">"):
-        # Header line → rewrite
+        # Header line → rewrite >GMAT1G01010.1 | Symbols:NAC001,NTL10,ANAC001 | NAC domain containing protein 1 |  Chr1:3760-5630 FORWARD LENGTH=429
         header = line.strip()[1:]
-        parts = header.split()
-
-        transcript = parts[0]          # g244.t1
-        gene = parts[1]                # g244
+        parts = header.split(sep="|")
+        gene = parts[1].strip().replace('Symbols:', '').replace(',', '_').strip()
         rest = " ".join(parts[2:])     # contig + organism info
-
-        accession = transcript.replace(".", "").upper()   # G244T1
-        entry = f"{accession}_THAPS"
-
-        new_header = (
-            f">tr|{accession}|{entry} "
-            f"{rest} protein "
-            f"OS=Thalassiosira pseudonana "
-            f"OX=35128 "
-            f"GN={gene}_t1 "
-            f"PE=1 SV=1"
-        )
-
-        print(new_header)
+        accession = parts[0].strip().replace('.', '-').upper()
+        print(f">tr|{accession}|{accession}_ARAPORT {rest} OS=Araport11_pep_20250411 GN={gene} PE=1 SV=1")
+        # >tr|GMAT1G01010-1|GMAT1G01010-1_ARAPORT  NAC domain containing protein 1    Chr1:3760-5630 FORWARD LENGTH=429 OS=Araport11_pep_20250411 GN=NAC001_NTL10_ANAC001 PE=1 SV=1
     else:
         # Sequence line → keep only alphabetic characters and uppercase
         cleaned = re.sub(r"[^A-Za-z]", "", line)
         cleaned = cleaned.upper()
         print(cleaned)
-
+        
