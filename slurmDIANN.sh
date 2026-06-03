d59184d0881eb7d0679517a8a84b0cbe74efaa34
ani@saga <ash022@login-1.saga>
2026-06-03 18:18:42 +0200

    move batch to bigmem nodes

--- slurmDIANN.sh
+++ slurmDIANN.sh
@@ -1,5 +1,5 @@
 #dos2unix scratch.slurm  slurmDIANN.sh
-#bash slurmDIANN.sh /cluster/projects/nn9036k/scrbkup/barbara
+#bash slurmDIANN.sh /cluster/projects/nn9036k/scrbkup/sonaliDIA2
 DATADIR=$1
 SEARCHTEXT=TestFile.d
 CURRENTEPOCTIME=`date +%s`
