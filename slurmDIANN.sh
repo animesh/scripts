fa8a305c250e71c0672abc25a8d94741adf5d360
ani@saga <ash022@login-1.saga>
2026-06-03 22:54:35 +0200

    check ppm15

--- slurmDIANN.sh
+++ slurmDIANN.sh
@@ -1,5 +1,5 @@
 #dos2unix scratch.slurm  slurmDIANN.sh
-#bash slurmDIANN.sh /cluster/projects/nn9036k/scrbkup/sonaliDIA2
+#bash slurmDIANN.sh /cluster/projects/nn9036k/scrbkup/barbara
 DATADIR=$1
 SEARCHTEXT=TestFile.d
 CURRENTEPOCTIME=`date +%s`
