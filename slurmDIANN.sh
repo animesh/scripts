159cfe100949652d50d1cbb2e40b8ab49489973a
ani@saga <ash022@login-2.saga>
2026-06-23 12:40:28 +0200

    add dotnet module and run latest diann as batch over saga with orbitrap raw data from https://www.ebi.ac.uk/pride/private/ws/archive/v2/file/fdownload/PXD078489/35854891?token=c0c9919b04c1e50d0a380c3c7fbb9d66

--- slurmDIANN.sh
+++ slurmDIANN.sh
@@ -1,11 +1,11 @@
 #dos2unix scratch.slurm  slurmDIANN.sh
-#bash slurmDIANN.sh /cluster/projects/nn9036k/scrbkup/sonaliDIA
+#bash slurmDIANN.sh /cluster/projects/nn9036k/scrbkup/nDIA/Fig2A-E_raw/DIA 
 DATADIR=$1
 SEARCHTEXT=TestFile.d
 CURRENTEPOCTIME=`date +%s`
 WRITEFILE=$CURRENTEPOCTIME.report
 echo $WRITEFILE
-for i in $DATADIR/*.d ; do echo $i ; sed "s|$SEARCHTEXT|$i|g" scratch.slurm > $i.$WRITEFILE.tmp  ; sed "s|WRITEFILE|$WRITEFILE|g" $i.$WRITEFILE.tmp > $i.$WRITEFILE.slurm  ; sbatch $i.$WRITEFILE.slurm ; done
+for i in $DATADIR/*.raw ; do echo $i ; sed "s|$SEARCHTEXT|$i|g" scratch.slurm > $i.$WRITEFILE.tmp  ; sed "s|WRITEFILE|$WRITEFILE|g" $i.$WRITEFILE.tmp > $i.$WRITEFILE.slurm  ; sbatch $i.$WRITEFILE.slurm ; done
 echo $WRITEFILE
 ls -ltrh $DATADIR/*$WRITEFILE.slurm 
 squeue -u ash022
