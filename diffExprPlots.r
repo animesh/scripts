36499c10ceca85cb2e4b8dce6d4d6457ae578863
Animesh Sharma <animeshs@ntnu.no>
2026-09-15 10:45:41 +0200

    plot for the skin slides conf

--- diffExprPlots.r
+++ diffExprPlots.r
@@ -1,30 +1,45 @@
-#Rscript diffExprPlots.r "L:/promec/Animesh/Sigrid/Hierarchical clustering/U87MG mut vs wt uten beh til publ.xlsx" 0.05 0.5
-names
+#Rscript diffExprPlots.r "L:/promec/Animesh/Kamila/Help_with_poster_for_Eadv_conference/BCC_PRM90_final for LARS.xlsx" 0.5 0.5 Gene
 #setup####
 #install.packages("readxl")
 #install.packages("svglite")
 #install.packages("ggplot2")
 args = commandArgs(trailingOnly=TRUE)
 inpW <- args[1]
-#inpW<-"L:/promec/Animesh/Sigrid/Hierarchical clustering/U87MG mut vs wt uten beh til publ.xlsx"
+#inpW<-"L:/promec/Animesh/Kamila/Help_with_poster_for_Eadv_conference/BCC_PRM90_final for LARS.xlsx"
 selThr <- args[2]
-#selThr<-0.05
+#selThr<-0.5
 selThrFC<-args[3]
 #selThrFC<-0.5
 labelS <- args[4]
 #labelS<-"Gene"
 print(args)
 #data####
-data<-readxl::read_xlsx(inpW,skip=1)
+data<-readxl::read_xlsx(inpW,sheet=1)
 data<-data.frame(data)
+data[,"ID"]<-data[,labelS]
 #volcano
-data[is.na(data)] <- 0
-Significance=data$MUTCtrlWTCtrlCorrectedPValueBH<selThr&data$MUTCtrlWTCtrlCorrectedPValueBH>0&abs(data$MUTCtrlWTCtrlLog2MedianChange)>selThrFC
-sum(Significance)
-dsub <- subset(data,Significance)
-p <- ggplot2::ggplot(data,ggplot2::aes(MUTCtrlWTCtrlLog2MedianChange,MUTCtrlWTCtrlPValueMinusLog10))+ ggplot2::geom_point(ggplot2::aes(color=Significance))
-p<-p + ggplot2::theme_bw(base_size=8) + ggplot2::geom_text(data=dsub,ggplot2::aes(label=labelS),hjust=0, vjust=0,size=1,position=ggplot2::position_jitter(width=0.5,height=0.1)) + ggplot2::scale_fill_gradient(low="white", high="darkblue") + ggplot2::xlab("Log2 Median Change") + ggplot2::ylab("-Log10 P-value") + ggplot2::xlim(-15, 15) + ggplot2::ylim(.Machine$double.eps,4.99999999999999)
+#data[is.na(data)] <- 0
+#data[,grep("FC",colnames(data))]<-log2(data[,grep("FC",colnames(data))])
+plot(data[,"Tumor_FC"],data[,"Stroma_FC"])
+data[,"log2Stroma_FC"]<-log2(data[,"Stroma_FC"])
+data[,"log10Stroma_padj"]<-log10(data[,"Stroma_padj"])*(-1)
+significance=data$Stroma_padj<selThr&abs(data$log2Stroma_FC)>selThrFC
+sum(significance,na.rm=TRUE)
+dsub <- subset(data,significance)
+p <- ggplot2::ggplot(data,ggplot2::aes(log2Stroma_FC,log10Stroma_padj))+ ggplot2::geom_point(ggplot2::aes(color=significance))
+p<-p + ggplot2::theme_bw(base_size=8) + ggplot2::geom_text(data=dsub,ggplot2::aes(label=ID),hjust=0, vjust=0,size=1,position=ggplot2::position_jitter(width=0.5,height=0.1)) + ggplot2::scale_fill_gradient(low="white", high="darkblue") + ggplot2::xlab("Log2 Median Change") + ggplot2::ylab("-Log10 P-value") + ggplot2::xlim(-15, 15) + ggplot2::ylim(.Machine$double.eps,4.99999999999999)
 #f=paste(file,proc.time()[3],".jpg")
 #install.packages("svglite")
-ggplot2::ggsave(paste0(inpW,selThr,selThrFC,labelS,"VolcanoTest.svg"),width=10, height=8,dpi=300, p)
+ggplot2::ggsave(paste0(inpW,selThr,selThrFC,labelS,".Stroma.VolcanoTest.svg"),width=10, height=8,dpi=300, p)
+print(p)
+data[,"log2Tumor_FC"]<-log2(data[,"Tumor_FC"])
+data[,"log10Tumor_padj"]<-log10(data[,"Tumor_padj"])*(-1)
+significance=data$Tumor_padj<selThr&abs(data$log2Tumor_FC)>selThrFC
+sum(significance,na.rm=TRUE)
+dsub <- subset(data,significance)
+p <- ggplot2::ggplot(data,ggplot2::aes(log2Tumor_FC,log10Tumor_padj))+ ggplot2::geom_point(ggplot2::aes(color=significance))
+p<-p + ggplot2::theme_bw(base_size=8) + ggplot2::geom_text(data=dsub,ggplot2::aes(label=ID),hjust=0, vjust=0,size=1,position=ggplot2::position_jitter(width=0.5,height=0.1)) + ggplot2::scale_fill_gradient(low="white", high="darkblue") + ggplot2::xlab("Log2 Median Change") + ggplot2::ylab("-Log10 P-value") + ggplot2::xlim(-15, 15) + ggplot2::ylim(.Machine$double.eps,4.99999999999999)
+#f=paste(file,proc.time()[3],".jpg")
+#install.packages("svglite")
+ggplot2::ggsave(paste0(inpW,selThr,selThrFC,labelS,".Tumor.VolcanoTest.svg"),width=10, height=8,dpi=300, p)
 print(p)
--- diffExprTestLIMPA.r
+++ diffExprTestLIMPA.r
@@ -134,6 +134,7 @@ compare_one_contrast <- function(fit_a, fit_b, coef_name, label_a, label_b) {
 }
 
 annoData <- read.table(file = labelF, header = TRUE, sep = "\t", row.names = 1, check.names = FALSE, quote = "")
+plot(annoData)
 stopifnot("Cell_Rep" %in% colnames(annoData))
 stopifnot("Cell" %in% colnames(annoData))
 stopifnot("Rep" %in% colnames(annoData))
@@ -150,7 +151,8 @@ annoData$Experiment <- factor(annoData$Rep, levels = c(1, 2, 3), labels = c("Exp
 cat("Computing observed-only group log2 medians per protein...\n")
 obs_summary <- compute_observed_group_medians(y.peptide = y.peptide, annoData cj= annoData, protein_col = "Leading razor protein")
 observed_group_medians <- obs_summary$protein_group_medians
-#plot(observed_group_medians$obs_groupMedian_log2_KPC_1, observed_group_medians$obs_groupMedian_log2_CT26_1, xlab = "Observed log2 median KPC_1", ylab = "Observed log2 median CT26_1", main = "Observed log2 medians per protein (KPC_1 vs CT26_1)")
+plot(observed_group_medians$obs_groupMedian_log2_KPC_1, observed_group_medians$obs_groupMedian_log2_CT26_1, xlab = "Observed log2 median KPC_1", ylab = "Observed log2 median CT26_1", main = "Observed log2 medians per protein (KPC_1 vs CT26_1)")
+hist(observed_group_medians$obs_groupMedian_log2_KPC_1)
 write.csv(observed_group_medians, file = file.path(outdir, "observed_group_medians", "observed_group_log2_medians_per_protein.csv"), row.names = FALSE)
 write.csv(obs_summary$protein_sample_obs, file = file.path(outdir, "observed_group_medians", "observed_protein_by_sample_log2_medians_from_original_data.csv"))
 
